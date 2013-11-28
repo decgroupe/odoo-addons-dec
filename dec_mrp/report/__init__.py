2b4440ad356871f4b7e2acfcc12c7f89ea46bad7
y.papouin <y.papouin@99bbe651-1ba9-0c45-94a2-e1a02bee17fe>
2013-11-28 15:48:39 +0000

    Cleaning by removing old records
    git-svn-id: svn://lan1.dec-industrie.com/default/openerp/addons-dec@1992 99bbe651-1ba9-0c45-94a2-e1a02bee17fe

--- dec_mrp/report/__init__.py
+++ dec_mrp/report/__init__.py
@@ -19,7 +19,6 @@
 #
 ##############################################################################
 
-import order_dec
 import mrp_parser
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
--- dec_mrp/report/generate_from_sxw.sh
+++
@@ -1,1 +0,0 @@
-python /media/raid2to/decindus/tools/openerp-6.1/addons/base_report_designer/openerp_sxw2rml/openerp_sxw2rml.py order_dec.sxw > order_dec.rml
