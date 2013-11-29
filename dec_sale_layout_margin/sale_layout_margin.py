5c4b5c55b3c5ffef1070b91e6437975282573ca7
y.papouin <y.papouin@99bbe651-1ba9-0c45-94a2-e1a02bee17fe>
2013-11-29 13:05:27 +0000

    Procurements added to sale view
    git-svn-id: svn://lan1.dec-industrie.com/default/openerp/addons-dec@1997 99bbe651-1ba9-0c45-94a2-e1a02bee17fe

--- dec_sale_layout_margin/sale_layout_margin.py
+++ dec_sale_layout_margin/sale_layout_margin.py
@@ -19,10 +19,13 @@
 ##############################################################################
 
 from osv import fields, osv
-from datetime import datetime
+from datetime import datetime, timedelta
 from dateutil.relativedelta import relativedelta
 from dateutil.rrule import *
+from tools import DEFAULT_SERVER_DATE_FORMAT, DEFAULT_SERVER_DATETIME_FORMAT, float_compare
 import time
+import netsvc
+
 
 import decimal_precision as dp
 from tools.translate import _
@@ -140,6 +143,12 @@ class sale_order_line(osv.osv):
         default['layout_type'] = self.browse(cr, uid, id, context=context).layout_type
         res = super(sale_order_line, self).copy(cr, uid, id, default, context)
         return res
+    
+    def copy_data(self, cr, uid, id, default=None, context=None):
+        if not default:
+            default = {}
+        default.update({'procurement_id': False, 'procurement_ids': []})
+        return super(sale_order_line, self).copy_data(cr, uid, id, default, context=context)
 
     def product_id_change(self, cr, uid, ids, pricelist, product, qty=0, uom=False, qty_uos=0, uos=False, name='', partner_id=False, lang=False, update_tax=True, date_order=False, packaging=False, fiscal_position=False, flag=False, context=None):
         result = super(sale_order_line, self).product_id_change(cr, uid, ids, pricelist, product, qty=qty, uom=uom, qty_uos=qty_uos, uos=uos, name=name, partner_id=partner_id, lang=lang, update_tax=update_tax, date_order=date_order, packaging=packaging, fiscal_position=fiscal_position, flag=flag, context=context)
@@ -252,7 +261,8 @@ class sale_order_line(osv.osv):
     _order = "order_id, sequence asc"
     _description = "Sales Order line"
     _inherit = "sale.order.line"
-    _columns = {
+    _columns = {        
+        'order_id': fields.many2one('sale.order', 'Order Reference', required=True, ondelete='cascade', select=True, readonly=False, states={'draft':[('readonly',False)]}),
         'layout_type': fields.selection([
                 ('article', 'Product'),
                 ('title', 'Title'),
@@ -265,6 +275,7 @@ class sale_order_line(osv.osv):
         'price_unit': fields.float('Unit Price', required=True, digits_compute= dp.get_precision('Sale Price'), readonly=True, states={'draft': [('readonly', False)]}),
         'product_uom_qty': fields.float('Quantity (UoM)', digits_compute= dp.get_precision('Product UoS')),
         'product_uom': fields.many2one('product.uom', 'Product UoM'),
+        'procurement_ids': fields.one2many('procurement.order', 'sale_line_id', 'Procurements'),
         # Override the field to call the overridden _amount_line function
         'price_subtotal': fields.function(_amount_line, method=True, string='Subtotal', digits_compute= dp.get_precision('Sale Price')),
         'margin': fields.function(_product_margin, string='Margin', store = True),  
@@ -289,6 +300,10 @@ class sale_order_line(osv.osv):
 
     def button_dummy(self, cr, uid, ids, context=None):
         return True
+    
+    def button_fix(self, cr, uid, ids, context=None):
+        res = self.write(cr, uid, ids, {'procurement_id': False, 'procurement_ids': []})
+        return res and self.button_done(cr, uid, ids, context=context)
 
     def compute_margin(self, price_unit, purchase_price, product_uos_qty, discount):
         result = (price_unit*product_uos_qty*(100.0-discount)/100.0) -(purchase_price*product_uos_qty)
@@ -373,8 +388,7 @@ class sale_order(osv.osv):
 
     def write(self, cr, uid, ids, vals, context=None):
 
-        locked_fields = set(['user_id','date_order','abstract_line_ids'])    
-              
+        locked_fields = set(['user_id','date_order','abstract_line_ids'])           
         for sale in self.browse(cr, uid, ids, context=context):
             if sale.state == 'draft':
                 # Check if someone is trying to unlock a quotation
@@ -388,6 +402,16 @@ class sale_order(osv.osv):
                 # Check if someone is trying to modify a locked quotation
                 if not vals.has_key('locked') and sale.locked and locked_fields.intersection(vals): 
                     raise osv.except_osv(_('Operation forbidden'), _('%s is currently locked, you are not allowed to make changes') % (sale.name))
+      
+                
+        if vals.get('partner_shipping_id', False): 
+            # Shipping address change propagated to all attached picking
+            picking_obj = self.pool.get('stock.picking')
+            picking_ids = [x['picking_ids'] for x in self.read(cr, uid, ids, ['picking_ids'])]
+            # Make a flatten list ie: [[1,2],[3]] -> [1,2,3]
+            picking_ids = [item for sublist in picking_ids for item in sublist]
+            picking_obj.write(cr, uid, picking_ids, {'address_id': vals['partner_shipping_id']}, context=context)
+            
                 
         return super(sale_order, self).write(cr, uid, ids, vals, context)
     
@@ -410,6 +434,113 @@ class sale_order(osv.osv):
         })
         res = super(sale_order, self).copy(cr, uid, id, default, context)
         return res
+    
+
+    # Override default
+    def _create_pickings_and_procurements(self, cr, uid, order, order_lines, picking_id=False, context=None):
+        """Create the required procurements to supply sale order lines, also connecting
+        the procurements to appropriate stock moves in order to bring the goods to the
+        sale order's requested location.
+
+        If ``picking_id`` is provided, the stock moves will be added to it, otherwise
+        a standard outgoing picking will be created to wrap the stock moves, as returned
+        by :meth:`~._prepare_order_picking`.
+
+        Modules that wish to customize the procurements or partition the stock moves over
+        multiple stock pickings may override this method and call ``super()`` with
+        different subsets of ``order_lines`` and/or preset ``picking_id`` values.
+
+        :param browse_record order: sale order to which the order lines belong
+        :param list(browse_record) order_lines: sale order line records to procure
+        :param int picking_id: optional ID of a stock picking to which the created stock moves
+                               will be added. A new picking will be created if ommitted.
+        :return: True
+        """
+        move_obj = self.pool.get('stock.move')
+        picking_obj = self.pool.get('stock.picking')
+        procurement_obj = self.pool.get('procurement.order')
+        proc_ids = []
+        
+        def create_procurement_move(line, move_data):
+            if move_data:
+                move_id = move_obj.create(cr, uid, move_data)
+            else:
+                move_id = False
+                
+            proc_data = self._prepare_order_line_procurement(cr, uid, order, line, move_id, date_planned, context=context)
+            if move_data:
+                proc_data['product_qty'] = move_data['product_qty']
+            
+            proc_id = procurement_obj.create(cr, uid, proc_data)
+            proc_ids.append(proc_id)
+            line.write({'procurement_id': proc_id})
+            line.write({'procurement_ids': [(4, proc_id)]})
+            
+            self.ship_recreate(cr, uid, order, line, move_id, proc_id)
+
+        for line in order_lines:
+            if line.state == 'done':
+                continue
+
+            date_planned = self._get_date_planned(cr, uid, order, line, order.date_confirm, context=context) #https://code.launchpad.net/~openerp-dev/openobject-addons/6.1-opw-587363-ado
+
+            # Create procurement for new lines but also for existing one where their procurement has been canceled
+            if line.product_id:
+                
+                if line.state == 'confirmed':
+                    # Do not recreate currently running procurement
+                    if line.procurement_id and line.procurement_id.state in ('confirmed', 'running', 'ready', 'done', 'waiting'):
+                        continue
+                elif line.state == 'exception':
+                    # Reset line state to confirmed state
+                    self.pool.get('sale.order.line').button_confirm(cr, uid, [line.id]) 
+                    # Cancel existing move if the canceled procurement exists 
+                    if line.procurement_id and line.procurement_id.state == 'cancel'\
+                    and line.procurement_id.move_id and line.procurement_id.move_id.state not in ('done', 'cancel'):
+                        move_obj.action_cancel(cr, uid, [line.procurement_id.move_id.id])
+                
+                if line.product_id.product_tmpl_id.type in ('product', 'consu'):
+                    if not picking_id:
+                        picking_id = picking_obj.create(cr, uid, self._prepare_order_picking(cr, uid, order, context=context))
+                        
+                    move_data = self._prepare_order_line_move(cr, uid, order, line, picking_id, date_planned, context=context) 
+                        
+                    # Specific for DEC Industrie: Split 1 procurement of Qty=n into n x procurements of Qty=1
+                    if line.product_id.procure_method=='make_to_order' and line.product_id.supply_method=='produce':
+                        qty = int(line.product_uom_qty)
+                        assert line.product_uom_qty / qty == 1 # assert 1.0 = 1
+
+                        for i in range(1, qty+1):
+                            alt_data = move_data.copy() 
+                            alt_data['product_qty'] = 1.0 
+                            create_procurement_move(line, alt_data)
+                            
+                    else:
+                        create_procurement_move(line, move_data)                       
+                        
+                else:
+                    # a service has no stock move
+                    create_procurement_move(line, False)  
+
+        wf_service = netsvc.LocalService("workflow")
+        if picking_id:
+            wf_service.trg_validate(uid, 'stock.picking', picking_id, 'button_confirm', cr)
+
+        for proc_id in proc_ids:
+            wf_service.trg_validate(uid, 'procurement.order', proc_id, 'button_confirm', cr)
+
+        val = {}
+        if order.state == 'shipping_except':
+            val['state'] = 'progress'
+            val['shipped'] = False
+
+            if (order.order_policy == 'manual'):
+                for line in order.order_line:
+                    if (not line.invoiced) and (line.state not in ('cancel', 'draft')):
+                        val['state'] = 'manual'
+                        break
+        order.write(val)
+        return True
 
     def _product_margin(self, cr, uid, ids, field_name, arg, context=None):
         result = {}
@@ -423,8 +554,12 @@ class sale_order(osv.osv):
         parent_get_order = super(sale_order, self.pool.get('sale.order'))._get_order.im_func
         return parent_get_order(self, cr, uid, ids, context=context)
     
+    # Override default
     def _get_date_planned(self, cr, uid, order, line, start_date, context=None):
-        result = super(sale_order, self)._get_date_planned(cr, uid, order, line, start_date, context=context)
+        dt_startdate = datetime.strptime(start_date, DEFAULT_SERVER_DATE_FORMAT)
+        date_planned = dt_startdate +  relativedelta(list(rrule(DAILY, count=1+(line.delay or 0.0), byweekday=(MO,TU,WE,TH,FR)))[-1], dt_startdate)
+        date_planned = (date_planned - timedelta(days=order.company_id.security_lead)).strftime(DEFAULT_SERVER_DATETIME_FORMAT)
+        result = date_planned
         if order.requested_date and (order.requested_date > result):
             result = order.requested_date        
         return result
@@ -502,23 +637,16 @@ class sale_order(osv.osv):
 
     _inherit = "sale.order"
     _columns = {
+        'partner_shipping_id': fields.many2one('res.partner.address', 'Shipping Address', readonly=True, required=True, states={'draft': [('readonly', False)], 'manual':[('readonly',False)]}, help="Shipping address for current sales order."),
         'abstract_line_ids': fields.one2many('sale.order.line', 'order_id', 'Order Lines', readonly=True, states={'draft': [('readonly', False)]}),
         'order_line': one2many_mod2('sale.order.line', 'order_id', 'Order Lines', readonly=True, states={'draft': [('readonly', False)]}),
-#        'margin': fields.function(_product_margin, string='Margin', help="It gives profitability by calculating the difference between the Unit Price and Cost Price.", store={
-#                'sale.order.line': (_get_order, [], 20),
-#                'sale.order': (lambda self, cr, uid, ids, c={}: ids, ['order_line'], 20),
-#                'sale.order': (lambda self, cr, uid, ids, c={}: ids, ['abstract_line_ids'], 20),
-#                'sale.order': (lambda self, cr, uid, ids, c={}: ids, ['summary'], 10),
-#                }),
         'margin': fields.function(_product_margin, string='Margin', help="It gives profitability by calculating the difference between the Unit Price and Cost Price.", store={}),
         'summary': fields.char('Summary', size=64),
         'validity': fields.integer('Validity period', readonly=True, states={'draft': [('readonly', False)]}, help="Validity delay in day(s)"),
         'warranty': fields.integer('Warranty period', readonly=True, states={'draft': [('readonly', False)]}, help="Warranty delay in year(s)"),
         'validity_date': fields.function(_get_validity_date, store=True, type='date', string='Validity Date', help="Date of validity"),
-#        'partner_shipping': fields.function(_get_partner_shipping, store=False, type='char', string="Partner"),
         'partner_delivery_id': fields.related('partner_shipping_id', 'partner_id', type='many2one', relation='res.partner', string='Alloted'),
         'partner_delivery_city_id': fields.related('partner_shipping_id', 'city_id', type='many2one', relation='city.city', string='City'),
-#        'partner_delivery_city': fields.related('partner_shipping_id', 'city', type='char', string='City'),
         'picked_in_rate': fields.function(_picked_in_rate, string='Received', type='float'),
         'invoice_ids': fields.many2many('account.invoice', 'sale_order_invoice_rel', 'order_id', 'invoice_id', 'Invoices', readonly=False, help="This is the list of invoices that have been generated for this sales order. The same sales order may have been invoiced in several times (by line for example)."),
         'picking_ids': fields.one2many('stock.picking', 'sale_id', 'Related Picking', readonly=False, help="This is a list of picking that has been generated for this sales order."),
@@ -528,6 +656,12 @@ class sale_order(osv.osv):
         'validity': 30,
         'warranty': 1,
     }
+    
+    # Override default
+    def action_ship_create(self, cr, uid, ids, context=None):
+        for order in self.browse(cr, uid, ids, context=context):
+            self._create_pickings_and_procurements(cr, uid, order, order.order_line, None, context=context)
+        return True
     
     def action_wait(self, cr, uid, ids, context=None):
         
@@ -565,8 +699,22 @@ class sale_order(osv.osv):
             raise osv.except_osv(_('Error !'), msg)
             
         return super(sale_order, self).action_wait(cr, uid, ids, context);
-
-sale_order()
+    
+    def action_cancel(self, cr, uid, ids, context=None):
+        if context is None:
+            context = {}
+        picking_obj = self.pool.get('stock.picking')
+        picking_ids = []
+        for sale in self.browse(cr, uid, ids, context=context):
+            for pick in sale.picking_ids:
+                if pick.state not in ('draft', 'cancel'):
+                    picking_ids.append(pick.id)
+                    
+        if picking_ids:
+            picking_obj.action_cancel(cr, uid, picking_ids, context=context)
+                    
+        return super(sale_order, self).action_cancel(cr, uid, ids, context=context)
+    
 
 class stock_picking(osv.osv):
     _inherit = "stock.picking"
