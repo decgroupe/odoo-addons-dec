e7513d14ff0fef11f8efe56f6d15320a6afd6e04
decindus <decindus@local.dec-industrie.com>
2019-01-29 17:52:29 +0100

    Label with QRCode

--- dec_product/report/label_parser.py
+++ dec_product/report/label_parser.py
@@ -3,20 +3,33 @@
 #
 ##############################################################################
 
+import json
 from report import report_sxw
 from report.report_sxw import rml_parse
+
 
 class Parser(report_sxw.rml_parse):
     def __init__(self, cr, uid, name, context):
         super(Parser, self).__init__(cr, uid, name, context)
         self.localcontext.update({
             'manufacturer': self.get_manufacturer,
-            'hello_world':self.hello_world,
+            'hello_world': self.hello_world,
+            'get_simple_json': self.get_simple_json,
         })
         self.context = context
 
     def hello_world(self, name):
         return "Hello, %s!" % name
+
+    def get_simple_json(self, product):
+        res = {
+            "id": product.id,
+            "code": product.default_code,
+            "categ": {
+                "id": product.categ_id.id,
+            }
+        }
+        return json.dumps(res)
 
     def get_manufacturer(self, product):
         res = ''
@@ -28,7 +41,6 @@ class Parser(report_sxw.rml_parse):
             if categ_id <> product.categ_id:
                 res = ('%s - %s') % (categ_id.name, product.categ_id.name)
             else:
-                res = ('%s') % (categ_id.name) 
+                res = ('%s') % (categ_id.name)
 
         return res
-
