f6f046ee51ccbe459525c5e9bb9957bc5393aeac
yann-papouin <y.papouin@dec-industrie.com>
2019-02-28 17:10:36 +0100

    Fix imports

--- partner_academy/__openerp__.py
+++ partner_academy/__openerp__.py
@@ -1,38 +1,18 @@
 # -*- encoding: utf-8 -*-
-##############################################################################
-#    
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.     
-#
-##############################################################################
 
 {
-    "name" : "Partner academy",
-    "version" : "8.0.1.0",
-    "author" : "Yann Papouin",
-    "website" : "http://www.dec-industrie.com",
-    "category" : "",
+    "name": "Partner academy",
+    "version": "8.0.1.0",
+    "author": "Yann Papouin",
+    "website": "http://www.dec-industrie.com",
+    "category": "",
     "description": """
     
                     """,
-    "depends" : ["base"],
-    "init_xml" : [ ],
-    "demo_xml" : [ ],
-    "update_xml" : ['views/partner_view.xml'],
+    "depends": ["base"],
+    "init_xml": [],
+    "demo_xml": [],
+    "update_xml": ['views/partner_view.xml'],
     "active": False,
     "installable": True
 }
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
