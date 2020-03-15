e72f76e2eefac1ea0e9ad9563654b25fe4e42700
Yann Papouin <y.papouin@dec-industrie.com>
2020-03-15 17:59:44 +0100

    tagging module hierarchy reorganized

---
+++ tagging/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Tagging',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'http://www.dec-industrie.com',
+    'summary': '''Tagging support for every models''',
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'data/tagging_view.xml',
+    ],
+    'installable': True
+}
