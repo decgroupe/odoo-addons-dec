1b1912ee07559d255dab26cd37cfa64a6b48d1ba
Yann Papouin <y.papouin@dec-industrie.com>
2020-03-16 10:29:12 +0100

    Common software module added

--- software/__manifest__.py
+++ software/__manifest__.py
@@ -1,22 +1,16 @@
 {
-    'name': 'Software license',
+    'name': 'Software',
     'version': '12.0.1.0.0',
     'author': 'DEC, Yann Papouin',
     'website': 'http://www.dec-industrie.com',
-    'summary': '''Manage software licenses (serial, dongle)''',
+    'summary': '''Software user groups''',
     'depends': [
-        'base',
-        'product',
-        'mrp',
-        'software',
+        'dec',
     ],
     #'force_migration':'12.0.0.0.0',
     'data':
         [
             'security/model_security.xml',
-            'security/ir.model.access.csv',
-            'views/software_license.xml',
-            'views/software_license_application.xml',
             'views/menu.xml',
         ],
     'installable': True
