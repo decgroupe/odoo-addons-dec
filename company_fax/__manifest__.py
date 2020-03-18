1d1984f797dbe49b16c6051028461483b9056ec0
Yann Papouin <y.papouin@dec-industrie.com>
2020-03-18 15:20:30 +0100

    company_fax module added

---
+++ company_fax/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Company fax',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'http://www.dec-industrie.com',
+    'summary': '''Add fax number on company''',
+    'depends': [
+        'partner_fax',
+    ],
+    'data': [
+        'views/res_company.xml',
+    ],
+    'installable': True
+}
