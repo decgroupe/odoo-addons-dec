e227a53ced9b1d1030523025c4a6055ffd420725
Yann Papouin <y.papouin@dec-industrie.com>
2020-03-23 18:32:34 +0100

    Remove old modules since code base is very different

--- dec_account_invoice_layout/report/generate_from_sxw.sh
+++
@@ -1,1 +0,0 @@
-python /media/raid2to/decindus/tools/openerp-6.1/addons/base_report_designer/openerp_sxw2rml/openerp_sxw2rml.py report_account_invoice_layout.sxw > report_account_invoice_layout.rml
--- dec_stock/report/generate_from_sxw.sh
+++
@@ -1,2 +0,0 @@
-python /media/raid2to/decindus/tools/openerp-6.1/addons/base_report_designer/openerp_sxw2rml/openerp_sxw2rml.py delivery_order.sxw > delivery_order.rml
-python /media/raid2to/decindus/tools/openerp-6.1/addons/base_report_designer/openerp_sxw2rml/openerp_sxw2rml.py picking_dec.sxw > picking_dec.rml
