4469c87bf62106e06c7a6bdf086d9f02452cc7b3
Yann Papouin <y.papouin@dec-industrie.com>
2020-03-23 18:23:39 +0100

    Transition module to re-use existing product_pack

---
+++ product_pack_transition/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Product Pack (transition)',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'http://www.dec-industrie.com',
+    'summary': '''Product pack backported model for transition:
+- Rename product.pack.saleline to product.pack.line
+    
+''',
+    'depends': [
+        'product_pack',
+    ],
+    'force_migration':'12.0.0.0.0',
+    'data': [
+
+    ],
+    'installable': True
+}
---
+++ product_pack_transition/migrations/12.0.1.0.0/pre-migration.py
@@ -0,0 +1,29 @@
+from openupgradelib import openupgrade
+
+_table_renames = [
+    ('product_pack_saleline', 'product_pack_line'),
+]
+
+@openupgrade.migrate()
+def migrate(env, version):
+    cr = env.cr
+    if openupgrade.table_exists(cr, 'product_pack_saleline'):
+        if openupgrade.table_exists(cr, 'product_pack_line'):
+            cr.execute("DROP TABLE product_pack_line;")
+        openupgrade.rename_tables(cr, _table_renames)
+
+    if openupgrade.table_exists(cr, 'product_pack_purchaseline'):
+        cr.execute(
+            """
+            INSERT INTO product_pack_line
+            (
+                create_uid, create_date, write_date, write_uid, 
+                quantity, product_id, parent_product_id
+            )
+            SELECT 
+                create_uid, create_date, write_date, 
+                write_uid, quantity, product_id, parent_product_id
+            FROM 
+                product_pack_purchaseline;
+            """)
+        cr.execute("DROP TABLE product_pack_line;")
