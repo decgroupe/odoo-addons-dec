536ac27ad3c5544d2420a000b052bf7f80fe76a8
Yann Papouin <y.papouin@dec-industrie.com>
2020-03-29 16:51:41 +0200

    New rack,row,case fields for product (6.1 forward port)

---
+++ product_location/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Product Location',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'http://www.dec-industrie.com',
+    'summary': '''Add (Rack,Row,Case) fields to help locating a product in Warehouse''',
+    'depends': ['product', ],
+    'data': [
+        'views/product_template.xml',
+    ],
+    'installable': True
+}
---
+++ product_location/models/product_template.py
@@ -0,0 +1,24 @@
+# Copyright (C) DEC SARL, Inc - All Rights Reserved.
+#
+# CONFIDENTIAL NOTICE: Unauthorized copying and/or use of this file,
+# via any medium is strictly prohibited.
+# All information contained herein is, and remains the property of
+# DEC SARL and its suppliers, if any.
+# The intellectual and technical concepts contained herein are
+# proprietary to DEC SARL and its suppliers and may be covered by
+# French Law and Foreign Patents, patents in process, and are
+# protected by trade secret or copyright law.
+# Dissemination of this information or reproduction of this material
+# is strictly forbidden unless prior written permission is obtained
+# from DEC SARL.
+# Written by Yann Papouin <y.papouin@dec-industrie.com>, Mar 2020
+
+from odoo import fields, models
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    loc_rack = fields.Char('Rack', size=16)
+    loc_row = fields.Char('Row', size=16)
+    loc_case = fields.Char('Case', size=16)
