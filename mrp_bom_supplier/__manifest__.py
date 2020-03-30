a48cac0852c978fb1f6326cff285ecf78b6d8ca2
Yann Papouin <y.papouin@dec-industrie.com>
2020-03-31 01:05:42 +0200

    New module for BoM

---
+++ mrp_bom_supplier/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Manufacturing (BoM supplier)',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'http://www.dec-industrie.com',
+    'summary': '''Add extra fields on BoM''',
+    'depends': [
+        'mrp',
+    ],
+    'data': [
+        'views/mrp_bom.xml',
+    ],
+    'installable': True
+}
