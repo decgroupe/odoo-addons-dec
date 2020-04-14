33165f3677254d5ac92551eb6948b88c428b4a6c
Yann Papouin <y.papouin@dec-industrie.com>
2020-04-14 12:21:48 +0200

    New module to search DEC ones

---
+++ base_module_search/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Module search DEC filter',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'http://www.dec-industrie.com',
+    'summary': '''Add built-in search for modules with autors credited to DEC''',
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'views/ir_module.xml',
+    ],
+    'installable': True
+}
