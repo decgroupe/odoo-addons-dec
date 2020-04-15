2786c4bbce484f86ba76599174de1b34a136cc93
Yann Papouin <y.papouin@dec-industrie.com>
2020-04-15 14:09:51 +0200

    New timeline module for manufaturing

---
+++ mrp_timeline/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Manufacturing timeline',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'http://www.dec-industrie.com',
+    'summary': "Timeline view for production",
+    'depends': [
+        'mrp',
+        'web_timeline',
+    ],
+    "data": [
+        "templates/assets.xml",
+        "views/mrp_production.xml",
+    ],
+    'installable': True
+}
