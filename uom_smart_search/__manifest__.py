5145d97cc0532802d475e671d20844b74fa2205d
Yann Papouin <y.papouin@dec-industrie.com>
2020-04-16 14:45:03 +0200

    New module to find UoM easilly

---
+++ uom_smart_search/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'UoM Smart Search',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'http://www.dec-industrie.com',
+    'summary': "Override name_search to find units starting with search value",
+    'depends': [
+        'uom',
+    ],
+    "data": [
+    ],
+    'installable': True
+}
---
+++ uom_smart_search/models/uom_uom.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) DEC SARL, Inc - All Rights Reserved.
+#
+# CONFIDENTIAL NOTICE: Unauthorized copying and/or use of this file,
+# via any medium is strictly prohibited.
+# All information contained herein is, and remains the property of
+# DEC SARL and its suppliers, if any.
+# The intellectual and technical concepts contained herein are
+# proprietary to DEC SARL and its suppliers and may be covered by
+# French Law and Foreign Patents, patents in process, and are
+# protected by trade secret or copyright law.
+# Dissemination of this information or reproduction of this material
+# is strictly forbidden unless prior written permission is obtained
+# from DEC SARL.
+# Written by Yann Papouin <y.papouin@dec-industrie.com>, Apr 2020
+
+from odoo import api, models
+
+
+class UoM(models.Model):
+    _inherit = 'uom.uom'
+
+    @api.model
+    def _name_search(
+        self, name, args=None, operator='ilike', limit=100, name_get_uid=None
+    ):
+        if not args:
+            args = []
+
+        uom_ids = []
+        if name:
+            positive_operators = ['=', 'ilike', '=ilike', 'like', '=like']
+            if operator in positive_operators:
+                uom_ids = self._search(
+                    args + [('name', '=ilike', name)],
+                    limit=limit,
+                    access_rights_uid=name_get_uid,
+                )
+
+            if not limit or len(uom_ids) < limit:
+                limit2 = (limit - len(uom_ids)) if limit else False
+                uom2_ids = self._search(
+                    args + [
+                        ('name', '=ilike', name + '%'),
+                        ('id', 'not in', uom_ids),
+                    ],
+                    limit=limit2,
+                    access_rights_uid=name_get_uid,
+                )
+                uom_ids.extend(uom2_ids)
+
+            if not limit or len(uom_ids) < limit:
+                limit3 = (limit - len(uom_ids)) if limit else False
+                uom3_ids = self._search(
+                    args + [
+                        ('name', operator, name),
+                        ('id', 'not in', uom_ids),
+                    ],
+                    limit=limit3,
+                    access_rights_uid=name_get_uid,
+                )
+                uom_ids.extend(uom3_ids)
+        else:
+            uom_ids = self._search(
+                args,
+                limit=limit,
+                access_rights_uid=name_get_uid,
+            )
+
+        return self.browse(uom_ids).name_get()
