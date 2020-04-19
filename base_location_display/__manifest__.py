ac2b62097746ae18088d03615a2d7471eade8ab0
Yann Papouin <y.papouin@dec-industrie.com>
2020-04-19 19:30:17 +0200

    Override zip display name

---
+++ base_location_display/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Location management (l10n_fr)',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'http://www.dec-industrie.com',
+    'summary': '''Customized display name for zips''',
+    'depends': [
+        'base_location',
+    ],
+    'data': [
+    ],
+    'installable': True
+}
---
+++ base_location_display/models/res_city_zip.py
@@ -0,0 +1,37 @@
+# Copyright (C) DEC SARL, Inc - All Rights Reserved.
+#
+# CONFIDENTIAL NOTICE: Unauthorized copying and/or use of this file,
+# via any medium is strictly prohibited.
+# All information contained herein is, and remains the property of
+# DEC SARL and its suppliers, if any.
+# The intellectual and technical concepts contained herein are
+# proprietary to DEC SARL and its suppliers and may be covered by
+# French Law and Foreign Patents, patents in process, and are
+# protected by trade secret or copyright law.
+# Dissemination of this information or reproduction of this material
+# is strictly forbidden unless prior written permission is obtained
+# from DEC SARL.
+# Written by Yann Papouin <y.papouin@dec-industrie.com>, Mar 2020
+
+from odoo import fields, models, api
+
+
+class ResCityZip(models.Model):
+    _inherit = "res.city.zip"
+
+    display_name = fields.Char(
+        compute='_compute_new_display_name',
+        store=True,
+        index=True,
+    )
+
+    @api.multi
+    @api.depends('name', 'city_id')
+    def _compute_new_display_name(self):
+        for rec in self:
+            name = ["{} {}".format(rec.name, rec.city_id.name)]
+            if rec.city_id.state_id:
+                name.append(rec.city_id.state_id.name)
+            if rec.city_id.country_id:
+                name.append(rec.city_id.country_id.name)
+            rec.display_name = ", ".join(name)
