c8d7d2b211ebc37793b906adc2e274cb575efa0f
Yann Papouin <y.papouin@dec-industrie.com>
2020-04-25 12:22:10 +0200

    Filter moves to write

--- purchase_stock_cancel/models/purchase_order_line.py
+++ purchase_stock_cancel/models/purchase_order_line.py
@@ -23,12 +23,22 @@ class PurchaseOrderLine(models.Model):
     def unlink(self):
         for line in self:
             if line.move_dest_ids:
-                move_dest_ids = line.move_dest_ids
-                line.move_dest_ids.write(
-                    {
-                        'procure_method': 'make_to_stock',
-                        'created_purchase_line_id': False,
-                    }
+                # We need to unlink this line from its destination moves to
+                # avoid inconsistent data.
+                # Note that Purchase Order cancelation does the same thing in
+                # button_cancel()
+                # addons/purchase_stock/models/purchase.py
+                move_dest_ids = line.move_dest_ids.filtered(
+                    lambda m: m.state not in ('done', 'cancel')
                 )
-                move_dest_ids._recompute_state()
+                move_orig_ids = move_dest_ids.mapped('move_orig_ids')
+                siblings_states = move_orig_ids.mapped('state')
+                if all(s in ('done', 'cancel') for s in siblings_states):
+                    move_dest_ids.write(
+                        {
+                            'procure_method': 'make_to_stock',
+                            'created_purchase_line_id': False,
+                        }
+                    )
+                    move_dest_ids._recompute_state()
         return super(PurchaseOrderLine, self).unlink()
