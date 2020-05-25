fae400c6bc41f9af03d26ac63da2775d93f8c020
Yann Papouin <y.papouin@dec-industrie.com>
2020-05-25 14:33:07 +0200

    New module to make translation search easier

---
+++ base_translation/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Translation improvements',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'http://www.dec-industrie.com',
+    'summary': '''Better translation search''',
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'views/ir_translation.xml',
+    ],
+    'installable': True
+}
