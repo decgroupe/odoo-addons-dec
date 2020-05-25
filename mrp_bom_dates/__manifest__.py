6c7b331c82f6d0ddd63ab0fe7a96cfb86c719292
Yann Papouin <y.papouin@dec-industrie.com>
2020-05-25 14:31:49 +0200

    New module to re-add legacy date fields to BoM

---
+++ mrp_bom_dates/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Manufacturing (BoM dates)',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'http://www.dec-industrie.com',
+    'summary': '''Add date fields on BoM''',
+    'depends': [
+        'mrp',
+    ],
+    'data': [
+        'views/mrp_bom.xml',
+    ],
+    'installable': True
+}
