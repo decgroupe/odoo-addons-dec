dcd8f243d55580dfcc3fba392ce09e5e1f40cccd
Yann Papouin <y.papouin@dec-industrie.com>
2020-05-26 12:29:23 +0200

    New pricelist analysis module to improve usability

---
+++ product_pricelist_analysis/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Product pricelist analysis',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'http://www.dec-industrie.com',
+    'summary': '''Show all pricelist items''',
+    'depends': [
+        'product',
+        'product_pricelist',
+    ],
+    'data':
+        [
+            'views/product_pricelist_item.xml',
+            'views/product_pricelist.xml',
+            'views/menu.xml',
+        ],
+    'installable': True
+}
