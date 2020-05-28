ccdb352f0381a1a2119d4c491778cad06dafd68d
Yann Papouin <y.papouin@dec-industrie.com>
2020-05-28 09:13:13 +0200

    Add qauntity UoM separator in supplier info view

---
+++ product_supplierinfo/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Product Supplier Infos',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'http://www.dec-industrie.com',
+    'summary': '''View Improvements''',
+    'depends': [
+        'product',
+    ],
+    'data':
+        [
+            'views/product_supplierinfo.xml',
+        ],
+    'installable': True
+}
