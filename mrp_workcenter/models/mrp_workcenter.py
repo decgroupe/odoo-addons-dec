51d2c82830430508d847094a97efcb7fa3d62080
Yann Papouin <y.papouin@dec-industrie.com>
2020-07-06 16:54:46 +0200

    Override name with user_id.name when necessary

--- mrp_workcenter/models/mrp_workcenter.py
+++ mrp_workcenter/models/mrp_workcenter.py
@@ -20,6 +20,11 @@ from odoo import api, fields, models, _
 class MrpWorkcenter(models.Model):
     _inherit = 'mrp.workcenter'
 
+    user_id = fields.Many2one(
+        'res.users',
+        string='Mapped to',
+    )
+
     production_ids = fields.One2many(
         'mrp.production',
         'assigned_workcenter_id',
@@ -38,3 +43,22 @@ class MrpWorkcenter(models.Model):
     def _compute_production_order(self):
         for workcenter in self:
             workcenter.production_count = len(workcenter.production_ids)
+
+    @api.multi
+    def write(self, vals):
+        if vals.get('user_id') and not 'name' in vals:
+            Users = self.env['res.users']
+            for workcenter in self:
+                user_name = Users.browse(vals['user_id']).name
+                if workcenter.name != Users.browse(vals['user_id']).name:
+                    workcenter.name = user_name
+        res = super().write(vals)
+        return res
+
+    @api.onchange('user_id')
+    def onchange_user_id(self):
+        if self.user_id:
+            values = {
+                'name': self.user_id.name,
+            }
+            self.update(values)
