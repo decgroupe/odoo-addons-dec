6f76750ff73b93091f8a79fe2341b32318042fa6
Yann Papouin <y.papouin@dec-industrie.com>
2020-07-06 10:01:19 +0200

    Fix exception with empty BoM

--- mrp_traceability/models/mrp_production.py
+++ mrp_traceability/models/mrp_production.py
@@ -19,6 +19,7 @@ from odoo import api, fields, models, _
 
 _logger = logging.getLogger(__name__)
 
+
 class MrpProduction(models.Model):
     _inherit = 'mrp.production'
 
@@ -32,27 +33,29 @@ class MrpProduction(models.Model):
             )
             consu_move_ids._action_assign()
 
-
     def _generate_raw_moves(self, exploded_lines):
         moves = super(MrpProduction, self)._generate_raw_moves(exploded_lines)
         return moves
 
     def _generate_raw_move(self, bom_line, line_data):
-        move = super(MrpProduction, self)._generate_raw_move(bom_line, line_data)
+        move = super(MrpProduction,
+                     self)._generate_raw_move(bom_line, line_data)
         return move
 
     def _get_raw_move_data(self, bom_line, line_data):
         res = super(MrpProduction, self)._get_raw_move_data(bom_line, line_data)
-        # Override source location in product is consumable
-        if bom_line.product_id.type == 'consu':
-            # Copy destination to source location to create a fake
-            # move that will stay in Warehouse/Production location
-            location = self.env['stock.location'].browse(
-                res['location_dest_id']
-            )
-            res['location_id'] = location.id
-            res['warehouse_id'] = location.get_warehouse().id
-        # Link move destination to production moves to-do, this hook is
-        # only done to keep REF Manager stock.move compatibility with legacy
-        res['move_dest_ids'] = [(6, 0, self.move_finished_ids.ids)]
+        if res is not None:
+            # Override source location in product is consumable
+            if bom_line.product_id.type == 'consu':
+                # Copy destination to source location to create a fake
+                # move that will stay in Warehouse/Production location
+                location = self.env['stock.location'].browse(
+                    res['location_dest_id']
+                )
+                res['location_id'] = location.id
+                res['warehouse_id'] = location.get_warehouse().id
+            # Link move destination to production moves to-do, this hook is
+            # only done to keep REF Manager stock.move compatibility with
+            # legacy
+            res['move_dest_ids'] = [(6, 0, self.move_finished_ids.ids)]
         return res
