9697cab1e5c6387607093ed9126e924a23bce193
Yann Papouin <y.papouin@dec-industrie.com>
2020-07-06 13:09:04 +0200

    Internal notes field added to production order as a module

---
+++ mrp_note/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Manufacturing (notes)',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'http://www.dec-industrie.com',
+    'summary': '''Add an internal notes field''',
+    'depends': [
+        'mrp',
+    ],
+    'data': [
+        'views/mrp_production.xml',
+    ],
+    'installable': True
+}
