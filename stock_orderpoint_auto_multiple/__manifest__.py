c112607f54214a577f2a3cd1f2a0091dfbe0d2a7
Yann Papouin <y.papouin@dec-industrie.com>
2020-07-12 22:55:08 +0200

    New module that will set qty_multiple from product purchase UoM

---
+++ stock_orderpoint_auto_multiple/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Orderpoint auto-multiple',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'http://www.dec-industrie.com',
+    'summary': '''Multiple will be computed from product purchase UoM factor''',
+    'depends': ['stock'],
+    #'force_migration':'12.0.0.0.0',
+    'data': [],
+    'installable': True
+}
---
+++ stock_orderpoint_auto_multiple/models/stock_warehouse_orderpoint.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) DEC SARL, Inc - All Rights Reserved.
+#
+# CONFIDENTIAL NOTICE: Unauthorized copying and/or use of this file,
+# via any medium is strictly prohibited.
+# All information contained herein is, and remains the property of
+# DEC SARL and its suppliers, if any.
+# The intellectual and technical concepts contained herein are
+# proprietary to DEC SARL and its suppliers and may be covered by
+# French Law and Foreign Patents, patents in process, and are
+# protected by trade secret or copyright law.
+# Dissemination of this information or reproduction of this material
+# is strictly forbidden unless prior written permission is obtained
+# from DEC SARL.
+# Written by Yann Papouin <y.papouin@dec-industrie.com>, Jul 2020
+
+from odoo import api, fields, models
+
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
+class Orderpoint(models.Model):
+    _inherit = "stock.warehouse.orderpoint"
+
+    qty_multiple = fields.Float(
+        compute="_compute_qty_multiple",
+        help="This value is automatically computed from the product purchase UoM",
+        required=False,
+    )
+
+    @api.multi
+    @api.depends('product_id.uom_po_id')
+    def _compute_qty_multiple(self):
+        for op in self:
+            op.qty_multiple = op.product_id.uom_po_id.factor_inv
