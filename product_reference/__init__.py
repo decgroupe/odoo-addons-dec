4c19c0f7b9f3ef3f4587a55b3478e93ff0080784
Yann Papouin <y.papouin@dec-industrie.com>
2020-07-22 23:14:08 +0200

    Missing refactoring on product field name

--- product_reference/__init__.py
+++ product_reference/__init__.py
@@ -5,7 +5,7 @@ from odoo import api, SUPERUSER_ID
 
 def set_mrp_production_request(env):
     for ref in env['ref.reference'].search([]):
-        ref.product.mrp_production_request = True
+        ref.product_id.mrp_production_request = True
 
 
 def post_init(cr, registry):
