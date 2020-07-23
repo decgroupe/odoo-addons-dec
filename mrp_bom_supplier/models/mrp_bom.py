04ab2dd95a9def92f4f09ade3e07488894e366d0
Yann Papouin <y.papouin@dec-industrie.com>
2020-07-23 13:41:35 +0200

    Smart search support added to BoM

--- mrp_bom_supplier/models/mrp_bom.py
+++ mrp_bom_supplier/models/mrp_bom.py
@@ -2,8 +2,51 @@
 # Copyright (C) DEC SARL, Inc - All Rights Reserved.
 # Written by Yann Papouin <y.papouin at dec-industrie.com>, Mar 2020
 
-from odoo import fields, models
+from odoo import fields, models, api
 
 
 class MrpBom(models.Model):
     _inherit = 'mrp.bom'
+
+    @api.model
+    def name_search(self, name, args=None, operator="ilike", limit=100):
+        if args is None:
+            args = []
+        
+        bom_ids = []
+        if name:
+            positive_operators = ['=', 'ilike', '=ilike', 'like', '=like']
+            if operator in positive_operators:
+                bom_ids = self._search(
+                    args + [('code', '=ilike', name)],
+                    limit=limit,
+                )
+
+            if not limit or len(bom_ids) < limit:
+                limit2 = (limit - len(bom_ids)) if limit else False
+                bom2_ids = self._search(
+                    args + [
+                        ('code', '=ilike', name + '%'),
+                        ('id', 'not in', bom_ids),
+                    ],
+                    limit=limit2,
+                )
+                bom_ids.extend(bom2_ids)
+
+            if not limit or len(bom_ids) < limit:
+                limit3 = (limit - len(bom_ids)) if limit else False
+                bom3_ids = self._search(
+                    args + [
+                        ('code', operator, name),
+                        ('id', 'not in', bom_ids),
+                    ],
+                    limit=limit3,
+                )
+                bom_ids.extend(bom3_ids)
+        else:
+            bom_ids = self._search(
+                args,
+                limit=limit,
+            )
+
+        return self.browse(bom_ids).name_get()
