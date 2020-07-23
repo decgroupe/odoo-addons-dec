0cefbc4a7a7a6d49bc85b3b1a325a97aea894afc
Yann Papouin <y.papouin@dec-industrie.com>
2020-07-23 16:57:22 +0200

    New action to create a quotation from ticket

---
+++ helpdesk_sale/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Helpdesk Sale',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'http://www.dec-industrie.com',
+    'summary': '''Create sale order from Ticket''',
+    'depends': [
+        'helpdesk_mgmt',
+        'helpdesk_references',
+        'sale_summary',
+    ],
+    'force_migration':'12.0.0.0.0',
+    'data': [
+        "views/helpdesk_ticket.xml",
+    ],
+    'installable': True
+}
---
+++ helpdesk_sale/models/helpdesk_ticket.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) DEC SARL, Inc - All Rights Reserved.
+# Written by Yann Papouin <y.papouin at dec-industrie.com>, Jul 2020
+
+from odoo import _, api, models, fields
+from werkzeug import url_encode
+
+
+class HelpdeskTicket(models.Model):
+    _inherit = 'helpdesk.ticket'
+
+    @api.multi
+    def close(self):
+        stage_done = self.env.ref('helpdesk_mgmt.helpdesk_ticket_stage_done')
+        for ticket in self:
+            ticket.stage_id = stage_done
+
+    @api.multi
+    def action_create_quotation(self):
+        ticket_action = self.env.ref('helpdesk_mgmt.helpdesk_ticket_action')
+        quot_action = self.env.ref('sale.action_quotations_with_onboarding')
+        for ticket in self:
+            data = {
+                'summary':
+                    _('Case %s: %s') % (ticket.number, ticket.name),
+                'origin':
+                    ticket.number,
+                'partner_id':
+                    ticket.partner_id and ticket.partner_id.id or False,
+                'date_order':
+                    fields.Date.today(),
+            }
+            order = self.env['sale.order'].create(data)
+
+            # Create a ref to sale_order to ticket references
+            if order:
+                data = {
+                    'ticket_id': ticket.id,
+                    'model_ref_id': 'sale.order,{}'.format(order.id),
+                }
+                self.env['helpdesk.ticket.reference'].create(data)
+
+            # Post a note with a reference to the ticket
+            body = (
+                _("Created from helpdesk ticket <a href='web#%s'>%s</a>") % (
+                    url_encode(
+                        {
+                            'id': ticket.id,
+                            'model': 'helpdesk.ticket',
+                            'action': ticket_action.id,
+                            'view_type': 'form',
+                        }
+                    ), ticket.number
+                )
+            )
+            order.message_post(body=body)
+
+            # Post a note with a reference to the quotation
+            body = (
+                _("New quotation <a href='web#%s'>%s</a> created") % (
+                    url_encode(
+                        {
+                            'id': order.id,
+                            'model': 'sale.order',
+                            'action': quot_action.id,
+                            'view_type': 'form',
+                        }
+                    ), order.name
+                )
+            )
+            ticket.message_post(body=body)
+            # Close ticket
+            ticket.close()
