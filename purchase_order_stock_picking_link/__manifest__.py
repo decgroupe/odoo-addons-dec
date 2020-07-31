4d861a048cfb690f238a8553279e48eac62531da
Yann Papouin <y.papouin@dec-industrie.com>
2020-07-31 11:47:26 +0200

    New module to show outgoing pickings from purchase form

---
+++ purchase_order_stock_picking_link/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Purchase Picking Link',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'http://www.dec-industrie.com',
+    'summary': '''Show related outgoing pickings on purchase form''',
+    'depends': [
+        'purchase_stock',
+    ],
+    #'force_migration':'12.0.0.0.0',
+    'data':
+        [
+            'views/purchase_order.xml',
+        ],
+    'installable': True
+}
