7f2eedd535eadc5bbe99cc8cfe52d7fe5c539a1a
Yann Papouin <y.papouin@dec-industrie.com>
2020-07-31 06:44:32 +0200

    Show linked sale line when purchasing services

--- purchase_traceability/__manifest__.py
+++ purchase_traceability/__manifest__.py
@@ -3,9 +3,10 @@
     'version': '12.0.1.0.0',
     'author': 'DEC, Yann Papouin',
     'website': 'http://www.dec-industrie.com',
-    'summary': '''Show related orderpoint on purchase form''',
+    'summary': '''Show related sale lines on purchase form''',
     'depends': [
-        'purchase'
+        'purchase',
+        'sale_purchase',
     ],
     #'force_migration':'12.0.0.0.0',
     'data':
