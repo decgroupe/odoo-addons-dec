558b4030f9e0486c4c92d8d7b6fe8165e766a7d7
Yann Papouin <y.papouin@dec-industrie.com>
2020-08-05 23:28:25 +0200

    Use same sequence for MR than MO

--- mrp_production_request_procurement/models/mrp_production_request.py
+++ mrp_production_request_procurement/models/mrp_production_request.py
@@ -35,8 +35,48 @@ class MrpProductionRequest(models.Model):
 
     @api.model
     def _create_sequence(self, vals):
-        res_vals = super()._create_sequence(vals)
+        # Generate and store a unique name as base for all production orders
         if not vals.get('name') or vals.get('name') == '/':
-            vals['name'] = self.env['ir.sequence'].next_by_code(
-                'mrp.production.request') or '/'
+            if vals.get('picking_type_id'):
+                picking_type_id = self.env['stock.picking.type'].browse(
+                    vals['picking_type_id']
+                )
+            else:
+                picking_type_id = False
+            vals['production_name'] = self._get_production_name(picking_type_id)
+            vals['name'] = vals['production_name'].replace('MO', 'MR')
+        vals = super()._create_sequence(vals)
         return vals
+
+    @api.multi
+    def button_approved(self):
+        res = super().button_approved()
+        for mr in self:
+            if mr.use_common_procurement_group and not mr.common_procurement_group_id:
+                mr.common_procurement_group_id = mr._create_procurement_group()
+        return res
+
+    @api.model
+    def _get_production_name(self, picking_type_id):
+        """This function helps to get a production name like it is done in the
+        original code: addons/mrp/models/mrp_production.py
+        Returns:
+            [string]: Production name
+        """
+        if picking_type_id:
+            res = picking_type_id.sequence_id.next_by_id()
+        else:
+            res = self.env['ir.sequence'].next_by_code('mrp.production')
+        return res
+
+    def _create_procurement_group(self):
+        """This function helps to create a procurement group like it's done
+        in the original code: addons/mrp/models/mrp_production.py
+        Returns:
+            [procurement.group]: [description]
+        """
+        self.ensure_one()
+        procurement_group_id = self.env["procurement.group"].create(
+            {'name': self.production_name}
+        )
+        return procurement_group_id
--- mrp_production_request_procurement/wizard/mrp_production_request_create_mo.py
+++ mrp_production_request_procurement/wizard/mrp_production_request_create_mo.py
@@ -10,52 +10,25 @@ from odoo.exceptions import UserError
 class MrpProductionRequestCreateMo(models.TransientModel):
     _inherit = "mrp.production.request.create.mo"
 
-    def _get_production_name(self):
-        self.ensure_one()
-        """This function helps to get a production name like it is done in the
-        original code: addons/mrp/models/mrp_production.py
-        Returns:
-            [string]: Production name
-        """
-        self.ensure_one()
-        request_id = self.mrp_production_request_id
-        if request_id.picking_type_id:
-            res = request_id.picking_type_id.sequence_id.next_by_id()
-        else:
-            res = self.env['ir.sequence'].next_by_code('mrp.production')
-        return res
-
-    def _create_procurement_group(self):
-        """This function helps to create a procurement group like it's done
-        in the original code: addons/mrp/models/mrp_production.py
-        Returns:
-            [procurement.group]: [description]
-        """
-        self.ensure_one()
-        procurement_group_id = self.env["procurement.group"].create(
-            {'name': self._get_production_name()}
-        )
-        return procurement_group_id
-
     @api.multi
     def _prepare_manufacturing_order(self):
         self.ensure_one()
         res = super()._prepare_manufacturing_order()
         request_id = self.mrp_production_request_id
-        # Generate and store a unique name as base for all production orders
-        if not request_id.production_name:
-            request_id.production_name = self._get_production_name()
+
         if request_id.use_common_procurement_group:
-            if not request_id.common_procurement_group_id:
-                request_id.common_procurement_group_id = self._create_procurement_group()
+            # Assign existing common group
             res['procurement_group_id'] = request_id.common_procurement_group_id.id
         else:
+            # Remove default procurement since using an existing SOxxxx group
+            # can lead to side effects (all pickings cancelled)
+            # https://github.com/OCA/manufacture/issues/516
             res['procurement_group_id'] = False
 
         # Use production_name as prefix to generate a manufacturing order with
         # the same name or with an index if waiting quantity > 1
         if request_id.product_qty > 1 or request_id.manufactured_qty > 0:
-            res['name'] = '{0}/{1:2d}'.format(
+            res['name'] = '{0}/{1:02d}'.format(
                 request_id.production_name,
                 round(request_id.manufactured_qty) + 1,
             )
