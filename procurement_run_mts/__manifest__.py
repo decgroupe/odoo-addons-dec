d97c27b7f0ad144b3059822ca554a8c3674d9cef
Yann Papouin <y.papouin@dec-industrie.com>
2020-08-16 00:34:18 +0200

    New module dedicated to make_to_stock move that needs reordering

---
+++ procurement_run_mts/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Procurement Run MTS',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'http://www.dec-industrie.com',
+    'summary': '''Scheduler task extended to validate make_to_stock moves''',
+    'depends': [
+        'stock',
+        'mrp',
+        'purchase_stock',
+    ],
+    'data':
+        [
+            'views/procurement.xml',
+        ],
+    'installable': True
+}
