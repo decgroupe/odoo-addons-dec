3ecc7071d308ae1993ea71c1625cd2c5c46b9a12
Yann Papouin <y.papouin@dec-industrie.com>
2020-08-16 21:21:36 +0200

    Fix migration step order issue

--- product_pack_transition/migrations/12.0.1.0.0/pre-migration.py
+++ product_pack_transition/migrations/12.0.1.0.0/pre-migration.py
@@ -1,29 +1,32 @@
 from openupgradelib import openupgrade
 
-_table_renames = [
-    ('product_pack_saleline', 'product_pack_line'),
-]
 
-@openupgrade.migrate()
-def migrate(env, version):
+def move_pack_x_line(env, version, table):
     cr = env.cr
-    if openupgrade.table_exists(cr, 'product_pack_saleline'):
-        if openupgrade.table_exists(cr, 'product_pack_line'):
-            cr.execute("DROP TABLE product_pack_line;")
-        openupgrade.rename_tables(cr, _table_renames)
-
-    if openupgrade.table_exists(cr, 'product_pack_purchaseline'):
+    if openupgrade.table_exists(cr, table):
+        openupgrade.logged_query(
+            env.cr, """
+            ALTER TABLE product_pack_line
+            DROP CONSTRAINT IF EXISTS product_pack_line_product_uniq""",
+        )
         cr.execute(
             """
             INSERT INTO product_pack_line
             (
-                create_uid, create_date, write_date, write_uid, 
+                create_uid, create_date, write_date, write_uid,
                 quantity, product_id, parent_product_id
             )
-            SELECT 
-                create_uid, create_date, write_date, 
+            SELECT
+                create_uid, create_date, write_date,
                 write_uid, quantity, product_id, parent_product_id
-            FROM 
-                product_pack_purchaseline;
-            """)
-        cr.execute("DROP TABLE product_pack_purchaseline;")
+            FROM
+                {};
+            """.format(table)
+        )
+        cr.execute("DROP TABLE {};".format(table))
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    move_pack_x_line(env, version, 'product_pack_saleline')
+    move_pack_x_line(env, version, 'product_pack_purchaseline')
