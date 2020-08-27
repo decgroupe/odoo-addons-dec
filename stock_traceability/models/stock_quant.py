1b6a9cb8cfe639ef118fa1fd431929553d3c76ba
Yann Papouin <y.papouin@dec-industrie.com>
2020-08-27 09:44:00 +0200

    Log warning to console on stock.quant reservation issue

---
+++ stock_traceability/models/stock_quant.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) DEC SARL, Inc - All Rights Reserved.
+# Written by Yann Papouin <y.papouin at dec-industrie.com>, Mar 2020
+
+
+from odoo import api, models, _
+from odoo.exceptions import UserError
+
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
+class StockQuant(models.Model):
+    _inherit = 'stock.quant'
+
+    @api.model
+    def _update_reserved_quantity(
+        self,
+        product_id,
+        location_id,
+        quantity,
+        lot_id=None,
+        package_id=None,
+        owner_id=None,
+        strict=False
+    ):
+        reserved_quants = []
+        try:
+            reserved_quants = super()._update_reserved_quantity(
+                product_id, location_id, quantity, lot_id, package_id, owner_id,
+                strict
+            )
+        except UserError as user_error:
+            _logger.warning('%s (%f)', user_error.name, quantity)
+            raise user_error
+        return reserved_quants
