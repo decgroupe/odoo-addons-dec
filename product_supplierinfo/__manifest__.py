bca5f5c1f246a37da30e621758368f55168b4769
Yann Papouin <y.papouin@dec-industrie.com>
2020-08-28 23:12:58 +0200

    xml views renamed with module name

--- product_supplierinfo/__manifest__.py
+++ product_supplierinfo/__manifest__.py
@@ -3,7 +3,7 @@
     'version': '12.0.1.0.0',
     'author': 'DEC, Yann Papouin',
     'website': 'http://www.dec-industrie.com',
-    'summary': '''View Improvements''',
+    'summary': '''Cosmetic Improvements''',
     'depends': [
         'product',
     ],
