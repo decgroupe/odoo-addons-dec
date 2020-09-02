482dea8c5b4fb50d3a3eed6af6527fd7b8bc0911
Yann Papouin <y.papouin@dec-industrie.com>
2020-09-02 18:54:27 +0200

    Use _onchange_origin from framework

--- sale_markup/models/sale_order_line.py
+++ sale_markup/models/sale_order_line.py
@@ -13,14 +13,10 @@ class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
     # taux_marge = marge_commerciale/cout_achat_HT * 100
-    margin_percent = fields.Float(
-        'Margin (%)', digits=(16, 2)
-    )
+    margin_percent = fields.Float('Margin (%)', digits=(16, 2))
 
-     # taux_marque = marge_commerciale/prix_vente_HT * 100
-    markup_percent = fields.Float(
-        'Markup (%)', digits=(16, 2)
-    )
+    # taux_marque = marge_commerciale/prix_vente_HT * 100
+    markup_percent = fields.Float('Markup (%)', digits=(16, 2))
 
     # Remove `price_unit` from @onchange to avoid price bouncing
     # with markup, margin, etc.
@@ -29,56 +25,71 @@ class SaleOrderLine(models.Model):
         previous_discount = self.discount
         super()._onchange_discount()
         if self.discount != previous_discount:
-            _logger.info('_onchange_discount: %f -> %f', previous_discount, self.discount)
-            
+            _logger.info(
+                '_onchange_discount: %f -> %f', previous_discount, self.discount
+            )
 
     @api.onchange('markup_percent')
-    def onchange_markup_percent(self): 
+    def onchange_markup_percent(self):
         vals = {}
         if self.purchase_price > 0 and self.discount < 100:
-            if self.markup_percent < 100: 
-                new_price_unit = self.purchase_price / float(1-self.discount/100.0) / float(1-self.markup_percent/100.0) 
-                new_price_unit = float_round(new_price_unit, precision_digits=2)
+            if self.markup_percent < 100:
+                discount_ratio = 1 - self.discount / 100.0
+                markup_ratio = 1 - self.markup_percent / 100.0
+                price = self.purchase_price / (discount_ratio * markup_ratio)
+                price = float_round(price, precision_digits=2)
             else:
-                new_price_unit = 0
-                
-            if hasattr(self, '_onchange_origin') and \
-                self._onchange_origin in ('discount', 'price_unit'):
+                price = 0
+
+            if self._onchange_origin in ('discount', 'price_unit'):
                 # Do not update `price_unit` when `discount` is at the origin
                 # of the @onchange event to avoid rounding issues
                 # Note that `_onchange_origin` is set from function
                 # `_update_markup_percent`
                 pass
             else:
-                vals.update({'price_unit': new_price_unit})
+                vals.update({'price_unit': price})
         else:
             vals.update({'markup_percent': 0})
-            
+
         if 'markup_percent' in vals:
-            _logger.info('onchange_markup_percent: set markup_percent=%f', vals.get('markup_percent'))
+            _logger.info(
+                'onchange_markup_percent: set markup_percent=%f',
+                vals.get('markup_percent')
+            )
         if 'price_unit' in vals:
-            _logger.info('onchange_markup_percent: set price_unit=%f', vals.get('price_unit'))
+            _logger.info(
+                'onchange_markup_percent: set price_unit=%f',
+                vals.get('price_unit')
+            )
 
         self.update(vals)
 
-    @api.onchange('purchase_price', 'product_uom_qty', 'price_unit', 'discount', 'margin')
+    @api.onchange(
+        'purchase_price',
+        'product_uom_qty',
+        'price_unit',
+        'discount',
+        'margin',
+    )
     def _update_markup_percent(self):
-        # Small hook to store origin if the @onchange event in a python object
-        # attribute that will be automatically put into garbage at the end
-        # of the @onchange propagation
-        if hasattr(self, '_onchange_sender') and not hasattr(self, '_onchange_origin'):
-            self._onchange_origin = self._onchange_sender
-
-        vals = {}
-        if (self.discount < 100) and (self.product_uom_qty > 0) and (self.price_unit > 0):
-            new_markup_percent = self.margin/float(self.product_uom_qty) * 100 / float(self.price_unit - (self.price_unit * self.discount / 100.0))
+        if self.product_uom_qty > 0:
+            margin = self.margin * 100 / self.product_uom_qty
         else:
-            new_markup_percent = 0
+            margin = 0
 
-        if not float_is_zero(new_markup_percent-self.markup_percent, precision_digits=2):
-            _logger.info('_product_markup: set markup_percent=%f (sender=%s)', new_markup_percent, self._onchange_sender)
-            vals.update({'markup_percent': new_markup_percent})
-            self.update(vals)
+        if self.discount < 100 and self.price_unit > 0:
+            price = self.price_unit - (self.price_unit * self.discount / 100.0)
+            markup = margin / price
+        else:
+            markup = 0
+
+        if not float_is_zero(markup - self.markup_percent, precision_digits=2):
+            _logger.info(
+                '_product_markup: set markup_percent=%f (sender=%s)', markup,
+                self._onchange_sender
+            )
+            self.markup_percent = markup
 
     @api.onchange('product_uom', 'product_uom_qty')
     def product_uom_change(self):
