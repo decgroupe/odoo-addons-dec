24a97e35db4485ce287f54a366e4435eccca41ea
Yann Papouin <y.papouin@dec-industrie.com>
2020-09-02 18:23:58 +0200

    Add view_pricelist_items action for product.product

--- product_pricelist_analysis/models/product_product.py
+++ product_pricelist_analysis/models/product_product.py
@@ -1,17 +1,18 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) DEC SARL, Inc - All Rights Reserved.
-# Written by Yann Papouin <y.papouin at dec-industrie.com>, May 2020
+# Written by Yann Papouin <y.papouin at dec-industrie.com>, Sep 2020
 
 from odoo import models
 
 
-class ProductTemplate(models.Model):
-    _inherit = 'product.template'
+class Product(models.Model):
+    _inherit = 'product.product'
 
     def action_view_pricelist_items(self):
         action = self.env.ref(
             'product_pricelist_analysis.act_window_product_pricelist_item'
         ).read()[0]
         action['context'] = dict(self.env.context)
-        action['context']['search_default_product_tmpl_id'] = self.id
+        action['context']['search_default_product_tmpl_id'] = self.product_tmpl_id.id
+        action['context']['search_default_product_id'] = self.id
         return action
