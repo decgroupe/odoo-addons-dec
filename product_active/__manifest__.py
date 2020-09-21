9cc339f9dad5749a57e965b5d2a6515f877e58d6
dsi <decindustrie.dsi@gmail.com>
2020-09-21 13:14:46 +0200

    New module to propagate active state from variant to template

---
+++ product_active/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Product active',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'http://www.dec-industrie.com',
+    'summary': '''Active state change propagation to template''',
+    'depends': ['product', ],
+    'data': [],
+    'installable': True
+}
---
+++ product_active/models/product_product.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) DEC SARL, Inc - All Rights Reserved.
+# Written by Yann Papouin <y.papouin at dec-industrie.com>, Sep 2020
+
+from odoo import api, fields, models
+from odoo.addons import decimal_precision as dp
+
+
+class ProductProduct(models.Model):
+    _inherit = 'product.product'
+
+    @api.multi
+    def write(self, vals):
+        res = super().write(vals)
+        # When we toggle active state of a 'product.template', the new state
+        # is propagated to variants ('product.product'). When adding
+        # 'active_to_tmpl' to the write context, the active change will be
+        # propagated the other side, from 'product.product' to
+        # 'product.template' then 'product.template' to 'product.product'.
+        if not self.env.context.get('active_to_tmpl_done'):
+            if 'active' in vals and self.env.context.get('active_to_tmpl'):
+                self.with_context(active_to_tmpl_done=True).\
+                    product_tmpl_id.active = vals.get('active')
+        return res
