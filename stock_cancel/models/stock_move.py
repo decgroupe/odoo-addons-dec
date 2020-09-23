4256907cac67a08c48673ffa9f2cd6affa61405f
dsi <decindustrie.dsi@gmail.com>
2020-09-23 15:11:37 +0200

    is_cancellable added to simplify view filtering

--- stock_cancel/models/stock_move.py
+++ stock_cancel/models/stock_move.py
@@ -2,11 +2,16 @@
 # Copyright (C) DEC SARL, Inc - All Rights Reserved.
 # Written by Yann Papouin <y.papouin at dec-industrie.com>, Sep 2020
 
-from odoo import models, api
+from odoo import fields, models, api
 
 
 class StockMove(models.Model):
     _inherit = "stock.move"
+
+    is_cancellable = fields.Boolean(
+        compute="_compute_is_cancellable",
+        help="Technical field to check for button visibility",
+    )
 
     @api.multi
     def _action_cancel_stream(self, downstream=False, upstream=False):
@@ -51,3 +56,13 @@ class StockMove(models.Model):
     @api.multi
     def action_cancel_upstream(self):
         self._action_cancel_stream(upstream=True)
+
+    @api.depends('procure_method', 'state')
+    def _compute_is_cancellable(self):
+        for move in self:
+            if move.procure_method == 'make_to_stock':
+                if move.state not in ('done', 'cancel'):
+                    move.is_cancellable = True
+            if move.procure_method == 'make_to_order':
+                if move.state in ('draft'):
+                    move.is_cancellable = True
