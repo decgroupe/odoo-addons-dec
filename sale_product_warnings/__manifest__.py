ce84cec39d1af7ad9354b4bbf2408d6147b841b7
dsi <decindustrie.dsi@gmail.com>
2020-09-29 09:11:36 +0200

    New module to warn seller about possible pre-sale actions to do

---
+++ sale_product_warnings/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Sale Warnings',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'http://www.dec-industrie.com',
+    'category': 'Sales',
+    'summary': '''Warnings on adding obsolete products to quotation''',
+    'depends': [
+        'sale',
+        'product',
+        'product_reference',
+        'product_state_review',
+    ],
+    #'force_migration':'12.0.0.0.0',
+    'data': [],
+    'installable': True
+}
---
+++ sale_product_warnings/models/sale_order.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) DEC SARL, Inc - All Rights Reserved.
+# Written by Yann Papouin <y.papouin at dec-industrie.com>, Sep 2020
+
+from odoo import api, models, _
+
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    @api.multi
+    @api.onchange('product_id')
+    def product_id_change(self):
+        res = super().product_id_change()
+
+        if self.product_id and self.product_id.state == 'obsolete':
+            warning = {
+                'title':
+                    _("Warning for %s") % self.product_id.name,
+                'message':
+                    _(
+                        'Obsolete product!\n '
+                        '(This product must not be sold anymore.)\n\n %s'
+                    ) %
+                    (self.product_id.internal_notes or _('No internal notes'))
+            }
+            res = {'warning': warning}
+
+        elif self.product_id and self.product_id.state == 'review':
+            warning = {
+                'title':
+                    _("Warning for %s") % self.product_id.name,
+                'message':
+                    _(
+                        'This product needs to be reviewed:\n\n - %s \n\n - '
+                        'Please contact "%s"'
+                    ) % (
+                        self.product_id.internal_notes or
+                        _('No internal notes'), self.product_id.responsible_id
+                        and self.product_id.responsible_id.name or
+                        _('No responsible for this product')
+                    )
+            }
+            res = {'warning': warning}
+
+        return res
