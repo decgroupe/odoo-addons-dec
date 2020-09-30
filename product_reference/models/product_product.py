3935e3a98add95f4eff6485eb63e4f74e4c6c04f
dsi <decindustrie.dsi@gmail.com>
2020-09-30 15:06:32 +0200

    Use * as a joker to search for all products (sellable or not)

--- product_reference/models/product_product.py
+++ product_reference/models/product_product.py
@@ -10,6 +10,10 @@ class Product(models.Model):
 
     @api.model
     def name_search(self, name='', args=None, operator='ilike', limit=100):
+        # If name starts with a wilcard, then clear arg domain
+        if name.startswith('*'):
+            name = name[1:]
+            args = False
         if not args:
             args = []
         # Make a search with default criteria
--- product_reference/models/product_template.py
+++ product_reference/models/product_template.py
@@ -50,6 +50,10 @@ class ProductTemplate(models.Model):
 
     @api.model
     def name_search(self, name='', args=None, operator='ilike', limit=100):
+        # If name starts with a wilcard, then clear arg domain
+        if name.startswith('*'):
+            name = name[1:]
+            args = False
         if not args:
             args = []
         # Make a search with default criteria
