10652a2015e580ddf731a9f4bce08e9dd1b5eedf
Yann Papouin <y.papouin@dec-industrie.com>
2020-10-02 17:25:02 +0200

    Use float_compare to compare quantity_done with product_uom_qty

--- mrp_transition/models/stock_move.py
+++ mrp_transition/models/stock_move.py
@@ -3,15 +3,20 @@
 # Written by Yann Papouin <y.papouin at dec-industrie.com>, Oct 2020
 
 from odoo import api, fields, models, _
+from odoo.tools import float_compare
 
 
 class StockMove(models.Model):
     _inherit = 'stock.move'
 
-    is_quantity_done_greater = fields.Boolean(
-        compute='_compute_is_quantity_done_greater',
+    compare_quantity_done = fields.Integer(
+        compute='_compute_compare_quantity_done',
         store=True,
         compute_sudo=True,
+        help="Compare quantity_done with product_uom_qty :"
+        "\n - Return 1 if quantity_done > product_uom_qty"
+        "\n - Return 0 if quantity_done = product_uom_qty"
+        "\n - Return -1 if quantity_done < product_uom_qty"
     )
 
     move_lines_count = fields.Integer(
@@ -22,10 +27,13 @@ class StockMove(models.Model):
 
     @api.multi
     @api.depends('product_uom_qty', 'quantity_done')
-    def _compute_is_quantity_done_greater(self):
+    def _compute_compare_quantity_done(self):
         for move in self:
-            move.is_quantity_done_greater = (
-                move.quantity_done > move.product_uom_qty
+            rounding = move.product_id.uom_id.rounding
+            move.compare_quantity_done = float_compare(
+                move.quantity_done,
+                move.product_uom_qty,
+                precision_rounding=rounding,
             )
 
     @api.depends('move_line_ids')
