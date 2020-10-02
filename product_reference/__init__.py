32425cbb663bd8b6800441d40b909ae3142ea561
Yann Papouin <y.papouin@dec-industrie.com>
2020-10-02 10:44:03 +0200

    Import only in post_init

--- product_reference/__init__.py
+++ product_reference/__init__.py
@@ -1,7 +1,5 @@
 from . import models
 
-from odoo import api, SUPERUSER_ID
-
 
 def set_mrp_production_request(env):
     for ref in env['ref.reference'].search([]):
@@ -9,5 +7,7 @@ def set_mrp_production_request(env):
 
 
 def post_init(cr, registry):
+    from odoo import api, SUPERUSER_ID
+
     env = api.Environment(cr, SUPERUSER_ID, {})
     set_mrp_production_request(env)
