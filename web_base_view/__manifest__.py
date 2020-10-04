072ffed3326a2e4a15845109eed9a94349aa153d
Yann Papouin <y.papouin@dec-industrie.com>
2020-10-04 11:34:38 +0200

    New module for web common styles

---
+++ web_base_view/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Web Base View',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'http://www.dec-industrie.com',
+    'category': 'Base',
+    'summary': '''Common styles for view display''',
+    'depends': [
+        'dec',
+        'sale_margin'
+    ],
+    'data':
+        [
+            'views/assets.xml',
+        ],
+    'installable': True
+}
