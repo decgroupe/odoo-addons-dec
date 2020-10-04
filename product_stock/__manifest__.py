1356d70593f8b6c1c97404835d41efd8ccc2bbf7
Yann Papouin <y.papouin@dec-industrie.com>
2020-10-04 10:42:21 +0200

    Split forecast stat button text to show all values

---
+++ product_stock/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Product Stock',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'http://www.dec-industrie.com',
+    'summary': '''Cosmetic changes''',
+    'depends': [
+        'product',
+    ],
+    # 'force_migration':'12.0.0.0.0',
+    'data': [
+        'views/product_product.xml',
+    ],
+    'installable': True
+}
