2e8008f82ce26dae3e55c51e0e4704228ee4c328
Yann Papouin <y.papouin@dec-industrie.com>
2020-10-06 21:50:28 +0200

    Apply procurement to lines when set to order

--- purchase_traceability/models/purchase_order.py
+++ purchase_traceability/models/purchase_order.py
@@ -2,8 +2,20 @@
 # Copyright (C) DEC SARL, Inc - All Rights Reserved.
 # Written by Yann Papouin <y.papouin at dec-industrie.com>, Jul 2020
 
-from odoo import models
+from odoo import models, api
 
 
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
+
+    @api.multi
+    def write(self, values):
+        if 'group_id' in values:
+            lines = self.env['purchase.order.line']
+            for order in self:
+                lines += order.order_line.filtered(
+                    lambda line: not line.procurement_group_id
+                )
+            if lines:
+                lines.procurement_group_id = values['group_id']
+        return super().write(values)
