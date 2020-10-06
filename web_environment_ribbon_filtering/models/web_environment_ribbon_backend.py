d2d3972929ab6233dbfd42593fa8567dfb6a7978
Yann Papouin <y.papouin@dec-industrie.com>
2020-10-06 21:13:43 +0200

    Use database list from config file to ignore ribbon

--- web_environment_ribbon_filtering/models/web_environment_ribbon_backend.py
+++ web_environment_ribbon_filtering/models/web_environment_ribbon_backend.py
@@ -3,10 +3,21 @@
 # Written by Yann Papouin <y.papouin at dec-industrie.com>, Oct 2020
 
 from odoo import api, models
+from odoo.tools import ormcache
+from odoo.tools.config import config, to_list
 
 
 class WebEnvironmentRibbonBackend(models.AbstractModel):
     _inherit = 'web.environment.ribbon.backend'
+
+    @api.model
+    @ormcache()
+    def _get_db_ribbon_ignorelist(self):
+        res = []
+        ignorelist = config.get("db_ribbon_ignorelist")
+        if ignorelist:
+            res = to_list(ignorelist)
+        return res
 
     @api.model
     def get_environment_ribbon(self):
@@ -15,6 +26,6 @@ class WebEnvironmentRibbonBackend(models.AbstractModel):
         :return: dictionary
         """
         res = super().get_environment_ribbon()
-        if self.env.cr.dbname in ['dec', 'esi']:
+        if self.env.cr.dbname in self._get_db_ribbon_ignorelist():
             res['name'] = ''
         return res
