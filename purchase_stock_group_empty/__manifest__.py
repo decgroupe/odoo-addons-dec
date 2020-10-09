cd2acfad0f610c94cfa12c925dc121260a8d4bda
Yann Papouin <y.papouin@dec-industrie.com>
2020-10-09 15:58:58 +0200

    New module to avoid grouping procurements without group with existing procurements with group

---
+++ purchase_stock_group_empty/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Purchase Stock Group Empty',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'http://www.dec-industrie.com',
+    'summary': '''Group procurements without group together''',
+    'depends': [
+        'purchase_stock',
+    ],
+    'data': [
+    ],
+    'installable': True
+}
---
+++ purchase_stock_group_empty/models/stock_rule.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) DEC SARL, Inc - All Rights Reserved.
+# Written by Yann Papouin <y.papouin at dec-industrie.com>, Oct 2020
+
+from odoo import models, api, _
+
+
+class StockRule(models.Model):
+    _inherit = 'stock.rule'
+
+    def _make_po_get_domain(self, values, partner):
+        domain = super()._make_po_get_domain(values, partner)
+        # Force group_id to False if not set to only group purchase orders
+        # without group_id set
+
+        group_id_set = False
+        for filter in domain:
+            if filter[0] == 'group_id':
+                group_id_set = True
+
+        if not group_id_set:
+            domain += (('group_id', '=', False), )
+
+        return domain
