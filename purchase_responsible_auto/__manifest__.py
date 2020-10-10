3246d79091132cec63ec078f45417ba6dae42f47
Yann Papouin <y.papouin@dec-industrie.com>
2020-10-10 12:12:21 +0200

    Auto set or replace purchase responsible

---
+++ purchase_responsible_auto/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Purchase Responsible',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'http://www.dec-industrie.com',
+    'summary': '''Auto replace responsible on print''',
+    'depends': [
+        'purchase',
+    ],
+    'data':
+        [
+        ],
+    'installable': True
+}
---
+++ purchase_responsible_auto/models/purchase_order.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) DEC SARL, Inc - All Rights Reserved.
+# Written by Yann Papouin <y.papouin at dec-industrie.com>, Oct 2020
+
+from odoo import models, api
+
+
+class PurchaseOrder(models.Model):
+    _inherit = 'purchase.order'
+
+    @api.multi
+    def print_quotation(self):
+        for order in self:
+            if order.user_id == self.env.ref('base.user_root'):
+                order.user_id = self.env.user
+        return super().print_quotation()
