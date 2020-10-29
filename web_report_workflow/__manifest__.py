2bd2ea20e756a4cd952ab5d7d0fcd7da85fc899b
Yann Papouin <y.papouin@dec-industrie.com>
2020-10-29 13:40:05 +0100

    QWeb report worfklow module (esi)

---
+++ web_report_workflow/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Web Report Workflow',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'http://www.dec-industrie.com',
+    'summary': '''Report template improvements''',
+    'depends': [
+        'web',
+        'company_report',
+    ],
+    'data':
+        [
+            'views/report_templates.xml',
+        ],
+    'installable': True
+}
