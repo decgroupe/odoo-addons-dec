c2fb13c24b94f05bc49911e785010d827f84fa02
Yann Papouin <y.papouin@dec-industrie.com>
2020-10-29 01:28:58 +0100

    Dedicated module to disable google sync on test databases

---
+++ google_calendar_filtering/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Google Calendar Filtering',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'http://www.dec-industrie.com',
+    'summary': '''Hard-coded database filtering''',
+    'depends': ['google_calendar', ],
+    'data': [],
+    'installable': True
+}
