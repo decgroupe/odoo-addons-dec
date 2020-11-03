21f7ec0e217166e4f717292eb7b8d5802c129dd4
Yann Papouin <y.papouin@dec-industrie.com>
2020-11-03 16:22:49 +0100

    Same module from sale to account for web rendering

---
+++ account_report_qweb/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Account (Qweb report)',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'http://www.dec-industrie.com',
+    'summary': '''Account invoice report''',
+    'depends': [
+        'account',
+    ],
+    'data':
+        [
+            'report/account_invoice.xml',
+        ],
+    'installable': True
+}
