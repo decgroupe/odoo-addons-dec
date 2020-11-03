ed9abc6e157bab27f2b299266a3c74426cf1098c
Yann Papouin <y.papouin@dec-industrie.com>
2020-11-03 14:48:31 +0100

    Module for custom sale report in qweb format

---
+++ sale_report_qweb/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Sale (Qweb report)',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'http://www.dec-industrie.com',
+    'summary': '''Sale order report''',
+    'depends': [
+        'sale',
+    ],
+    'data':
+        [
+            'report/sale_order.xml',
+        ],
+    'installable': True
+}
