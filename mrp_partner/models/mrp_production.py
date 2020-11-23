2db4214fe65d31bd54a49ad14534e13a8cef4c54
Yann Papouin <y.papouin@dec-industrie.com>
2020-11-23 12:04:15 +0100

    Use sale_mrp_link to find matching partner_id

--- mrp_partner/models/mrp_production.py
+++ mrp_partner/models/mrp_production.py
@@ -8,4 +8,7 @@ from odoo import models, api, fields
 class MrpProduction(models.Model):
     _inherit = "mrp.production"
 
+    # TODO: partner_id is currently defined in mrp_sale module
+    # it should be moved here and mrp_sale should depends on mrp_partner
+    
     zip_id = fields.Many2one(related='partner_id.zip_id')
--- mrp_sale/models/mrp_production.py
+++ mrp_sale/models/mrp_production.py
@@ -15,14 +15,21 @@ class MrpProduction(models.Model):
 
     @api.model
     def create(self, values):
+        sale_order_id = self.env['sale.order']
+
+        # Use sale_order_id from sale_mrp_link module to retrieve partner_id
+        if 'sale_order_id' in values and values['sale_order_id']:
+            sale_order_id = sale_order_id.browse(values['sale_order_id'])
         # Use sale_line_id from sale_stock module to retrieve partner_id
-        if 'move_prod_id' in values and values['move_prod_id']:
+        elif 'move_prod_id' in values and values['move_prod_id']:
             move_prod_id = self.env['stock.move'].browse(values['move_prod_id'])
             while move_prod_id:
                 if move_prod_id.sale_line_id and move_prod_id.sale_line_id.order_id:
-                    partner_id = move_prod_id.sale_line_id.order_id.partner_shipping_id or False
-                    values['partner_id'] = partner_id.id
+                    sale_order_id = move_prod_id.sale_line_id.order_id
                     break
                 move_prod_id = move_prod_id.move_dest_id
+        if sale_order_id:
+            partner_id = sale_order_id.partner_shipping_id or False
+            values['partner_id'] = partner_id.id
         production = super(MrpProduction, self).create(values)
         return production
