b6ed3d544946170ebd74816d74964d7f8ec684b2
Yann Papouin <y.papouin@dec-industrie.com>
2020-11-24 08:36:04 +0100

    Main public category added for easier groupby

---
+++ website_sale_main_category/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Website Sale Main Category',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'http://www.dec-industrie.com',
+    'summary': 'Allow user to group products with their main '
+               'public category',
+    'depends': ['website_sale', ],
+    'data': ['views/product_template.xml', ],
+    'installable': True
+}
---
+++ website_sale_main_category/models/product_template.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) DEC SARL, Inc - All Rights Reserved.
+# Written by Yann Papouin <y.papouin at dec-industrie.com>, Nov 2020
+
+from odoo import fields, models, api
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    public_categ_id = fields.Many2one(
+        'product.public.category',
+        string='Website Main Product Category',
+        compute='_compute_public_categ_id',
+        store=True,
+    )
+
+    @api.multi
+    @api.depends('public_categ_ids')
+    def _compute_public_categ_id(self):
+        for record in self:
+            record.public_categ_id = record.public_categ_ids \
+                and record.public_categ_ids[0] or False
