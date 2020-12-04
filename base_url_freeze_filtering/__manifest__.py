a8d8701867576aab5b2398dfb73ead6c3935e266
Yann Papouin <y.papouin@dec-industrie.com>
2020-12-04 16:01:38 +0100

    New module to disable web base url freeze when not on production database

---
+++ base_url_freeze_filtering/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Webbase Url Freeze Filtering',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'http://www.dec-industrie.com',
+    'summary': '''Hard-coded database filtering''',
+    'depends': ['base', ],
+    'data': [],
+    'installable': True
+}
---
+++ base_url_freeze_filtering/models/ir_config_parameter.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) DEC SARL, Inc - All Rights Reserved.
+# Written by Yann Papouin <y.papouin at dec-industrie.com>, Dec 2020
+
+import logging
+
+from odoo import api, models, _
+from odoo.tools import ormcache
+from odoo.tools.config import config, to_list
+
+_logger = logging.getLogger(__name__)
+
+WEB_BASE_URL_FREEZE = 'web.base.url.freeze'
+
+class IrConfigParameter(models.Model):
+    _inherit = 'ir.config_parameter'
+
+    @api.model
+    @ormcache()
+    def _get_db_url_freeze_allowedlist(self):
+        res = []
+        allowedlist = config.get("db_url_freeze_allowedlist")
+        if allowedlist:
+            res = to_list(allowedlist)
+        return res
+
+    @api.model
+    def get_param(self, key, default=False):
+        value = super().get_param(key, default=default)
+        if key == WEB_BASE_URL_FREEZE and value and \
+            self.env.cr.dbname not in self._get_db_url_freeze_allowedlist():
+            _logger.info('%s forced to False', WEB_BASE_URL_FREEZE)
+            value = False
+        return value
