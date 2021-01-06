d246bd736c5e6686d60687656fb0398cf16f95d6
Yann Papouin <y.papouin@dec-industrie.com>
2021-01-06 15:45:48 +0100

    Small tool to identify location duplicates

---
+++ base_location_count_caps/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Base Location Count Caps',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'http://www.dec-industrie.com',
+    'depends': [
+        'base_location',
+    ],
+    'data': [
+    ],
+    'installable': True
+}
---
+++ base_location_count_caps/models/res_city.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) DEC SARL, Inc - All Rights Reserved.
+# Written by Yann Papouin <y.papouin at dec-industrie.com>, Dec 2020
+
+from string import ascii_uppercase, ascii_lowercase
+
+from odoo import fields, models, api
+from odoo.tools import remove_accents
+
+import unicodedata
+
+
+class ResCity(models.Model):
+    _inherit = "res.city"
+
+    caps_count = fields.Integer(
+        compute='_compute_caps',
+        store=True,
+    )
+    caps_ratio = fields.Float(
+        compute='_compute_caps',
+        store=True,
+    )
+    normalized_name = fields.Char(
+        compute='_compute_caps',
+        store=True,
+    )
+
+    @api.multi
+    @api.depends('name')
+    def _compute_caps(self):
+        for rec in self:
+            alpha_count = sum(
+                1 for c in rec.name if c.upper() in ascii_uppercase
+            )
+            rec.caps_count = sum(1 for c in rec.name if c.isupper())
+            rec.caps_ratio = rec.caps_count / alpha_count
+
+            # Create a normalized name to easilly find duplicates
+            s_name = remove_accents(rec.name.lower())
+            rec.normalized_name = ''.join(
+                c for c in s_name if c in ascii_lowercase
+            )
+
+    @api.model
+    def read_group(
+        self,
+        domain,
+        fields,
+        groupby,
+        offset=0,
+        limit=None,
+        orderby=False,
+        lazy=True
+    ):
+        res = super().read_group(
+            domain,
+            fields,
+            groupby,
+            offset=offset,
+            limit=limit,
+            orderby=orderby,
+            lazy=lazy
+        )
+        if 'normalized_name' in groupby:
+            filtered_res = []
+            for value in res:
+                if value['normalized_name_count'] > 1:
+                    filtered_res.append(value)
+            res = filtered_res
+        return res
