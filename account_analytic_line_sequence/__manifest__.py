af76e2df232bf3802a223072510346c874d38b02
Yann Papouin <y.papouin@dec-industrie.com>
2021-01-06 15:46:21 +0100

    Add sequence for analytic lines (used by REFManager)

---
+++ account_analytic_line_sequence/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Account Analytic Line Sequence',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'http://www.dec-industrie.com',
+    'summary': '''Add sequence field on analytic lines''',
+    'depends': [
+        'account',
+    ],
+    'data': [],
+    'installable': True
+}
---
+++ account_analytic_line_sequence/models/account_analytic_line.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) DEC SARL, Inc - All Rights Reserved.
+# Written by Yann Papouin <y.papouin at dec-industrie.com>, Jan 2021
+
+from odoo import fields, models
+
+
+class AccountAnalyticLine(models.Model):
+    _inherit = 'account.analytic.line'
+
+    sequence = fields.Integer()
