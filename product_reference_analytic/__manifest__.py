b3d0d750feb3c8ce7e33eef5e714ad37e4afd388
Yann Papouin <y.papouin@dec-industrie.com>
2021-01-08 18:36:59 +0100

    Hook to create default analytic lines for categories

--- product_reference_analytic/__manifest__.py
+++ product_reference_analytic/__manifest__.py
@@ -4,15 +4,17 @@
     'author': 'DEC, Yann Papouin',
     'website': 'http://www.dec-industrie.com',
     'summary': '''Product reference analytic linking''',
-    'depends': [
-        'product_reference',
-        'product_analytic',
-        'account_analytic_parent',
-    ],
-    'data':
+    'depends':
         [
-            'data/account_analytic_account.xml',
-            'views/ref_category.xml',
+            'product_reference',
+            'product_analytic',
+            'account_analytic_parent',
         ],
+    'data': [
+        'data/account_analytic_account.xml',
+        'views/ref_category.xml',
+    ],
+    # 'force_post_init_hook': True,
+    'post_init_hook': 'post_init',
     'installable': True
 }
