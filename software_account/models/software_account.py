2afd6ee7bb30ddcf47a4bf6bcd5195460d74c44c
Yann Papouin <y.papouin@dec-industrie.com>
2021-01-19 16:27:16 +0100

    Fix printing multiple account reports

--- software_account/models/software_account.py
+++ software_account/models/software_account.py
@@ -32,6 +32,6 @@ class SoftwareAccount(models.Model):
 
     @api.multi
     def _get_aeroo_report_filename(self):
-        names = [x.name or str(x.id) for x in self]
+        names = [x.login or str(x.id) for x in self]
         res = '-'.join(names)
         return res
