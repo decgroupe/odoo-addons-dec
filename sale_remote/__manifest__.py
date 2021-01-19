3076b6505f991f727615263ede34e8b03d7ae808
Yann Papouin <y.papouin@dec-industrie.com>
2021-01-19 16:26:28 +0100

    Allow RPC on sales

---
+++ sale_remote/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Sale Remote',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'http://www.dec-industrie.com',
+    'category': 'Sales',
+    'summary': '''Allow RPC on sales''',
+    'depends': [
+        'sale_markup',
+        'onchange_helper',
+    ],
+    'data': [],
+    'installable': True
+}
---
+++ sale_remote/models/sale_order.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) DEC SARL, Inc - All Rights Reserved.
+# Written by Yann Papouin <y.papouin at dec-industrie.com>, Jan 2021
+
+import logging
+from odoo import api, models
+
+_logger = logging.getLogger(__name__)
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    @api.multi
+    def create_line(
+        self,
+        product_id,
+        product_uom_id,
+        product_uom_qty=1.0,
+        markup_percent=0
+    ):
+        self.ensure_one()
+        vals = {
+            'order_id': self.id,
+            'product_id': product_id,
+        }
+        vals = self.env['sale.order.line'].play_onchanges(vals, ['product_id'])
+        vals['product_uom'] = product_uom_id
+        vals = self.env['sale.order.line'].play_onchanges(vals, ['product_uom'])
+        vals['product_uom_qty'] = product_uom_qty
+        vals = self.env['sale.order.line'].play_onchanges(
+            vals, ['product_uom_qty']
+        )
+        if markup_percent:
+            # We need to remove price_unit to get a new one, take a look on
+            # _get_new_values
+            # from oca/server-tools/onchange_helper/models/base.py
+            vals.pop('price_unit')
+            vals['markup_percent'] = markup_percent
+            vals = self.env['sale.order.line'].play_onchanges(
+                vals, ['markup_percent']
+            )
+        line = self.env['sale.order.line'].create(vals)
+        return line.id
