515639495cc9ba24f041565e90fda0421ece1e1b
Yann Papouin <y.papouin@dec-industrie.com>
2021-01-23 16:29:21 +0100

    Use same way for product variant than template to update price write dates

--- product_prices/models/product_product.py
+++ product_prices/models/product_product.py
@@ -2,6 +2,8 @@
 # Copyright (C) DEC SARL, Inc - All Rights Reserved.
 # Written by Yann Papouin <y.papouin at dec-industrie.com>, May 2020
 
+from datetime import datetime
+
 from odoo import api, fields, models
 from odoo.addons import decimal_precision as dp
 
@@ -11,6 +13,21 @@ class ProductProduct(models.Model):
 
     # Override digit field to increase precision
     standard_price = fields.Float(digits=dp.get_precision('Purchase Price'), )
+
+    @api.multi
+    def write(self, vals):
+        if 'list_price' in vals:
+            vals['price_write_date'] = fields.Datetime.to_string(
+                datetime.now()
+            ),
+            vals['price_write_uid'] = self.env.user.id
+        if 'standard_price' in vals:
+            vals['standard_price_write_date'] = fields.Datetime.to_string(
+                datetime.now()
+            ),
+            vals['standard_price_write_uid'] = self.env.user.id
+        res = super().write(vals)
+        return res
 
     @api.multi
     def open_price_graph(self):
