494c710e7140596ba7fc2156c2428159c2a67477
Yann Papouin <y.papouin@dec-industrie.com>
2021-01-23 16:29:59 +0100

    Allow to search fo raw products on mrp

---
+++ mrp_search_product/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Production Search Raw Product',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'http://www.dec-industrie.com',
+    'summary':
+        "Search for production orders containing specific "
+        "raw products",
+    'depends': ['mrp', ],
+    'data': ['views/mrp_production.xml', ],
+    'installable': True
+}
