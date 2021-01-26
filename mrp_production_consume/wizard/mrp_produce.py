9eeeb7179dddb5842a8747af2c707ef9661418c1
Yann Papouin <y.papouin@dec-industrie.com>
2021-01-26 23:46:18 +0100

    Fix for the case when there is no more thing to produce but some things to consume

--- mrp_production_consume/wizard/mrp_produce.py
+++ mrp_production_consume/wizard/mrp_produce.py
@@ -14,45 +14,101 @@ class MrpProduce(models.TransientModel):
     @api.onchange('product_qty')
     def _onchange_product_qty(self):
         lines = []
-        qty_todo = self.product_uom_id._compute_quantity(self.product_qty, self.production_id.product_uom_id, round=False)
+        qty_todo = self.product_uom_id._compute_quantity(
+            self.product_qty, self.production_id.product_uom_id, round=False
+        )
         # Do not filter out moves not linked to a bom_line_id like it is done in
         # addons/mrp/wizard/mrp_product_produce.py
-        for move in self.production_id.move_raw_ids.filtered(lambda m: m.state not in ('done', 'cancel')):
-            qty_to_consume = float_round(qty_todo * move.unit_factor, precision_rounding=move.product_uom.rounding)
+        for move in self.production_id.move_raw_ids.filtered(
+            lambda m: m.state not in ('done', 'cancel')
+        ):
+            qty_to_consume = float_round(
+                qty_todo * move.unit_factor,
+                precision_rounding=move.product_uom.rounding
+            )
             for move_line in move.move_line_ids:
-                if float_compare(qty_to_consume, 0.0, precision_rounding=move.product_uom.rounding) <= 0:
+                if float_compare(
+                    qty_to_consume,
+                    0.0,
+                    precision_rounding=move.product_uom.rounding
+                ) <= 0:
                     break
-                if move_line.lot_produced_id or float_compare(move_line.product_uom_qty, move_line.qty_done, precision_rounding=move.product_uom.rounding) <= 0:
+                if move_line.lot_produced_id or float_compare(
+                    move_line.product_uom_qty,
+                    move_line.qty_done,
+                    precision_rounding=move.product_uom.rounding
+                ) <= 0:
                     continue
-                to_consume_in_line = min(qty_to_consume, move_line.product_uom_qty)
-                lines.append({
-                    'move_id': move.id,
-                    'qty_to_consume': to_consume_in_line,
-                    'qty_done': to_consume_in_line,
-                    'lot_id': move_line.lot_id.id,
-                    'product_uom_id': move.product_uom.id,
-                    'product_id': move.product_id.id,
-                    'qty_reserved': min(to_consume_in_line, move_line.product_uom_qty),
-                })
+                to_consume_in_line = min(
+                    qty_to_consume, move_line.product_uom_qty
+                )
+                lines.append(
+                    {
+                        'move_id':
+                            move.id,
+                        'qty_to_consume':
+                            to_consume_in_line,
+                        'qty_done':
+                            to_consume_in_line,
+                        'lot_id':
+                            move_line.lot_id.id,
+                        'product_uom_id':
+                            move.product_uom.id,
+                        'product_id':
+                            move.product_id.id,
+                        'qty_reserved':
+                            min(to_consume_in_line, move_line.product_uom_qty),
+                    }
+                )
                 qty_to_consume -= to_consume_in_line
-            if float_compare(qty_to_consume, 0.0, precision_rounding=move.product_uom.rounding) > 0:
+            if float_compare(
+                qty_to_consume,
+                0.0,
+                precision_rounding=move.product_uom.rounding
+            ) > 0:
                 if move.product_id.tracking == 'serial':
-                    while float_compare(qty_to_consume, 0.0, precision_rounding=move.product_uom.rounding) > 0:
-                        lines.append({
-                            'move_id': move.id,
-                            'qty_to_consume': 1,
-                            'qty_done': 1,
-                            'product_uom_id': move.product_uom.id,
-                            'product_id': move.product_id.id,
-                        })
+                    while float_compare(
+                        qty_to_consume,
+                        0.0,
+                        precision_rounding=move.product_uom.rounding
+                    ) > 0:
+                        lines.append(
+                            {
+                                'move_id': move.id,
+                                'qty_to_consume': 1,
+                                'qty_done': 1,
+                                'product_uom_id': move.product_uom.id,
+                                'product_id': move.product_id.id,
+                            }
+                        )
                         qty_to_consume -= 1
                 else:
-                    lines.append({
-                        'move_id': move.id,
-                        'qty_to_consume': qty_to_consume,
-                        'qty_done': qty_to_consume,
-                        'product_uom_id': move.product_uom.id,
-                        'product_id': move.product_id.id,
-                    })
+                    lines.append(
+                        {
+                            'move_id': move.id,
+                            'qty_to_consume': qty_to_consume,
+                            'qty_done': qty_to_consume,
+                            'product_uom_id': move.product_uom.id,
+                            'product_id': move.product_id.id,
+                        }
+                    )
 
-        self.produce_line_ids = [(5,)] + [(0, 0, x) for x in lines]
+        # In case of no lines were added, it could be because the production
+        # order is done but some moves have been added after
+        if qty_todo == 0 and not lines:
+            for move in self.production_id.move_raw_ids.filtered(
+                lambda m: m.state not in ('done', 'cancel')
+            ):
+                for move_line in move.move_line_ids:
+                    lines.append(
+                        {
+                            'move_id': move.id,
+                            'qty_to_consume': move_line.product_uom_qty,
+                            'qty_done': move_line.product_uom_qty,
+                            'product_uom_id': move.product_uom.id,
+                            'product_id': move.product_id.id,
+                            'qty_reserved': move_line.product_uom_qty,
+                        }
+                    )
+
+        self.produce_line_ids = [(5, )] + [(0, 0, x) for x in lines]
