f813587d9a39eb4449e4d09cbc5333242cf178a5
Yann Papouin <y.papouin@dec-industrie.com>
2021-01-29 08:56:23 +0100

    Remove non exiting module from import

--- business_exception/models/__init__.py
+++ business_exception/models/__init__.py
@@ -1,6 +1,4 @@
 from . import mail_activity
-# from . import business_activity_interception_mixin
-from . import business_exception
 from . import stock_picking
 from . import mrp_production
 from . import purchase_order
