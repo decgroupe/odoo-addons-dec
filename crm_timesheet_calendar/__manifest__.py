d056159f87371e9fb5efacd6655478fe7add2d33
Yann Papouin <y.papouin@dec-industrie.com>
2021-01-29 14:25:43 +0100

    Add leads on timesheet calendar view

---
+++ crm_timesheet_calendar/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'CRM Timesheet Calendar',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'http://www.dec-industrie.com',
+    'summary': "Add calendar view for CRM Timesheet lines",
+    'depends': ['hr_timesheet_calendar', ],
+    'data': ['views/account_analytic_line.xml', ],
+    'installable': True
+}
