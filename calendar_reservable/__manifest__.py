e0babef63a89a1ea1656d20b397af6cd2a5ee568
Yann Papouin <y.papouin@dec-industrie.com>
2021-02-01 20:17:08 +0100

    Reservable filter for meetings

---
+++ calendar_reservable/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Calendar Reservable',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'http://www.dec-industrie.com',
+    'summary': "Filter for reservable <partners> like vehicles, rooms, etc.",
+    'depends': ['calendar', ],
+    'data': ['views/calendar.xml', ],
+    'installable': True
+}
---
+++ calendar_reservable/models/calendar.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) DEC SARL, Inc - All Rights Reserved.
+# Written by Yann Papouin <y.papouin at dec-industrie.com>, Feb 2021
+
+from odoo import models, api, fields
+
+
+class Meeting(models.Model):
+    _inherit = 'calendar.event'
+
+    @api.model
+    def _search(
+        self,
+        args,
+        offset=0,
+        limit=None,
+        order=None,
+        count=False,
+        access_rights_uid=None
+    ):
+        instance = self
+        if self._context.get('reservable'):
+            if self._context.get('mymeetings'):
+                args += [
+                    ('|'),
+                    ('partner_ids', 'in', self.env.user.partner_id.ids),
+                    ('partner_ids.function', 'ilike', "reservable"),
+                ]
+                instance = self.with_context(mymeetings=False)
+            else:
+                args += [('partner_ids.function', 'ilike', "reservable")]
+
+        return super(Meeting, instance)._search(
+            args,
+            offset=offset,
+            limit=limit,
+            order=order,
+            count=count,
+            access_rights_uid=access_rights_uid
+        )
