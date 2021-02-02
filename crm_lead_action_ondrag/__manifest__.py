4f641076b80035b6667f8fbf7dfdd8b4bf0c4cda
Yann Papouin <y.papouin@dec-industrie.com>
2021-02-03 00:02:38 +0100

    Set won/lost when dragging card in kanban mode

---
+++ crm_lead_action_ondrag/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'CRM Lead action on Drag',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'http://www.dec-industrie.com',
+    'summary':
+        "Call lost or won actions when dragging card in columns "
+        "with matching probabilities",
+    'depends': ['crm', ],
+    'data': ['views/crm_lead.xml', ],
+    'installable': True
+}
---
+++ crm_lead_action_ondrag/models/crm_lead.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) DEC SARL, Inc - All Rights Reserved.
+# Written by Yann Papouin <y.papouin at dec-industrie.com>, Feb 2021
+
+import logging
+
+from odoo import fields, api, models
+
+_logger = logging.getLogger(__name__)
+
+
+class CrmLead(models.Model):
+    _inherit = 'crm.lead'
+
+    @api.multi
+    def write(self, vals):
+        if vals.get('stage_id') and not 'set_stage' in self._context:
+            lost_stage_id = self._stage_find(domain=[('probability', '<=', 0)])
+            won_stage_id = self._stage_find(domain=[('probability', '>=', 100)])
+            if vals.get('stage_id') == lost_stage_id.id:
+                self.with_context(set_stage=True).action_set_lost()
+            elif vals.get('stage_id') == won_stage_id.id:
+                self.with_context(set_stage=True).action_set_won()
+        return super().write(vals)
