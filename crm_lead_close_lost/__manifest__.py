98d9dbb2a589141aed41a352dc6422cd74c74b37
Yann Papouin <y.papouin@dec-industrie.com>
2021-02-03 00:02:57 +0100

    Set close date when lead is lost

---
+++ crm_lead_close_lost/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'CRM Close Lost',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'http://www.dec-industrie.com',
+    'summary': "Set close date when a lead is lost",
+    'depends': ['crm', ],
+    'data': ['views/crm_lead.xml', ],
+    'installable': True
+}
---
+++ crm_lead_close_lost/models/crm_lead.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) DEC SARL, Inc - All Rights Reserved.
+# Written by Yann Papouin <y.papouin at dec-industrie.com>, Feb 2021
+
+import logging
+
+from odoo import fields, api, models
+
+_logger = logging.getLogger(__name__)
+
+
+class CrmLead(models.Model):
+    _inherit = 'crm.lead'
+
+    @api.multi
+    def action_set_lost(self):
+        res = super().action_set_lost()
+        for lead in self:
+            # Set lost stage when probability is set to 0
+            stage_id = lead._stage_find(domain=[('probability', '<=', 0)])
+            lead.write({'stage_id': stage_id.id})
+        return res
+
+    @api.model
+    def _onchange_stage_id_values(self, stage_id):
+        vals = super()._onchange_stage_id_values(stage_id)
+        # When probability is set to 0, always set active to false, that way
+        # date_closed will be set
+        if 'probability' in vals and vals.get('probability') <= 0:
+            vals['active'] = False
+        return vals
