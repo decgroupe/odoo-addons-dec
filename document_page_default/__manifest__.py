3c57218b8a040839582e700d7e5ef91160c5c264
Yann Papouin <y.papouin@dec-industrie.com>
2021-02-03 10:09:08 +0100

    Auto-fill document name

---
+++ document_page_default/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Document Page Default',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'http://www.dec-industrie.com',
+    'summary': '''Set default draft name and summary''',
+    'depends': [
+        'document_page',
+    ],
+    'data': [
+        'views/document_page.xml',
+    ],
+    'installable': True
+}
---
+++ document_page_default/models/document_page.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) DEC SARL, Inc - All Rights Reserved.
+# Written by Yann Papouin <y.papouin at dec-industrie.com>, Feb 2021
+
+import re
+import string
+
+from odoo import models, fields, api, _
+
+
+class DocumentPage(models.Model):
+    _inherit = 'document.page'
+
+    @api.model
+    def _default_draft_name(self):
+        return "Rev 01"
+
+    @api.model
+    def _default_draft_summary(self):
+        return _("Init")
+
+    draft_name = fields.Char(default=_default_draft_name)
+    draft_summary = fields.Char(default=_default_draft_summary)
+
+    @api.multi
+    def write(self, vals):
+        if not 'draft_name' in vals:
+            vals['draft_name'] = "Rev {:02d}".format(len(self.history_ids) + 1)
+        result = super(DocumentPage, self).write(vals)
+        return result
