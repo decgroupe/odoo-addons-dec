8ff4833c9b6fc15a398b7a53eb21cada8f29f13c
Yann Papouin <y.papouin@dec-industrie.com>
2021-02-08 16:57:45 +0100

    Default analytic account parent for production

--- mrp_project/__manifest__.py
+++ mrp_project/__manifest__.py
@@ -3,13 +3,14 @@
     'version': '12.0.1.0.0',
     'author': 'DEC, Yann Papouin',
     'website': 'http://www.dec-industrie.com',
-    'summary': '---',
+    'summary': 'Add project linking support to production',
     'depends': [
         'mrp',
         'project',
     ],
     'data':
         [
+            'data/analytic_account.xml',
             'views/mrp_production.xml',
             'views/project.xml',
             'views/project_task.xml',
