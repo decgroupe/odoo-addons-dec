8bd9ecbecee1f575be6eb5fa75744ef9a9dac311
Yann Papouin <y.papouin@dec-industrie.com>
2021-02-08 17:02:18 +0100

    Allow project to be merged

---
+++ project_merge/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Merge Projects',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin ',
+    'website': 'http://www.dec-industrie.com',
+    'depends': [
+        'project',
+        'deltatech_merge',
+    ],
+    'data': [
+        'security/res_groups.xml',
+        'wizard/merge_project.xml',
+    ],
+    'installable': True
+}
---
+++ project_merge/wizard/merge_project.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) DEC SARL, Inc - All Rights Reserved.
+# Written by Yann Papouin <y.papouin at dec-industrie.com>, Feb 2021
+
+from odoo import api, fields, models
+
+
+class MergeProject(models.TransientModel):
+    _inherit = "merge.object.wizard"
+    _name = "merge.project.project.wizard"
+    _description = "Merge Project Wizard"
+    _model_merge = "project.project"
+    _table_merge = "project_project"
+
+    object_ids = fields.Many2many(_model_merge, string="Project")
+    dst_object_id = fields.Many2one(_model_merge, string="Project")
+
+    def _merge(self, object_ids, dst_object=None, extra_checks=True):
+        return super()._merge(object_ids, dst_object, extra_checks)
