dc0a6810f40668cbcaafad8fb6cd04b2a2e12246
Yann Papouin <y.papouin@dec-industrie.com>
2021-02-09 12:34:53 +0100

    Avoid multiple rules for the same product

---
+++ stock_orderpoint_unique/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Unique Orderpoint',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'http://www.dec-industrie.com',
+    'summary':
+        "Create a new contraint that will avoid having multiple rules for the "
+        "same product",
+    'depends': [
+        'stock',
+    ],
+    'data': [],
+    'installable': True
+}
---
+++ stock_orderpoint_unique/models/stock_warehouse_orderpoint.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) DEC SARL, Inc - All Rights Reserved.
+# Written by Yann Papouin <y.papouin at dec-industrie.com>, Feb 2021
+
+from odoo import fields, models
+
+
+class StockWarehouseOrderpoint(models.Model):
+    _inherit = 'stock.warehouse.orderpoint'
+
+    # This SQL constraint blocks the use of the "active" field
+    # but I think it's not very useful to have such an "active" field
+    # on orderpoints ; when you think the order point is bad, you update
+    # the min/max values, you don't de-active it !
+    _sql_constraints = [
+        (
+            'company_wh_location_product_unique',
+            'unique(company_id, warehouse_id, location_id, product_id)',
+            'An orderpoint already exists for the same company, same '
+            'warehouse, same stock location and same product.',
+        )
+    ]
