b3b35fa5ea5348518044e03c2062feb74738b3eb
Yann Papouin <y.papouin@dec-industrie.com>
2021-02-23 23:14:13 +0100

    Store user order-by choices to be persistent over updates

---
+++ web_form_orderedby/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Web From Ordered-By',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'http://www.dec-industrie.com',
+    'category': 'Base',
+    'summary': "Store user order-by choices to be persistent over updates",
+    'depends': ['web', ],
+    'data': ['views/assets.xml', ],
+    'installable': True
+}
