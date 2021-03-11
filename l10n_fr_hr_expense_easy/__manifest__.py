c7c55a8d0b463105d32fd2cf63a1831c7f7522b8
Yann Papouin <y.papouin@dec-industrie.com>
2021-03-11 18:39:24 +0100

    Typo fix

--- l10n_fr_hr_expense_easy/__manifest__.py
+++ l10n_fr_hr_expense_easy/__manifest__.py
@@ -1,5 +1,5 @@
 {
-    'name': 'L10n FR Easy expense sheet (HE)r',
+    'name': 'L10n FR Easy expense sheet (HE)',
     'version': '12.0.1.0.0',
     'author': 'DEC, Yann Papouin',
     'website': 'http://www.dec-industrie.com',
