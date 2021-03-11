1af80f6273b99f6cfc3dca2f8e81ae4d1f6254e2
Yann Papouin <y.papouin@dec-industrie.com>
2021-03-11 15:02:10 +0100

    Typo

--- project_timesheet_time_rounding/__manifest__.py
+++ project_timesheet_time_rounding/__manifest__.py
@@ -1,5 +1,5 @@
 {
-    'name': 'Project Timesheet Time Rouding',
+    'name': 'Project Timesheet Time Rounding',
     'version': '12.0.1.0.0',
     'author': 'DEC, Yann Papouin',
     'website': 'http://www.dec-industrie.com',
