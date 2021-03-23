a3837ff5cf68009243cb4fa238166377240f46d3
Yann Papouin <y.papouin@dec-industrie.com>
2021-03-23 14:00:04 +0100

    New model to store multiple hardware identifiers

--- software_license/models/software_license.py
+++ software_license/models/software_license.py
@@ -17,15 +17,29 @@ class SoftwareLicense(models.Model):
         help="If unchecked, it will allow you to hide the license "
         "without removing it.",
     )
-    serial = fields.Char(required=True, )
+    serial = fields.Char(
+        required=True,
+        help="Unique serial used as an authorization identifier",
+    )
     application_id = fields.Many2one(
         'software.license.application',
         'Application',
         required=True,
     )
+    # Renamed to hardware_identifier, a computed field to get data from first
+    # hardware_ids
     hardware_id = fields.Char(
         'Hardware ID',
         oldname='dongle_id_encrypted',
+    )
+    hardware_ids = fields.One2many(
+        comodel_name='software.license.hardware',
+        inverse_name='license_id',
+        string="Hardware Identifiers",
+        copy=False,
+        help="Unique hardware identifiers sent by client application made to "
+        "identify a system. This identifier must not change over time or "
+        "activation related data would be invalidated"
     )
     product_id = fields.Many2one(
         'product.product',
---
+++ software_license/models/software_license_hardware.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) DEC SARL, Inc - All Rights Reserved.
+# Written by Yann Papouin <y.papouin at dec-industrie.com>, Mar 2021
+
+from odoo import api, fields, models
+
+
+class SoftwareLicenseHardware(models.Model):
+    _name = 'software.license.hardware'
+    _description = 'License Hardware'
+    _order = 'id desc'
+
+    license_id = fields.Many2one(
+        'software.license',
+        'License',
+        required=True,
+    )
+    name = fields.Char(
+        required=True,
+        string="Identifier",
+    )
+    info = fields.Text('Informations')
