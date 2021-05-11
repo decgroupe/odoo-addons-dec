467ae5c015347507673a70e2eb1fa8124cf35091
Yann Papouin <y.papouin@dec-industrie.com>
2021-05-11 15:21:35 +0200

    Get a manager with rights for leave management

---
+++ hr_holidays_manager/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'HR Leaves Manager',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'http://www.dec-industrie.com',
+    'summary': "Get employee manager if has specific group",
+    'depends': ['hr_holidays', ],
+    'data': [
+    ],
+    'installable': True
+}
---
+++ hr_holidays_manager/models/hr_leave.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) DEC SARL, Inc - All Rights Reserved.
+# Written by Yann Papouin <y.papouin at dec-industrie.com>, May 2021
+
+from odoo import api, models
+
+
+class HolidaysRequest(models.Model):
+    _inherit = 'hr.leave'
+
+    def _get_employee_leave_manager(self, employee_id):
+        parent_id = employee_id.parent_id
+        is_officer = parent_id.user_id.has_group('hr_holidays.group_hr_holidays_user')
+        is_manager = parent_id.user_id.has_group('hr_holidays.group_hr_holidays_manager')
+        if parent_id:
+            if (is_officer or is_manager):
+                return parent_id
+            else:
+                return self._get_employee_leave_manager(parent_id)
+        else:
+            return False
+
+    @api.multi
+    def _sync_employee_details(self):
+        super()._sync_employee_details()
+        for holiday in self:
+            manager_id = self._get_employee_leave_manager(holiday.employee_id)
+            if manager_id and manager_id != holiday.manager_id:
+                holiday.manager_id = manager_id
