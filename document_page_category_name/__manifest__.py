87471a9fb09ac5699dc0a0f49010d105cb00f389
Yann Papouin <y.papouin@dec-industrie.com>
2021-05-20 09:43:24 +0200

    Display complete category name for documents

---
+++ document_page_category_name/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Document Page Category Complete Name',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'http://www.dec-industrie.com',
+    'summary': "Displays the complete category",
+    'depends': ['document_page', ],
+    'data': ['views/document_page.xml', ],
+    'installable': True
+}
---
+++ document_page_category_name/models/document_page.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) DEC SARL, Inc - All Rights Reserved.
+# Written by Yann Papouin <y.papouin at dec-industrie.com>, May 2021
+
+from odoo import models, fields, api, _
+
+
+class DocumentPage(models.Model):
+    _inherit = 'document.page'
+    _parent_name = "parent_id"
+    _parent_store = True
+    _order = 'complete_name'
+
+    parent_path = fields.Char(index=True)
+    complete_name = fields.Char(
+        'Complete Name',
+        compute='_compute_complete_name',
+        store=True,
+    )
+
+    @api.depends('name', 'parent_id.complete_name')
+    def _compute_complete_name(self):
+        for rec in self:
+            if rec.parent_id and rec.type == 'category':
+                rec.complete_name = '%s / %s' % (
+                    rec.parent_id.complete_name, rec.name,
+                )
+            else:
+                rec.complete_name = rec.name
+
+    @api.multi
+    @api.depends('name', 'complete_name', 'type')
+    def name_get(self):
+        result = []
+        for rec in self:
+            result.append((rec.id, rec.complete_name))
+        return result
