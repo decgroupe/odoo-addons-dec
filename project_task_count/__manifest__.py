1cc362320f47fbf2cd8561eb21a255ab182befdd
Yann Papouin <y.papouin@dec-industrie.com>
2021-06-03 18:15:15 +0200

    New field to know how many task are still active in a project (ignoring the one used for time-tracking)

---
+++ project_task_count/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Project Task Count',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'https://www.dec-industrie.com',
+    'summary': "Store current task count",
+    'depends': [
+        'project',
+        'project_identification',
+    ],
+    'data': [
+        'views/project_project.xml',
+    ],
+    'installable': True
+}
---
+++ project_task_count/models/project.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) DEC SARL, Inc - All Rights Reserved.
+# Written by Yann Papouin <y.papouin at dec-industrie.com>, Jun 2021
+
+from odoo import api, models, fields
+
+
+class Project(models.Model):
+    _inherit = "project.project"
+
+    todo_task_count = fields.Integer(
+        string="To-Do Task Count",
+        compute='_compute_todo_task_count',
+        store=True,
+        help="Number of currently open tasks",
+    )
+
+    @api.multi
+    @api.depends('task_ids', 'task_ids.stage_id', 'task_ids.type_id')
+    def _compute_todo_task_count(self):
+        time_tracking_type = self.env.ref(
+            'project_identification.time_tracking_type'
+        )
+        task_data = self.env['project.task'].read_group(
+            [
+                ('project_id', 'in', self.ids),
+                '|',
+                ('stage_id.fold', '=', False),
+                ('stage_id', '=', False),
+                '|',
+                ('type_id', '=', False),
+                ('type_id', '!=', time_tracking_type.id),
+            ], ['project_id'], ['project_id']
+        )
+        result = dict(
+            (data['project_id'][0], data['project_id_count'])
+            for data in task_data
+        )
+        for rec in self:
+            rec.todo_task_count = result.get(rec.id, 0)
