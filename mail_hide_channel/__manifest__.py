a8a9fcec1f35056df3b57958f8b44e0b15fb67fd
Yann Papouin <y.papouin@dec-industrie.com>
2021-06-29 18:24:44 +0200

    Option to hide a channel from messaging system

---
+++ mail_hide_channel/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Mail Hide Channel',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'http://www.dec-industrie.com',
+    'summary': "Hide a channel from messaging system",
+    'depends': ['mail', ],
+    'data': ['views/mail_channel.xml', ],
+    'installable': True
+}
