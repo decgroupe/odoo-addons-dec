0e41859eb07e0c1d747e2d194304040be872c750
Yann Papouin <ypa@decgroupe.com>
2021-07-06 16:31:29 +0200

    Allow to add a custom prefix

---
+++ mail_channel_prefix/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Mail Channel Prefix',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'http://www.dec-industrie.com',
+    'summary': "Add a custom prefix to e-mail subject",
+    'depends': ['mail', ],
+    'data': ['views/mail_channel.xml', ],
+    'installable': True
+}
---
+++ mail_channel_prefix/models/mail_channel.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) DEC SARL, Inc - All Rights Reserved.
+# Written by Yann Papouin <y.papouin at dec-industrie.com>, Jul 2021
+
+import logging
+
+from odoo import api, models, _, fields
+
+_logger = logging.getLogger(__name__)
+
+
+class MailChannel(models.AbstractModel):
+    _inherit = 'mail.channel'
+
+    subject_prefix = fields.Char(string="Subject's Prefix")
+
+    @api.multi
+    @api.returns('mail.message', lambda value: value.id)
+    def message_post(self, message_type='notification', **kwargs):
+        if self.subject_prefix and 'subject' in kwargs:
+            kwargs['subject'] = '%s %s' % (
+                self.subject_prefix,
+                kwargs['subject'],
+            )
+        return super().message_post(message_type=message_type, **kwargs)
