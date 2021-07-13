0a5c8a006aec691379c04d8e02c3fdfa7c501194
Yann Papouin <ypa@decgroupe.com>
2021-07-13 15:29:17 +0200

    Use alternate signature on reply

--- res_users_signature/models/__init__.py
+++ res_users_signature/models/__init__.py
@@ -2,3 +2,4 @@ from . import res_users
 from . import res_users_signature_template
 from . import hr_employee
 from . import hr_department
+from . import res_partner
---
+++ res_users_signature/models/res_partner.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) DEC SARL, Inc - All Rights Reserved.
+# Written by Yann Papouin <y.papouin at dec-industrie.com>, Jul 2021
+
+from odoo import fields, models, api
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    @api.multi
+    def action_generate_signatures(self):
+        global_template = self.env.ref(
+            'res_users_signature.user_signature_template'
+        )
+        for user in self:
+            template = user.signature_template or global_template
+            user._generate_from_template(template)
+
+    @api.model
+    def _notify_prepare_template_context(
+        self, message, record, model_description=False, mail_auto_delete=True
+    ):
+        res = super()._notify_prepare_template_context(
+            message,
+            record,
+            model_description,
+            mail_auto_delete,
+        )
+        if res.get('signature') and message.email_to != False:
+            if message.author_id and message.author_id.user_ids:
+                user = message.author_id.user_ids[0]
+                if message.add_sign and user.signature_answer:
+                    res['signature'] = user.signature_answer
+        return res
