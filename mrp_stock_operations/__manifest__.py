0168d3c465db8d3cc149711d1233c627fa43d122
Yann Papouin <ypa@decgroupe.com>
2021-09-03 17:18:48 +0200

    Stock operation for production orders like the one existing for pickings

---
+++ mrp_stock_operations/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Manufacturing Stock Operations',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'http://www.dec-industrie.com',
+    'summary': "Allow to open an advance view to edit moves like pickings",
+    'depends': [
+        'mrp',
+        'sale_stock',
+        'stock_assign',
+    ],
+    'data': ['views/mrp_production.xml', ],
+    'installable': True
+}
---
+++ mrp_stock_operations/models/mrp_production.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) DEC SARL, Inc - All Rights Reserved.
+# Written by Yann Papouin <y.papouin at dec-industrie.com>, Sep 2020
+
+from datetime import datetime
+
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError
+from odoo.tools import float_compare, float_round
+
+class MrpProduction(models.Model):
+    _inherit = 'mrp.production'
