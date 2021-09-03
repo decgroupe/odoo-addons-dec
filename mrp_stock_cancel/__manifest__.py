898c01dd627be742914d981b27d8138548626f32
Yann Papouin <ypa@decgroupe.com>
2021-09-03 17:18:07 +0200

    Ban icon and confirmation added for the action to cancel moves

--- mrp_stock_cancel/__manifest__.py
+++ mrp_stock_cancel/__manifest__.py
@@ -1,20 +1,16 @@
 {
-    'name':
-        'Manufacturing Stock Cancel',
-    'version':
-        '12.0.1.0.0',
-    'author':
-        'DEC, Yann Papouin',
-    'website':
-        'http://www.dec-industrie.com',
+    'name': 'Manufacturing Stock Cancel',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'http://www.dec-industrie.com',
     'summary':
-        '''Allow to cancel a stock move from a production order when unlocked''',
+        "Allow to cancel a stock move from a production order "
+        "when unlocked",
     'depends': [
         'mrp',
         'sale_stock',
         'stock_cancel',
     ],
     'data': ['views/mrp_production.xml', ],
-    'installable':
-        True
+    'installable': True
 }
