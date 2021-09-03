a2a5130f02e3fdb1523b4ffa169bdf5021c751d5
Yann Papouin <ypa@decgroupe.com>
2021-09-03 17:15:30 +0200

    Basic safety added to not auto-validate next moves when initial move quantity is zero

--- stock_auto_validate/models/stock_move.py
+++ stock_auto_validate/models/stock_move.py
@@ -2,7 +2,8 @@
 # Copyright (C) DEC SARL, Inc - All Rights Reserved.
 # Written by Yann Papouin <y.papouin at dec-industrie.com>, Mar 2020
 
-from odoo import  fields, models
+from odoo import fields, models
+from odoo.tools.float_utils import float_is_zero
 
 
 class StockMove(models.Model):
@@ -17,15 +18,28 @@ class StockMove(models.Model):
 
     def _action_done(self):
         res = super()._action_done()
-        auto_validated_moves = res.\
+        for move in res:
+            move._action_auto_validate()
+        return res
+
+    def _action_auto_validate(self):
+        self.ensure_one()
+        auto_validated_moves = self.\
             mapped('move_dest_ids').\
             filtered(lambda m: m.auto_validate and m.state == 'assigned')
-        for move in auto_validated_moves:
-            # Apply logic from addons/stock/wizard/stock_immediate_transfer.py
-            # and process every move lines
-            for move_line in move.move_line_ids:
-                move_line.qty_done = move_line.product_uom_qty
-        # Finally call action_done
-        if auto_validated_moves:
-            auto_validated_moves._action_done()
-        return res
+        # If initial quantity changed to 0, then do not auto_validate
+        # descendants moves
+        if float_is_zero(
+            self.product_uom_qty,
+            precision_rounding=self.product_id.uom_id.rounding
+        ):
+            auto_validated_moves._do_unreserve()
+        else:
+            for move in auto_validated_moves:
+                # Apply logic from addons/stock/wizard/stock_immediate_transfer.py
+                # and process every move lines
+                for move_line in move.move_line_ids:
+                    move_line.qty_done = move_line.product_uom_qty
+            # Finally call action_done
+            if auto_validated_moves:
+                auto_validated_moves._action_done()
