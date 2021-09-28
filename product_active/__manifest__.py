dd1383c1b70312425538072f5083db02cb1737ae
Yann Papouin <ypa@decgroupe.com>
2021-09-28 13:48:37 +0200

    Manifest comment

--- product_active/__manifest__.py
+++ product_active/__manifest__.py
@@ -1,5 +1,5 @@
 {
-    'name': 'Product active',
+    'name': 'Product active', # Duplicated of OCA product_active_propagate ?
     'version': '12.0.1.0.0',
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
