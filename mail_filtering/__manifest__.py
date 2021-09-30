4419b8b2b3459ffddc38cf83ca686e4c12b559c5
Yann Papouin <ypa@decgroupe.com>
2021-09-30 14:20:07 +0200

    Typo fix

--- mail_filtering/__manifest__.py
+++ mail_filtering/__manifest__.py
@@ -3,7 +3,7 @@
     'version': '12.0.1.0.0',
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
-    'summary': '''Hard-coded database filtering''',
+    'summary': "Hard-coded database filtering",
     'depends': ['mail', ],
     'data': [],
     'installable': True
