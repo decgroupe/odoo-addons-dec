8875574fd69b29ba7916eb3aad550bc38922c7eb
Yann Papouin <ypa@decgroupe.com>
2021-11-09 18:58:48 +0100

    Inverse field added to get contracts from project

---
+++ sale_timesheet_project/models/project_project.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) DEC SARL, Inc - All Rights Reserved.
+# Written by Yann Papouin <ypa at decgroupe.com>, Nov 2021
+
+from odoo import models, api, fields
+
+
+class Project(models.Model):
+    _inherit = "project.project"
+
+    contract_confirmation_date = fields.Datetime(
+        string='Contract Confirmation Date',
+        help="Date on which the contract was confirmed.",
+        compute="_compute_contract_confirmation_date",
+        store=True,
+    )
+    contract_ids = fields.One2many(
+        comodel_name="sale.order",
+        inverse_name="project_id",
+        string="Contract",
+    )
+
+    @api.multi
+    @api.depends("contract_ids", "contract_ids.confirmation_date")
+    def _compute_contract_confirmation_date(self):
+        for rec in self:
+            if rec.contract_ids:
+                contract_id = rec.contract_ids[0]
+                rec.contract_confirmation_date = contract_id.confirmation_date
+            else:
+                rec.contract_confirmation_date = False
