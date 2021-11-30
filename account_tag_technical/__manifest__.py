245c41bf33c057bd23997d7f4ec7bee160cdbfb5
Yann Papouin <ypa@decgroupe.com>
2021-11-30 10:23:23 +0100

    Add XML-ID to account tag names when debug is active

---
+++ account_tag_technical/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Account Tag Technical',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'https://www.decgroupe.com',
+    'summary': "Show technical XML IDs",
+    'depends': [
+        'account_tag_menu',
+        'base_xmlid',
+    ],
+    'data': [],
+    'installable': True
+}
---
+++ account_tag_technical/models/account_tag.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) DEC SARL, Inc - All Rights Reserved.
+# Written by Yann Papouin <ypa at decgroupe.com>, Nov 2021
+
+from odoo import api, models
+
+
+class AccountAccountTag(models.Model):
+    _inherit = 'account.account.tag'
+
+    @api.multi
+    @api.depends('name')
+    def name_get(self):
+        """
+        """
+        super_res = super().name_get()
+        res = []
+        if self.user_has_groups('base.group_no_one'):
+            ModelData = self.env["ir.model.data"]
+            for item in super_res:
+                rec = self.browse(item[0])[0]
+                xml_module, xml_name = ModelData.get_xmlid(rec)
+                if xml_name:
+                    name = "%s [%s]" % (item[1], xml_name)
+                else:
+                    name = item[1]
+                res.append((rec.id, name))
+        return res or super_res
