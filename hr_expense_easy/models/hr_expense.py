fd93e34308f7d8be68c705a0c7960efaf261669c
Yann Papouin <ypa@decgroupe.com>
2021-12-13 17:58:09 +0100

    Quick attachment create Duplicate button action added

--- hr_expense_easy/models/hr_expense.py
+++ hr_expense_easy/models/hr_expense.py
@@ -2,8 +2,40 @@
 # Copyright (C) DEC SARL, Inc - All Rights Reserved.
 # Written by Yann Papouin <ypa at decgroupe.com>, Dec 2020
 
-from odoo import api, models
+from odoo import _, api, models
 
 
 class HrExpense(models.Model):
     _inherit = "hr.expense"
+
+    @api.multi
+    def action_duplicate(self):
+        self.ensure_one()
+        self.copy(default={
+            'sheet_id': self.sheet_id.id,
+        })
+
+    @api.multi
+    def action_get_attachment_view(self):
+        self.ensure_one()
+        if self.attachment_number == 0:
+            res = self.action_create_attachment_view()
+        else:
+            res = super().action_get_attachment_view()
+        return res
+
+    @api.multi
+    def action_create_attachment_view(self):
+        self.ensure_one()
+        context = {'default_res_model': self._name, 'default_res_id': self.id}
+        domain = [('res_model', '=', self._name), ('res_id', 'in', self.ids)]
+        return {
+            'name': _('Create attachment'),
+            'type': 'ir.actions.act_window',
+            'view_type': 'form',
+            'view_mode': 'form',
+            'target': 'new',
+            'res_model': "ir.attachment",
+            'context': context,
+            'domain': domain,
+        }
--- hr_expense_easy/models/hr_expense_sheet.py
+++ hr_expense_easy/models/hr_expense_sheet.py
@@ -2,8 +2,52 @@
 # Copyright (C) DEC SARL, Inc - All Rights Reserved.
 # Written by Yann Papouin <ypa at decgroupe.com>, Dec 2020
 
-from odoo import api, models
+from dateutil.relativedelta import relativedelta
+
+from odoo import _, api, fields, models
 
 
 class HrExpenseSheet(models.Model):
     _inherit = "hr.expense.sheet"
+
+    @api.model
+    def _get_default_name(self):
+        """ Return a 'standardized' name for sheets:
+            - if we are between the 11th november and the 10th december, the
+                name will be: 2021/11-November
+            - if we are between the 11th december and the 10th january, the
+                name will be: 2021/12-December
+        """
+        date = fields.Date.today() - relativedelta(days=10)
+        prefix = date.strftime("%Y/%m")
+        if date.month == 1:
+            suffix = _('January')
+        elif date.month == 2:
+            suffix = _('February')
+        elif date.month == 3:
+            suffix = _('March')
+        elif date.month == 4:
+            suffix = _('April')
+        elif date.month == 5:
+            suffix = _('May')
+        elif date.month == 6:
+            suffix = _('June')
+        elif date.month == 7:
+            suffix = _('July')
+        elif date.month == 8:
+            suffix = _('August')
+        elif date.month == 9:
+            suffix = _('September')
+        elif date.month == 10:
+            suffix = _('October')
+        elif date.month == 11:
+            suffix = _('November')
+        elif date.month == 12:
+            suffix = _('December')
+        else:
+            suffix = date.strftime("%B")
+
+        return "%s-%s" % (prefix, suffix)
+
+    # Override existing field definition with a default value
+    name = fields.Char(default=_get_default_name, )
