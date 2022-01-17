ad5446717318be16c72d5976e024d6445202e359
Yann Papouin <ypa@decgroupe.com>
2022-01-17 09:16:27 +0100

    Added a way to make a fence between a contact and its commercial parent

---
+++ partner_commercial_fencing/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Partner Commercial Fencing',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'https://www.decgroupe.com',
+    'summary':
+        "Add an option to hide business documents in portal "
+        "to specific contacts",
+    'depends': ['base', ],
+    'data': ['views/partner.xml', ],
+    'installable': True
+}
---
+++ partner_commercial_fencing/models/res_partner.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) DEC SARL, Inc - All Rights Reserved.
+# Written by Yann Papouin <ypa at decgroupe.com>, Jan 2022
+
+from odoo import _, api, models, fields
+
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    inherit_commercial_partner = fields.Boolean(
+        string='Inherit commercial contact from the parent/company',
+        default=True,
+        help="If unchecked, then this partner will not be able to see "
+        "business documents from its commercial partner.",
+    )
+
+    @api.depends(
+        'is_company', 'inherit_commercial_partner',
+        'parent_id.inherit_commercial_partner'
+    )
+    def _compute_commercial_partner(self):
+        super()._compute_commercial_partner()
+        # for partner in self.filtered(lambda x: not x.is_company):
+        for rec in self:
+            if not rec.inherit_commercial_partner and not rec.is_company:
+                rec.commercial_partner_id = rec
+                # rec.commercial_partner_id = getattr(self, '_origin', self)
