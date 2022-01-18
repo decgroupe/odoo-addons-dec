83fa20235b66bf62215831289a0a5973db5c29d1
Yann Papouin <ypa@decgroupe.com>
2022-01-18 16:41:30 +0100

    Improvements of licenses view search using partner location

---
+++ software_license_location/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Software License Location',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'https://www.decgroupe.com',
+    'summary': "Add shipping partner location to licenses",
+    'depends': [
+        'base_location',
+        'software_license',
+    ],
+    'data': ['views/software_license.xml', ],
+    'installable': True
+}
---
+++ software_license_location/models/software_license_pass.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) DEC SARL, Inc - All Rights Reserved.
+# Written by Yann Papouin <ypa at decgroupe.com>, Jan 2022
+
+from odoo import fields, models
+
+
+class SoftwareLicense(models.Model):
+    _inherit = 'software.license'
+
+    partner_zip_id = fields.Many2one(
+        'res.city.zip',
+        related='partner_id.zip_id',
+        string="Partner's ZIP",
+        store=True,
+    )
+    partner_city_id = fields.Many2one(
+        'res.city',
+        related='partner_id.city_id',
+        string="Partner's City",
+        store=True,
+    )
