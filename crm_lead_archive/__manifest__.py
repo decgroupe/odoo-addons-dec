bf26d14abfe52a4259b765748e299db619366f51
Yann Papouin <ypa@decgroupe.com>
2022-02-10 16:36:55 +0100

    Add archive action to crm leads

---
+++ crm_lead_archive/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'CRM Archive',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'https://www.decgroupe.com',
+    'summary': "Archiving actions for better usability",
+    'depends': ['crm', ],
+    'data': ['views/crm_lead.xml', ],
+    'installable': True
+}
