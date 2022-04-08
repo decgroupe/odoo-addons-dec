fd2b14b9c733e9d81e456142c3fccb36affc7033
Yann Papouin <ypa@decgroupe.com>
2022-04-08 14:20:24 +0200

    [IMP] mail_activity_forecast_dhxgantt: Support for `mail_activity_partner` and `mail_activity_project` modules

--- mail_activity_forecast_dhxgantt/static/src/js/mail_activity_gantt_model.js
+++ mail_activity_forecast_dhxgantt/static/src/js/mail_activity_gantt_model.js
@@ -10,14 +10,22 @@ odoo.define('mail_activity_forecast_dhxgantt.MailActivityGanttModel', function (
             values.push("res_model");
             values.push("activity_type_id");
             values.push("icon");
+            // From `mail_activity_partner` module
+            values.push("partner_id");
+            // From `mail_activity_project` module
+            values.push("project_id");
             return values;
         },
-        create_task: function (rec, ganttGroups, groupBy, links, css_classes) {
+        createTask: function (rec, ganttGroups, groupBy, links, css_classes) {
             var task = this._super(rec, ganttGroups, groupBy, links, css_classes);
             task.overrideModelName = rec["res_model"];
             task.overrideModelId = rec["res_id"];
-
-            task.columnTitle = rec["activity_type_id"][1] + ": " + `<b>${task.columnTitle}</b>`;
+            var type = rec["activity_type_id"][1];
+            if (task.columnTitle) {
+                task.columnTitle = `${type}:<small><b> ${task.columnTitle}</b></small>`;
+            } else {
+                task.columnTitle = type;
+            }
             task.columnTitle = `<i class="fa ${rec["icon"]}"></i> ${task.columnTitle}`;
             return task;
         }
