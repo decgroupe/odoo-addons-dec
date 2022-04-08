8f037cbf7308d83e5356e81e706318d0a6f72e95
Yann Papouin <ypa@decgroupe.com>
2022-04-08 14:19:47 +0200

    [FORECAST] `project_id` related field added to activities

---
+++ mail_activity_project/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Mail Activity Project',
+    'summary': "Add Project to Activities",
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'category': 'Social Network',
+    'website': 'https://github.com/OCA/social',
+    'license': 'AGPL-3',
+    'depends': ['mail_activity_board', ],
+    "data": ['views/mail_activity.xml', ],
+    'qweb': [],
+    'installable': True
+}
