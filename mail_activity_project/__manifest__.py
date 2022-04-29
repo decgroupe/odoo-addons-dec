d3dbd709189d9e2196c22f3e1c681d134a328cb8
Yann Papouin <ypa@decgroupe.com>
2022-04-29 15:26:00 +0200

    [ADD] mail_activity_project: `project_id` related field added to activities

---
+++ mail_activity_project/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Mail Activity Project',
+    'summary': "Add Project to Activities",
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'category': 'Social Network',
+    'website': 'https://github.com/OCA/social',
+    'license': 'AGPL-3',
+    'depends': [
+        'project',
+        'mail_activity_board',
+    ],
+    "data": ['views/mail_activity.xml', ],
+    'qweb': [],
+    'installable': True
+}
---
+++ mail_activity_project/models/mail_activity.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) DEC SARL, Inc - All Rights Reserved.
+# Written by Yann Papouin <ypa at decgroupe.com>, Apr 2022
+
+import logging
+from odoo import models, api, fields
+
+_logger = logging.getLogger(__name__)
+
+
+class MailActivity(models.Model):
+    _inherit = 'mail.activity'
+
+    project_id = fields.Many2one(
+        comodel_name='project.project',
+        index=True,
+        compute='_compute_project_id',
+        store=True,
+    )
+
+    @api.depends('res_model', 'res_id')
+    def _compute_project_id(self):
+        for obj in self:
+            res_model = obj.res_model
+            res_id = obj.res_id
+            if not res_model or not res_id:
+                _logger.error(
+                    "Activity %d is missing a model/id "
+                    "(res_model=%s, res_id=%d)", obj.id, res_model, res_id
+                )
+                continue
+            if res_model == 'project.project':
+                obj.project_id = res_id
+            else:
+                res_model_id = obj.env[res_model].search([('id', '=', res_id)])
+                if 'project_id' in res_model_id._fields and \
+                        res_model_id.project_id:
+                    obj.project_id = res_model_id.project_id
+                else:
+                    obj.project_id = None
