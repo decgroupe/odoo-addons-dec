13f488be4e959ffe1435351202c3a2cb77218406
Yann Papouin <ypa@decgroupe.com>
2022-05-04 14:28:31 +0200

    [FORECAST] Forecast support for mrp

---
+++ mrp_forecast/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Manufacturing Forecast',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'https://www.decgroupe.com',
+    'summary': "Forecast",
+    'depends': ['mail_activity_forecast', ],
+    'data': ['views/mrp_production.xml', ],
+    'installable': True
+}
