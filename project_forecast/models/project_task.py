07cc55fab8b144bab27659cbd6af98ff48b3bd98
Yann Papouin <ypa@decgroupe.com>
2022-05-04 16:01:34 +0200

    [FORECAST] Forecast support added to projects and tasks

---
+++ project_forecast/models/project_task.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) DEC SARL, Inc - All Rights Reserved.
+# Written by Yann Papouin <ypa at decgroupe.com>, May 2022
+
+from odoo import api, fields, models
+
+
+class ProjectTask(models.Model):
+    _inherit = ['project.task', 'mail.activity.forecast.mixin']
+    _name = 'project.task'
+
+    def _get_forecast_date_fields(self):
+        res = super()._get_forecast_date_fields()
+        res.update(
+            {
+                'start': 'date_start',
+                'stop': 'date_end',
+                'deadline': 'date_deadline',
+            }
+        )
+        return res
