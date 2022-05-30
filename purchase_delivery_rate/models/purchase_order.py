04ce21d4070022920e9073e2bf4f63b44920b9a6
Yann Papouin <ypa@decgroupe.com>
2022-05-30 16:38:33 +0200

    Fix invalid picked_rate computation for purchase orders

--- purchase_delivery_rate/models/purchase_order.py
+++ purchase_delivery_rate/models/purchase_order.py
@@ -3,6 +3,7 @@
 # Written by Yann Papouin <ypa at decgroupe.com>, Dec 2020
 
 from odoo import models, api, fields
+from odoo.tools import float_compare
 
 
 class PurchaseOrder(models.Model):
@@ -15,15 +16,29 @@ class PurchaseOrder(models.Model):
     )
 
     @api.multi
-    @api.depends('picking_ids', 'picking_ids.move_lines')
+    @api.depends('state', 'order_line.qty_received', 'order_line.product_qty')
     def _compute_picked_rate(self):
+        precision = self.env['decimal.precision'].precision_get(
+            'Product Unit of Measure'
+        )
         for purchase in self:
-            all_move_ids = purchase.picking_ids.mapped('move_lines').filtered(
-                lambda x: x.state != 'cancel'
-            )
-            if all_move_ids:
-                received_move_ids = all_move_ids.filtered(
-                    lambda x: x.state == 'done'
-                )
+            received_count = 0
+            line_count = 0
+            purchase.picked_rate = 0
+            for line in purchase.order_line:
+                if line.product_type in (
+                    'consu', 'product'
+                ) and line.product_qty > 0:
+                    line_count += 1
+                    if float_compare(
+                        line.qty_received,
+                        line.product_qty,
+                        precision_digits=precision
+                    ) >= 0:
+                        received_count += 1
+            if line_count > 0:
                 purchase.picked_rate = \
-                    len(received_move_ids) * 100 / len(all_move_ids)
+                    received_count * 100 / line_count
+
+    def action_update_picked_rate(self):
+        self._compute_picked_rate()
