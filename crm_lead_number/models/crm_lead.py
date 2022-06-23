f15a7a884b2d89350c9aea0c2042b3e29ec7c3b5
Yann Papouin <ypa@decgroupe.com>
2022-06-23 15:46:39 +0200

    Custom fields for complete name and search name

--- crm_lead_number/models/crm_lead.py
+++ crm_lead_number/models/crm_lead.py
@@ -11,11 +11,23 @@ _logger = logging.getLogger(__name__)
 
 class CrmLead(models.Model):
     _inherit = 'crm.lead'
+    _rec_name_get = 'complete_name'
+    _rec_name_search = "search_name"
 
     number = fields.Char(
         string='Opportunity Number',
         default="/",
         readonly=True,
+    )
+    complete_name = fields.Char(
+        string='Complete Name',
+        compute='_compute_names',
+        store=True,
+    )
+    search_name = fields.Char(
+        string='Search Name',
+        compute='_compute_names',
+        store=True,
     )
 
     def _prepare_number(self, values=None):
@@ -38,3 +50,38 @@ class CrmLead(models.Model):
         res = super().write(vals)
         self._init_number(vals)
         return res
+
+    @api.multi
+    @api.depends(
+        'name', 'number', 'email_from', 'partner_id', 'partner_id.name'
+    )
+    def _compute_names(self):
+        for rec in self:
+            rec.complete_name = "{} {}".format(rec.number, rec.name)
+            rec.search_name = rec.complete_name
+            if rec.email_from:
+                rec.search_name = "{} {}".format(
+                    rec.search_name, rec.email_from
+                )
+            if rec.partner_id:
+                rec.search_name = "{} {}".format(
+                    rec.search_name, rec.partner_id.display_name
+                )
+
+    @api.multi
+    def name_get(self):
+        original_rec_name = self._rec_name
+        self._rec_name = self._rec_name_get
+        result = super(CrmLead, self).name_get()
+        self._rec_name = original_rec_name
+        return result
+
+    @api.model
+    def name_search(self, name='', args=None, operator='ilike', limit=100):
+        original_rec_name = self._rec_name
+        self._rec_name = self._rec_name_search
+        result = super().name_search(
+            name=name, args=args, operator=operator, limit=limit
+        )
+        self._rec_name = original_rec_name
+        return result
