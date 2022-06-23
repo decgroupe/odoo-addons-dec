10beaeba66ae056fc150007e4b9c6901ff1d62bf
Yann Papouin <ypa@decgroupe.com>
2022-06-23 15:48:50 +0200

    Add an identification field for opportunity when filling a timesheet

---
+++ crm_timesheet_identification/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'CRM Timesheet Identification',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'https://www.decgroupe.com',
+    'summary': "Add extra data to identify opportunity entry",
+    'depends': [
+        'crm_timesheet',
+        'crm_lead_identification',
+    ],
+    'data': ['views/account_analytic_line.xml', ],
+    'installable': True
+}
---
+++ crm_timesheet_identification/models/account_analytic_line.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) DEC SARL, Inc - All Rights Reserved.
+# Written by Yann Papouin <ypa at decgroupe.com>, Jun 2022
+
+from odoo import api, fields, models
+
+
+class AccountAnalyticLine(models.Model):
+    _inherit = 'account.analytic.line'
+
+    lead_identification = fields.Char(
+        string="CRM Identification",
+        compute="_compute_lead_identification",
+    )
+
+    @api.multi
+    @api.depends('lead_id')
+    def _compute_lead_identification(self):
+        for rec in self.filtered('lead_id'):
+            identifications = rec.lead_id._get_name_identifications()
+            rec.lead_identification = ' / '.join(identifications)
