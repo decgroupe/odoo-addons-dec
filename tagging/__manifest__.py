8342c50519a912495b4f1919db21efe4cfa00a56
Yann Papouin <ypa@decgroupe.com>
2022-06-29 12:02:47 +0200

    No more default menu to remove dependency to `dec` module

--- tagging/__manifest__.py
+++ tagging/__manifest__.py
@@ -1,18 +1,16 @@
 {
-    'name': 'Tagging',
+    'name': 'Tagging (base)',
     'version': '12.0.1.0.0',
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
-    'summary': '''Tagging support for every models''',
+    'summary':" Tagging support for every models",
     'depends': [
         'base',
-        'dec',
     ],
     'data':
         [
             'security/ir.model.access.csv',
             'views/tagging.xml',
-            'views/menu.xml',
         ],
     'installable': True
 }
