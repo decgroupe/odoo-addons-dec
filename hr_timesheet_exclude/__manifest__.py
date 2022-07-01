c3a6e2a129bc217715da2b5f9ebce07a4e7cc727
Yann Papouin <ypa@decgroupe.com>
2022-07-01 11:47:01 +0200

    Exclude from sale order checkbox added to timesheet form view

--- hr_timesheet_exclude/__manifest__.py
+++ hr_timesheet_exclude/__manifest__.py
@@ -4,11 +4,17 @@
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
     'summary': "Select task/projects that should be excluded from analysis",
-    'depends': ['hr_timesheet', ],
-    'data': [
-        'views/project_task.xml',
-        'views/project_project.xml',
-        'views/account_analytic_line.xml',
-    ],
+    'depends':
+        [
+            'hr_timesheet',
+            'sale_timesheet_line_exclude',
+            'sale_timesheet_task_exclude',
+        ],
+    'data':
+        [
+            'views/project_task.xml',
+            'views/project_project.xml',
+            'views/account_analytic_line.xml',
+        ],
     'installable': True
 }
