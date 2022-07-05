12e75a70cde567897c7eb0eab335cc3fe6f50a7a
Yann Papouin <ypa@decgroupe.com>
2022-07-05 17:03:16 +0200

    Picking option for REFManager

---
+++ product_picking/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Product Picking',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'https://www.decgroupe.com',
+    'summary': "Remember product picking options",
+    'depends': [
+        'product',
+    ],
+    'data': [
+        'views/product_template.xml',
+        'views/product_product.xml',
+    ],
+    'installable': True
+}
---
+++ product_picking/models/product_template.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) DEC SARL, Inc - All Rights Reserved.
+# Written by Yann Papouin <ypa at decgroupe.com>, Jul 2022
+
+from odoo import models, fields
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    picking_uom = fields.Selection(
+        selection=[
+            ('default_uom', 'Default UoM'),
+            ('purchase_uom', 'Purchase UoM'),
+        ],
+        string='Picking UoM',
+        default=False,
+    )
