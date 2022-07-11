78e2389b5a3349117c587f919053a0bbbda0f48b
Yann Papouin <ypa@decgroupe.com>
2022-07-11 12:40:16 +0200

    Base module to hook notify data

---
+++ mail_extra_notify/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Mail Extra Notify',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'https://www.decgroupe.com',
+    'summary': "Add extra data in assignation message",
+    'depends': ['mail'],
+    'data': ['data/mail.xml', ],
+    'installable': True
+}
