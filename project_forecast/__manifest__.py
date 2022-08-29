37b85a43621268ce7eb0315274f8857e0c71bb25
Yann Papouin <ypa@decgroupe.com>
2022-08-29 09:24:53 +0200

    [IMP] project_forecast: Hook on module install to auto-create needed activities

--- project_forecast/__manifest__.py
+++ project_forecast/__manifest__.py
@@ -1,10 +1,15 @@
 {
-    'name': 'Manufacturing Forecast',
+    'name': 'Project Forecast',
     'version': '12.0.1.0.0',
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
     'summary': "Forecast",
-    'depends': ['mail_activity_forecast', ],
+    'depends': [
+        'mail_activity_forecast',
+        'project_activity',
+    ],
     'data': [],
+    'force_post_init_hook': True,
+    'post_init_hook': 'post_init_hook',
     'installable': True
 }
---
+++ project_forecast/hooks.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) DEC SARL, Inc - All Rights Reserved.
+# Written by Yann Papouin <ypa at decgroupe.com>, Aug 2022
+
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
+def post_init_hook(cr, registry):
+    from odoo import api, SUPERUSER_ID
+    env = api.Environment(cr, SUPERUSER_ID, {})
+
+    project_ids = env['project.project'].search([])
+    project_ids.with_context(
+        mail_activity_quick_update=True,
+        tracking_disable=True,
+    )._ensure_scheduling_activity()
+
+    task_ids = env['project.project'].search([])
+    task_ids.with_context(
+        mail_activity_quick_update=True,
+        tracking_disable=True,
+    )._ensure_scheduling_activity()
