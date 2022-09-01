6c6e15cefc850cf1290eb98d09c271a85946b895
Yann Papouin <ypa@decgroupe.com>
2022-09-01 14:33:51 +0200

    Extended mrp_stage to add sale informations

--- mrp_sale/__manifest__.py
+++ mrp_sale/__manifest__.py
@@ -3,9 +3,10 @@
     'version': '12.0.1.0.0',
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
-    'summary': '''Get partner id from sale order shipping informations''',
+    'summary': "Get partner id from sale order shipping informations",
     'depends': [
         'mrp_partner',
+        'mrp_stage',
         'sale_stock',
         'sale_mrp_link',
     ],
