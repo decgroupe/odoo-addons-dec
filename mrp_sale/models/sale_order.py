bc3c0256868def600847895812ca47d9f2f1977e
Yann Papouin <ypa@decgroupe.com>
2022-09-05 15:23:49 +0200

    Open mrp kanban view directly from sale order

---
+++ mrp_sale/models/sale_order.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) DEC SARL, Inc - All Rights Reserved.
+# Written by Yann Papouin <ypa at decgroupe.com>, Sep 2022
+
+from odoo import api, fields, models
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    @api.multi
+    def action_view_production(self):
+        """ Override `action_view_production` from `sale_mrp_link` to use the
+            kanban view from `mrp_stage`
+        """
+        return self.production_ids.action_staged_view()
