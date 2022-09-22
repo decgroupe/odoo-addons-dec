0d823668dce4f10ce19cc16246ecbd357c833324
Yann Papouin <ypa@decgroupe.com>
2022-09-22 10:32:53 +0200

    [ADD] mail_disable_notify: Don't send a notification (e-mail/chat) when a specific context key is set

---
+++ mail_disable_notify/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'Mail Disable Notify',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'https://www.decgroupe.com',
+    'summary': "Don't send an e-mail when a specific context key is set",
+    'depends': ['mail', ],
+    'installable': True
+}
---
+++ mail_disable_notify/models/res_partner.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) DEC SARL, Inc - All Rights Reserved.
+# Written by Yann Papouin <ypa at decgroupe.com>, Sep 2022
+
+import logging
+
+from odoo import fields, models, api, tools
+
+_logger = logging.getLogger(__name__)
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    @api.multi
+    def _notify(
+        self,
+        message,
+        rdata,
+        record,
+        force_send=False,
+        send_after_commit=True,
+        model_description=False,
+        mail_auto_delete=True
+    ):
+        if self.env.context.get('mail_partner_notify_disable') \
+          or self.env.context.get('mail_partner_notify_disable_email'):
+            _logger.info("📧 E-Mail notification disabled")
+            return True
+        else:
+            return super(ResPartner, self)._notify(
+                message,
+                rdata=rdata,
+                record=record,
+                force_send=force_send,
+                send_after_commit=send_after_commit,
+                model_description=model_description,
+                mail_auto_delete=mail_auto_delete
+            )
+
+    @api.multi
+    def _notify_by_chat(self, message):
+        if self.env.context.get('mail_partner_notify_disable') \
+          or self.env.context.get('mail_partner_notify_disable_chat'):
+            _logger.info("💬 Chat notification disabled")
+            return
+        else:
+            return super()._notify_by_chat(message)
