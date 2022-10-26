ba3441850665be77538d84df0dceb2d12ea268db
Yann Papouin <ypa@decgroupe.com>
2022-10-26 11:45:37 +0200

    [FIX] helpdesk_notify: Also send a mail notification when assigned user is not from the assigned team
    Don't know why, but sometimes when a customer reply to the initial ticket, a new ticket is created and assigned to the Bot

--- helpdesk_notify/models/helpdesk_ticket.py
+++ helpdesk_notify/models/helpdesk_ticket.py
@@ -10,19 +10,34 @@ class HelpdeskTicket(models.Model):
 
     @api.model
     def create(self, vals):
-        res = super().create(vals)
+        rec = super().create(vals)
         if self._should_notify_new_ticket():
-            if vals.get('team_id') and not vals.get('user_id'):
-                team_id = self.env['helpdesk.ticket.team'].browse(
+            rec._notify_new_ticket(
+                team_id=self.env['helpdesk.ticket.team'].browse(
                     vals.get('team_id')
-                )
+                ),
+                assigned_user_id=self.env['res.users'].browse(
+                    vals.get('user_id')
+                ),
+            )
+        return rec
+
+    def _notify_new_ticket(self, team_id, assigned_user_id):
+        self.ensure_one()
+        if team_id:
+            # Check if assigned_user_id is set and is in this team otherwise
+            # send an e-mail to all members of this team
+            if not assigned_user_id or (
+                assigned_user_id not in team_id.user_ids
+            ):
                 emails = []
                 for user_id in team_id.user_ids:
                     emails.append(user_id.partner_id.email)
-                    #res.message_subscribe(partner_ids=user_id.partner_id.ids)
-                res.with_context(emails=','.join(emails)). \
+                    # self.message_subscribe(
+                    #     partner_ids=user_id.partner_id.ids
+                    # )
+                self.with_context(emails=','.join(emails)). \
                     send_user_internal_mail()
-        return res
 
     def send_user_internal_mail(self):
         self.env.ref('helpdesk_notify.created_ticket_internal_template'). \
