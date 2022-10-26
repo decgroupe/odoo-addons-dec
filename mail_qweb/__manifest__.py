2f9c051959b1ed5cf5e5ddd76474d7716396be23
Yann Papouin <ypa@decgroupe.com>
2022-10-26 15:53:12 +0200

    [ADD] mail_qweb: Qweb templates for email messages and notifications

---
+++ mail_qweb/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Mail QWeb',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'https://www.decgroupe.com',
+    'summary': "Default templates",
+    # TODO: rewrite templates of message_notification_email/mail_notification_light from odoo/addons/mail/data/mail_data.xml
+    'depends': [
+        'mail',
+        'mail_inline_css',
+        'email_template_qweb',
+        'partner_company_type',
+    ],
+    'data': ['data/ir_ui_view.xml', ],
+    'installable': True
+}
