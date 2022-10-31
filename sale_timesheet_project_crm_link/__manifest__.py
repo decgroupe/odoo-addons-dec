6ed68a2faa85fe9a10d846bb706d6a5ae19b9ab8
Yann Papouin <ypa@decgroupe.com>
2022-10-31 16:34:01 +0100

    [ADD] sale_timesheet_project_crm_link: Glue module to map `sale_timesheet` with `project_crm_link`

---
+++ sale_timesheet_project_crm_link/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Sale Timesheet Project CRM Link',
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'website': 'https://www.decgroupe.com',
+    'category': 'Sales',
+    'summary': "Link the new project with sale order opportunity",
+    'depends': [
+        'sale_timesheet_project',
+        'project_crm_link',
+    ],
+    'data': [],
+    'installable': True
+}
---
+++ sale_timesheet_project_crm_link/models/sale_order.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) DEC SARL, Inc - All Rights Reserved.
+# Written by Yann Papouin <ypa at decgroupe.com>, Oct 2022
+
+from odoo import models
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    def _get_create_project_data(self):
+        res = super()._get_create_project_data()
+        res.update({
+            'linked_lead_id': self.opportunity_id.id,
+        })
+        return res
