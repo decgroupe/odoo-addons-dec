8c565908fd685c4a8221cb12607d564081fed439
Yann Papouin <ypa@decgroupe.com>
2022-11-09 10:11:34 +0100

    [IMP] account_sale_traceability: Better default search filters

--- account_sale_traceability/__manifest__.py
+++ account_sale_traceability/__manifest__.py
@@ -3,12 +3,15 @@
     'version': '12.0.1.0.0',
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
-    'summary': '''Account traceability''',
+    'summary': "Accounting traceability",
     'depends': [
         'account',
         'sale',
         'purchase',
     ],
-    'data': ['views/account_invoice.xml', ],
+    'data': [
+        'views/account_invoice.xml',
+        'views/sale_order.xml',
+    ],
     'installable': True
 }
