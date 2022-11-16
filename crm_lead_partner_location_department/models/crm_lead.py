6f847ff1961652ece1945d02a863755ad4441f4b
Yann Papouin <ypa@decgroupe.com>
2022-11-16 12:06:43 +0100

    [IMP] crm_lead_partner_location_department: Add state and department from delivery (shipping) address

--- crm_lead_partner_location_department/models/crm_lead.py
+++ crm_lead_partner_location_department/models/crm_lead.py
@@ -9,14 +9,27 @@ class CrmLead(models.Model):
     _inherit = 'crm.lead'
 
     partner_department_id = fields.Many2one(
-        'res.country.department',
+        comodel_name='res.country.department',
         related='partner_id.department_id',
         string="Department",
         store=True,
     )
     partner_state_id = fields.Many2one(
-        'res.country.state',
+        comodel_name='res.country.state',
         related='partner_id.state_id',
         string="Partner's State",
+        store=True,
+    )
+
+    partner_shipping_department_id = fields.Many2one(
+        comodel_name='res.country.department',
+        related='partner_shipping_id.department_id',
+        string="Shipping Partner's Department",
+        store=True,
+    )
+    partner_shipping_state_id = fields.Many2one(
+        comodel_name='res.country.state',
+        related='partner_shipping_id.state_id',
+        string="Shipping Partner's State",
         store=True,
     )
