894fec02e3848d63bcb382cbcf8625e465d242e8
Yann Papouin <ypa@decgroupe.com>
2022-12-02 14:45:55 +0100

    [MIG] hr_timesheet_autofill: Migration to 13.0

--- hr_timesheet_autofill/__manifest__.py
+++ hr_timesheet_autofill/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'HR Timesheet Auto-fill',
-    'version': '12.0.1.0.0',
+    'version': "13.0.1.0.0",
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
     'summary': "Fill data with previously entered values",
