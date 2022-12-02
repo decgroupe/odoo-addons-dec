b59b1922fd42799f41f246a1368bc31158c9a655
Yann Papouin <ypa@decgroupe.com>
2022-12-02 14:45:44 +0100

    [MIG] account_merge: Migration to 13.0

--- account_merge/__manifest__.py
+++ account_merge/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Merge Accounts',
-    'version': '12.0.1.0.0',
+    'version': "13.0.1.0.0",
     'author': 'DEC, Yann Papouin ',
     'website': 'https://www.decgroupe.com',
     'depends': [
