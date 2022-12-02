8ed104ae5e44122492d0bb7cf75870c1697067b9
Yann Papouin <ypa@decgroupe.com>
2022-12-02 14:45:49 +0100

    [MIG] mrp_as_product: Migration to 13.0

--- mrp_as_product/models/mrp_bom.py
+++ mrp_as_product/models/mrp_bom.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright (C) DEC SARL, Inc - All Rights Reserved.
 # Written by Yann Papouin <ypa at decgroupe.com>, Oct 2020
 
@@ -8,7 +7,6 @@ from odoo import api, models
 class MrpBom(models.Model):
     _inherit = 'mrp.bom'
 
-    @api.multi
     def action_open_as_product(self):
         action = self.mapped('product_tmpl_id').action_view()
         action['context'] = {}
