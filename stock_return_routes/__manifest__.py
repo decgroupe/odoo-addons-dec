765bce2bde5139626f2fc508216a493ec278f1ed
Yann Papouin <ypa@decgroupe.com>
2022-12-02 14:45:43 +0100

    [MIG] stock_return_routes: Migration to 13.0

--- stock_return_routes/__manifest__.py
+++ stock_return_routes/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Stock Return Routes',
-    'version': '12.0.1.0.0',
+    'version': "13.0.1.0.0",
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
     'summary': '''Autoset product route to MTS+MTO on unbuild''',
