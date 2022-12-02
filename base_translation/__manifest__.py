668b4f8a2ceb93648e5deaccc2034e6f07acc52b
Yann Papouin <ypa@decgroupe.com>
2022-12-02 14:45:56 +0100

    [MIG] base_translation: Migration to 13.0

--- base_translation/__manifest__.py
+++ base_translation/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Translation view/editor improvements',
-    'version': '12.0.1.0.0',
+    'version': "13.0.1.0.0",
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
     'summary': '''Better translation search''',
