4e44a1c468e51854c879b57e184a2a57bd66b5b9
Yann Papouin <ypa@decgroupe.com>
2022-12-02 14:45:44 +0100

    [MIG] hr_timesheet_task_context: Migration to 13.0

--- hr_timesheet_task_context/models/project_task.py
+++ hr_timesheet_task_context/models/project_task.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright (C) DEC SARL, Inc - All Rights Reserved.
 # Written by Yann Papouin <ypa at decgroupe.com>, Sep 2021
 
@@ -8,21 +7,18 @@ from odoo import models, api, fields
 class ProjectTask(models.Model):
     _inherit = "project.task"
 
-    @api.multi
     def _update_timesheet_project(self, project_id):
         self.ensure_one()
         self.timesheet_ids.write({
             'project_id': project_id
         })
 
-    @api.multi
     def _update_timesheet_sale_line(self, sale_line_id):
         self.ensure_one()
         self.timesheet_ids.write({
             'so_line': sale_line_id
         })
 
-    @api.multi
     def write(self, vals):
         res = super().write(vals)
         if vals and 'project_id' in vals:
