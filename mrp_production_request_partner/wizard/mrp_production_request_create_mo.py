a594a8a0f403d1c673e6e9e1bd4fb58ff2fd2fd7
Yann Papouin <ypa@decgroupe.com>
2022-12-02 14:45:49 +0100

    [MIG] mrp_production_request_partner: Migration to 13.0

--- mrp_production_request_partner/wizard/mrp_production_request_create_mo.py
+++ mrp_production_request_partner/wizard/mrp_production_request_create_mo.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright (C) DEC SARL, Inc - All Rights Reserved.
 # Written by Yann Papouin <ypa at decgroupe.com>, Feb 2021
 
@@ -8,7 +7,6 @@ from odoo import api, models
 class MrpProductionRequestCreateMo(models.TransientModel):
     _inherit = "mrp.production.request.create.mo"
 
-    @api.multi
     def _prepare_manufacturing_order(self):
         vals = super()._prepare_manufacturing_order()
         request_id = self.mrp_production_request_id
