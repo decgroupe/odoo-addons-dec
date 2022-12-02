f30847a8fe85989874d26adf596ceafd1e538e12
Yann Papouin <ypa@decgroupe.com>
2022-12-02 14:45:50 +0100

    [MIG] mrp_bom_product_state: Migration to 13.0

--- mrp_bom_product_state/__manifest__.py
+++ mrp_bom_product_state/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Manufacturing (BoM Product State)',
-    'version': '12.0.1.0.0',
+    'version': "13.0.1.0.0",
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
     'summary': "Add product state as a BoM field",
