68a7c7d027f94d4c4ce988a5150b7b96938403dd
Yann Papouin <ypa@decgroupe.com>
2022-12-02 14:45:56 +0100

    [MIG] crm_lead_close_lost: Migration to 13.0

--- crm_lead_close_lost/models/crm_lead.py
+++ crm_lead_close_lost/models/crm_lead.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright (C) DEC SARL, Inc - All Rights Reserved.
 # Written by Yann Papouin <ypa at decgroupe.com>, Feb 2021
 
@@ -12,7 +11,6 @@ _logger = logging.getLogger(__name__)
 class CrmLead(models.Model):
     _inherit = 'crm.lead'
 
-    @api.multi
     def action_set_lost(self):
         res = super().action_set_lost()
         for lead in self:
