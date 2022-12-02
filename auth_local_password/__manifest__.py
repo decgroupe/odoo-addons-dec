282acfc60cf61d29a91b0e4b6e6f02449877e3a6
Yann Papouin <ypa@decgroupe.com>
2022-12-02 14:45:53 +0100

    [MIG] auth_local_password: Migration to 13.0

--- auth_local_password/__manifest__.py
+++ auth_local_password/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Auth Local Password',
-    'version': '12.0.1.0.0',
+    'version': "13.0.1.0.0",
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
     'summary': "Allow use of a seconday password (PIN) when local",
