0f98892e1e67e5c1ebf58fcd82eff0ab51b35d28
Yann Papouin <ypa@decgroupe.com>
2022-12-02 14:45:55 +0100

    [MIG] crm_timesheet_time_control: Migration to 13.0

--- crm_timesheet_time_control/__manifest__.py
+++ crm_timesheet_time_control/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'CRM Timesheet Time Control',
-    'version': '12.0.1.0.0',
+    'version': "13.0.1.0.0",
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
     'summary':
