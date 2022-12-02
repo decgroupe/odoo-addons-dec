97f776a073e122f8a03e68095f58e6f9def6f01a
Yann Papouin <ypa@decgroupe.com>
2022-12-02 14:45:55 +0100

    [MIG] account_partner_location_department: Migration to 13.0

--- account_partner_location_department/__manifest__.py
+++ account_partner_location_department/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Account Partner State and Department',
-    'version': '12.0.1.0.0',
+    'version': "13.0.1.0.0",
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
     'summary':
