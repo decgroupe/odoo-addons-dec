78dabfc7628940b322bc1d838cc1b5be28efe38d
Yann Papouin <ypa@decgroupe.com>
2022-12-02 14:45:41 +0100

    [MIG] mrp_activity_my: Migration to 13.0

--- mrp_activity_my/__manifest__.py
+++ mrp_activity_my/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'MRP My Activities',
-    'version': '12.0.1.0.0',
+    'version': "13.0.1.0.0",
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
     'summary':
