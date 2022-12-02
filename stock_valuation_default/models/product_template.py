1ed2819ba901732583236c3ce0c07de2ddf0d236
Yann Papouin <ypa@decgroupe.com>
2022-12-02 14:45:50 +0100

    [MIG] stock_valuation_default: Migration to 13.0

--- stock_valuation_default/models/product_template.py
+++ stock_valuation_default/models/product_template.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright (C) DEC SARL, Inc - All Rights Reserved.
 # Written by Yann Papouin <ypa at decgroupe.com>, Feb 2021
 
@@ -8,7 +7,6 @@ from odoo import api, fields, models
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    @api.one
     def _set_valuation_type(self):
         # Do not set property_valuation otherwise category valuation property
         # will never be taken into account
