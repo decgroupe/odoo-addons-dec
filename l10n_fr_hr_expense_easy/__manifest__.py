96e3a89649377def3d96a0933bcb87bccd43e532
Yann Papouin <ypa@decgroupe.com>
2022-12-02 14:45:58 +0100

    [MIG] l10n_fr_hr_expense_easy: Migration to 13.0

--- l10n_fr_hr_expense_easy/__manifest__.py
+++ l10n_fr_hr_expense_easy/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'L10n FR Easy expense sheet (HE)',
-    'version': '12.0.1.0.0',
+    'version': "13.0.1.0.0",
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
     'summary': "French Localization for easy expense module",
