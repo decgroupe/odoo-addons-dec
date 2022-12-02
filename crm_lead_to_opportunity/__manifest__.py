215f28791c450b3680b2ff54a17749657d3e452c
Yann Papouin <ypa@decgroupe.com>
2022-12-02 14:45:58 +0100

    [MIG] crm_lead_to_opportunity: Migration to 13.0

--- crm_lead_to_opportunity/__manifest__.py
+++ crm_lead_to_opportunity/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'CRM Lead to Opportunity',
-    'version': '12.0.1.0.0',
+    'version': "13.0.1.0.0",
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
     'summary': "Set default seller when converting a lead to an opportunity",
