5e09c5f9bd498bc0205a15492ed1144b7ce75206
Yann Papouin <ypa@decgroupe.com>
2022-12-02 14:45:57 +0100

    [MIG] web_pdf_preview: Migration to 13.0

--- web_pdf_preview/__manifest__.py
+++ web_pdf_preview/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Web PDF preview",
-    'version': '12.0.1.0.0',
+    'version': "13.0.1.0.0",
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
     'summary': '''Open PDF report directly in browser''',
