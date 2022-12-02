3765c15bed16783c386834797e1e818510cdc435
Yann Papouin <ypa@decgroupe.com>
2022-12-02 14:45:45 +0100

    [MIG] partner_folder: Migration to 13.0

--- partner_folder/__manifest__.py
+++ partner_folder/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Partner Folder',
-    'version': '12.0.1.0.0',
+    'version': "13.0.1.0.0",
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
     'summary': 'Add a folder/URL field to store path to resources',
