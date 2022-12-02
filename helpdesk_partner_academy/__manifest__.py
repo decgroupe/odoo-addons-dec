8d16b0d3c0518f36be756d80392a83067907819e
Yann Papouin <ypa@decgroupe.com>
2022-12-02 14:45:42 +0100

    [MIG] helpdesk_partner_academy: Migration to 13.0

--- helpdesk_partner_academy/__manifest__.py
+++ helpdesk_partner_academy/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Helpdesk Partner Academy',
-    'version': '12.0.1.0.0',
+    'version': "13.0.1.0.0",
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
     'summary': '''Add partner academy to tickets''',
