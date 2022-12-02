f5d0aad13222b3a9b7923ecae452b85460a2149b
Yann Papouin <ypa@decgroupe.com>
2022-12-02 14:45:47 +0100

    [MIG] product_service_no_routes: Migration to 13.0

--- product_service_no_routes/models/product_template.py
+++ product_service_no_routes/models/product_template.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright (C) DEC SARL, Inc - All Rights Reserved.
 # Written by Yann Papouin <ypa at decgroupe.com>, Mar 2021
 
@@ -15,13 +14,11 @@ class ProductTemplate(models.Model):
             product.unset_route_ids()
         return product
 
-    @api.multi
     def write(self, vals):
         res = super().write(vals)
         if vals.get('type') == 'service':
             self.unset_route_ids()
         return res
 
-    @api.multi
     def unset_route_ids(self):
         self.write({"route_ids": [(5, 0, 0)]})
