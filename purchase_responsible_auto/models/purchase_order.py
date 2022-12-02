1bf3eefeb2ef2fb5316070f31594bb5d71f65ae5
Yann Papouin <ypa@decgroupe.com>
2022-12-02 14:45:51 +0100

    [MIG] purchase_responsible_auto: Migration to 13.0

--- purchase_responsible_auto/models/purchase_order.py
+++ purchase_responsible_auto/models/purchase_order.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright (C) DEC SARL, Inc - All Rights Reserved.
 # Written by Yann Papouin <ypa at decgroupe.com>, Oct 2020
 
@@ -8,7 +7,6 @@ from odoo import models, api
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
 
-    @api.multi
     def print_quotation(self):
         for order in self:
             if order.user_id == self.env.ref('base.user_root'):
