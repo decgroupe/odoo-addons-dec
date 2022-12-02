a301ca61f39eae95c3eb623dc188c090df4ae84e
Yann Papouin <ypa@decgroupe.com>
2022-12-02 14:46:00 +0100

    [MIG] base_sequence_first_number: Migration to 13.0

--- base_sequence_first_number/__manifest__.py
+++ base_sequence_first_number/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Sequence first number',
-    'version': '12.0.1.0.0',
+    'version': "13.0.1.0.0",
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
     'summary': '''First number can be customized instead of default 1''',
