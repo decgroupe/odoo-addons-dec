75206608d898e4d901493a10e964d6e7c42f34ef
Yann Papouin <ypa@decgroupe.com>
2022-12-02 14:45:45 +0100

    [MIG] product_template_orderpoints: Migration to 13.0

--- product_template_orderpoints/__manifest__.py
+++ product_template_orderpoints/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Product Template Orderpoints',
-    'version': '12.0.1.0.0',
+    'version': "13.0.1.0.0",
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
     'summary': "Allow to filter product based on orderpoints status",
