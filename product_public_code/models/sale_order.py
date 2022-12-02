63703ab8a6f5dbeeff458589ddcd7c8b57337318
Yann Papouin <ypa@decgroupe.com>
2022-12-02 14:45:48 +0100

    [MIG] product_public_code: Migration to 13.0

--- product_public_code/models/sale_order.py
+++ product_public_code/models/sale_order.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright (C) DEC SARL, Inc - All Rights Reserved.
 # Written by Yann Papouin <ypa at decgroupe.com>, Sep 2020
 
@@ -8,7 +7,6 @@ from odoo import api, models
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-    @api.multi
     @api.onchange('product_id')
     def product_id_change(self):
         res = super().product_id_change()
