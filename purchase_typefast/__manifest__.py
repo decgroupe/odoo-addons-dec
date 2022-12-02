d3a401b0edc0ca8058e1ff93599c4219911e4fee
Yann Papouin <ypa@decgroupe.com>
2022-12-02 14:45:41 +0100

    [MIG] purchase_typefast: Migration to 13.0

--- purchase_typefast/__manifest__.py
+++ purchase_typefast/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Purchase Type Fast',
-    'version': '12.0.1.0.0',
+    'version': "13.0.1.0.0",
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
     'summary': "Type fast for a quick search",
