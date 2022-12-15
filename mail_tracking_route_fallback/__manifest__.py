6894e30191757245f0278cfab7102fca9fbd406e
Yann Papouin <ypa@decgroupe.com>
2022-12-15 11:18:10 +0100

    [MIG] mail_tracking_route_fallback: Migration to 14.0

--- mail_tracking_route_fallback/__manifest__.py
+++ mail_tracking_route_fallback/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Mail Tracking Route Fallback',
-    'version': "13.0.1.0.0",
+    'version': "14.0.1.0.0",
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
     'summary': "Use tracking token to re-route a message if needed",
