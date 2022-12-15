acd61fc70df13e8b1744f631baaab22b3ba029fd
Yann Papouin <ypa@decgroupe.com>
2022-12-15 11:18:02 +0100

    [MIG] partner_identification_location: Migration to 14.0

--- partner_identification_location/__manifest__.py
+++ partner_identification_location/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Partner Identification (base_location)',
-    'version': "13.0.1.0.0",
+    'version': "14.0.1.0.0",
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
     'summary': "Use base_location when customizing name search",
