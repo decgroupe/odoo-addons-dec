5e893df02e4fef7d18ef09880ebe369a2538b89f
Yann Papouin <ypa@decgroupe.com>
2022-12-15 11:17:55 +0100

    [MIG] mrp_prepare: Migration to 14.0

--- mrp_prepare/__manifest__.py
+++ mrp_prepare/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Manufacturing Prepare',
-    'version': "13.0.1.0.0",
+    'version': "14.0.1.0.0",
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
     'summary': 'Plan the new order using product data',
