0611f412667b750f2ed17f08aad4197ab1257fdc
Yann Papouin <ypa@decgroupe.com>
2022-12-15 11:17:56 +0100

    [MIG] software_license_portal: Migration to 14.0

--- software_license_portal/__manifest__.py
+++ software_license_portal/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Software License (Portal)',
-    'version': "13.0.1.0.0",
+    'version': "14.0.1.0.0",
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
     'summary': "Manage software licenses through the portal",
