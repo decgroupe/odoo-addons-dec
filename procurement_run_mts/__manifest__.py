4b3996292eccd2314b02bed3b3487bec3e71478a
Yann Papouin <ypa@decgroupe.com>
2022-12-15 11:18:15 +0100

    [MIG] procurement_run_mts: Migration to 14.0

--- procurement_run_mts/__manifest__.py
+++ procurement_run_mts/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Procurement Run MTS',
-    'version': "13.0.1.0.0",
+    'version': "14.0.1.0.0",
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
     'summary': '''Scheduler task extended to validate make_to_stock moves''',
