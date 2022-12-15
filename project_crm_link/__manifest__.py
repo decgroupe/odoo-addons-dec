8d1247785e71c0dcc14c7804838c3685546964c8
Yann Papouin <ypa@decgroupe.com>
2022-12-15 11:18:03 +0100

    [MIG] project_crm_link: Migration to 14.0

--- project_crm_link/__manifest__.py
+++ project_crm_link/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Project CRM Link',
-    'version': "13.0.1.0.0",
+    'version': "14.0.1.0.0",
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
     'summary': "Allow projects to be linked with an opportunity",
