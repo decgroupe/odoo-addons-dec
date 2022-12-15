d2b5ab8f0090573456c01dfbd980abc209b68967
Yann Papouin <ypa@decgroupe.com>
2022-12-15 11:18:04 +0100

    [MIG] document_page_category_name: Migration to 14.0

--- document_page_category_name/__manifest__.py
+++ document_page_category_name/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Document Page Category Complete Name',
-    'version': "13.0.1.0.0",
+    'version': "14.0.1.0.0",
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
     'summary': "Displays the complete category",
