8bebfe287edc467a34e19358f4c0cdd32ee75eba
Yann Papouin <ypa@decgroupe.com>
2022-12-15 11:18:04 +0100

    [MIG] purchase_stock_traceability: Migration to 14.0

--- purchase_stock_traceability/__manifest__.py
+++ purchase_stock_traceability/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Purchase Stock Traceability',
-    'version': "13.0.1.0.0",
+    'version': "14.0.1.0.0",
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
     'summary': 'Get origin for each purchase line',
