41264347c9a8f15b348805084b518d2d65f9a4f5
Yann Papouin <ypa@decgroupe.com>
2022-12-15 11:18:02 +0100

    [MIG] sale_pricelist_analysis: Migration to 14.0

--- sale_pricelist_analysis/__manifest__.py
+++ sale_pricelist_analysis/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Sale Pricelist Analysis',
-    'version': "13.0.1.0.0",
+    'version': "14.0.1.0.0",
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
     'summary': "Add menu entry",
