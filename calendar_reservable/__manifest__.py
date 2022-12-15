337a1cdae0de4abc3e23eb742f58bd0290a94f26
Yann Papouin <ypa@decgroupe.com>
2022-12-15 11:18:00 +0100

    [MIG] calendar_reservable: Migration to 14.0

--- calendar_reservable/__manifest__.py
+++ calendar_reservable/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Calendar Reservable',
-    'version': "13.0.1.0.0",
+    'version': "14.0.1.0.0",
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
     'summary': "Filter for reservable <partners> like vehicles, rooms, etc.",
