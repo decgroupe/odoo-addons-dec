68851d1c5a29cc95f06049445eb2d44303d16aa3
Yann Papouin <ypa@decgroupe.com>
2022-12-15 11:18:10 +0100

    [MIG] product_reference_price_history: Migration to 14.0

--- product_reference_price_history/__manifest__.py
+++ product_reference_price_history/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Product Reference Price History',
-    'version': "13.0.1.0.0",
+    'version': "14.0.1.0.0",
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
     'summary': '''Price history and reporting''',
