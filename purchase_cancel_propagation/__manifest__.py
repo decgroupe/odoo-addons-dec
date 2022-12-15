3cf14ccad739602f5c56603e3feaf29d2c761f11
Yann Papouin <ypa@decgroupe.com>
2022-12-15 11:17:56 +0100

    [MIG] purchase_cancel_propagation: Migration to 14.0

--- purchase_cancel_propagation/__manifest__.py
+++ purchase_cancel_propagation/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Purchase cancel propagation',
-    'version': "13.0.1.0.0",
+    'version': "14.0.1.0.0",
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
     'summary': '''Choose between simple delete or delete and propagate to cancel procurement''',
