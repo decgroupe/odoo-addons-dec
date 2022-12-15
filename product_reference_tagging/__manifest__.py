5d9563f0a72fbb38d6e69308ce550f0c96ef2f0b
Yann Papouin <ypa@decgroupe.com>
2022-12-15 11:17:59 +0100

    [MIG] product_reference_tagging: Migration to 14.0

--- product_reference_tagging/__manifest__.py
+++ product_reference_tagging/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Tagging (product_reference)',
-    'version': "13.0.1.0.0",
+    'version': "14.0.1.0.0",
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
     'summary': '''Tagging references models''',
