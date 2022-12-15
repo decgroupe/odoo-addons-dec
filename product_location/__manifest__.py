9b29c8ffe31aeca1cfb51442f3456e96b6293c6a
Yann Papouin <ypa@decgroupe.com>
2022-12-15 11:18:00 +0100

    [MIG] product_location: Migration to 14.0

--- product_location/__manifest__.py
+++ product_location/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Product Location',
-    'version': "13.0.1.0.0",
+    'version': "14.0.1.0.0",
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
     'summary': '''Add (Rack,Row,Case) fields to help locating a product in Warehouse''',
