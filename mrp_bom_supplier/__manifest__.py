e334f0b6efc83ff0a9093583bf441dfe751ad9e8
Yann Papouin <ypa@decgroupe.com>
2022-12-15 11:18:00 +0100

    [MIG] mrp_bom_supplier: Migration to 14.0

--- mrp_bom_supplier/__manifest__.py
+++ mrp_bom_supplier/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Manufacturing (BoM supplier)',
-    'version': "13.0.1.0.0",
+    'version': "14.0.1.0.0",
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
     'summary': '''Add extra fields on BoM''',
