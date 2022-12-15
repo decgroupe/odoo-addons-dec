7dde06e7b36e9e299c4b1930428c542ef1211fb4
Yann Papouin <ypa@decgroupe.com>
2022-12-15 11:18:08 +0100

    [MIG] project_typefast: Migration to 14.0

--- project_typefast/__manifest__.py
+++ project_typefast/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Project Type Fast',
-    'version': "13.0.1.0.0",
+    'version': "14.0.1.0.0",
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
     'summary': "Type fast for a quick search",
