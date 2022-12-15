0e7ae9d6a70cfc54718cf36450c6fe1e8c3eb347
Yann Papouin <ypa@decgroupe.com>
2022-12-15 11:18:06 +0100

    [MIG] procurement_run_mto: Migration to 14.0

--- procurement_run_mto/__manifest__.py
+++ procurement_run_mto/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Procurement Run MTO',
-    'version': "13.0.1.0.0",
+    'version': "14.0.1.0.0",
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
     'summary': "Scheduler task extended to re-create PO/MO",
