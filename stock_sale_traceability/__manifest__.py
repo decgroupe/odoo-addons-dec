95eacd55bb2164c7defbc21607f37850eb63861b
Yann Papouin <ypa@decgroupe.com>
2022-12-15 11:17:59 +0100

    [MIG] stock_sale_traceability: Migration to 14.0

--- stock_sale_traceability/__manifest__.py
+++ stock_sale_traceability/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Stock Sale Traceability',
-    'version': "13.0.1.0.0",
+    'version': "14.0.1.0.0",
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
     'summary': '''Show sale line origin''',
