bcb329ca5ded8b42d5347fade818dd840cce53fa
Yann Papouin <ypa@decgroupe.com>
2022-12-15 11:18:01 +0100

    [MIG] product_form_quotation_link: Migration to 14.0

--- product_form_quotation_link/__manifest__.py
+++ product_form_quotation_link/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Product Form Quotation Link',
-    'version': "13.0.1.0.0",
+    'version': "14.0.1.0.0",
     'author':
         'DEC, Yann Papouin, ACSONE SA/NV, '
         'Odoo Community Association (OCA)',
