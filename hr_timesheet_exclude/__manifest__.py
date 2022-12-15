9c31faf4dc7aa08877fdd6aa2aa846ab333b57e8
Yann Papouin <ypa@decgroupe.com>
2022-12-15 11:18:01 +0100

    [MIG] hr_timesheet_exclude: Migration to 14.0

--- hr_timesheet_exclude/__manifest__.py
+++ hr_timesheet_exclude/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'HR Timesheet Exclude',
-    'version': "13.0.1.0.0",
+    'version': "14.0.1.0.0",
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
     'summary': "Select task/projects that should be excluded from analysis",
