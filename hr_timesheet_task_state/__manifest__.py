46c5f10193e9bb994a6686cfb57be67720186147
Yann Papouin <ypa@decgroupe.com>
2022-12-15 11:17:56 +0100

    [MIG] hr_timesheet_task_state: Migration to 14.0

--- hr_timesheet_task_state/__manifest__.py
+++ hr_timesheet_task_state/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'HR Timesheet Task State',
-    'version': "13.0.1.0.0",
+    'version': "14.0.1.0.0",
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
     'summary':
