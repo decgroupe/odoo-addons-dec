12972a755d5817198a4a8ef9dd29efbea67445ed
Yann Papouin <ypa@decgroupe.com>
2022-12-15 11:18:00 +0100

    [MIG] base_module_migration_tracking: Migration to 14.0

--- base_module_migration_tracking/__manifest__.py
+++ base_module_migration_tracking/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Module Migration Tracking',
-    'version': "13.0.1.0.0",
+    'version': "14.0.1.0.0",
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
     'summary': "Add fields to track individual migration status",
