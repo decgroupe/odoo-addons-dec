fc0d99ca984d7dcfce0fad1866127015319f7e88
Yann Papouin <ypa@decgroupe.com>
2022-12-15 11:18:08 +0100

    [MIG] crm_timesheet_sale: Migration to 14.0

--- crm_timesheet_sale/__manifest__.py
+++ crm_timesheet_sale/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'CRM Timesheet Sale',
-    'version': "13.0.1.0.0",
+    'version': "14.0.1.0.0",
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
     'summary': "Fix missing default project when creating an opportunity",
