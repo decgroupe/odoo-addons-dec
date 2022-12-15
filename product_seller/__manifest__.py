b3af3ea7dbcbb6fea06838e3ecd40ee516b1af02
Yann Papouin <ypa@decgroupe.com>
2022-12-15 11:18:16 +0100

    [MIG] product_seller: Migration to 14.0

--- product_seller/__manifest__.py
+++ product_seller/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Product seller',
-    'version': "13.0.1.0.0",
+    'version': "14.0.1.0.0",
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
     'summary': '''Shortcut to main seller''',
