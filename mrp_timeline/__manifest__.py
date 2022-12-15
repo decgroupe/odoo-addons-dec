1424420d8a0a4404a9e2347c3121a14d954914cc
Yann Papouin <ypa@decgroupe.com>
2022-12-15 11:17:54 +0100

    [MIG] mrp_timeline: Migration to 14.0

--- mrp_timeline/__manifest__.py
+++ mrp_timeline/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Manufacturing timeline',
-    'version': "13.0.1.0.0",
+    'version': "14.0.1.0.0",
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
     'summary': "Timeline view for production",
