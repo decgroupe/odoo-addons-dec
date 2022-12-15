56a55b9cb7f8206878aff6a8797d1ec3805b829d
Yann Papouin <ypa@decgroupe.com>
2022-12-15 11:17:57 +0100

    [MIG] purchase_split: Migration to 14.0

--- purchase_split/__manifest__.py
+++ purchase_split/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Purchase Split',
-    'version': "13.0.1.0.0",
+    'version': "14.0.1.0.0",
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
     'summary': '''Split a quotation by selecting lines to move''',
