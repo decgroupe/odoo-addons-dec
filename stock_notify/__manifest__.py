66a9a98349d50c21c25ff525af29913ba6b1a79b
Yann Papouin <ypa@decgroupe.com>
2022-12-15 11:18:07 +0100

    [MIG] stock_notify: Migration to 14.0

--- stock_notify/__manifest__.py
+++ stock_notify/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Stock Notify',
-    'version': "13.0.1.0.0",
+    'version': "14.0.1.0.0",
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
     'summary': '''Send a notification when a stock move becomes assigned''',
