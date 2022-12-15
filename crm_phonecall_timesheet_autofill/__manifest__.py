9e914076aefd29061862ef9c5c7a2475f1f259ac
Yann Papouin <ypa@decgroupe.com>
2022-12-15 11:17:56 +0100

    [MIG] crm_phonecall_timesheet_autofill: Migration to 14.0

--- crm_phonecall_timesheet_autofill/__manifest__.py
+++ crm_phonecall_timesheet_autofill/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'CRM Phonecall Timesheet Auto-fill',
-    'version': "13.0.1.0.0",
+    'version': "14.0.1.0.0",
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
     'summary': "Fill data with previously entered values",
