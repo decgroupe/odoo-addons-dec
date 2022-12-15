bb7d25e962a0d6d6dff363613d9563dad8ea73a7
Yann Papouin <ypa@decgroupe.com>
2022-12-15 11:18:01 +0100

    [MIG] account_analytic_line_sequence: Migration to 14.0

--- account_analytic_line_sequence/__manifest__.py
+++ account_analytic_line_sequence/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Account Analytic Line Sequence',
-    'version': "13.0.1.0.0",
+    'version': "14.0.1.0.0",
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
     'summary': '''Add sequence field on analytic lines''',
