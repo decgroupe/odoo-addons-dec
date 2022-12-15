29a0218fc1bf7b5ebcbbb9a621d480bd0497f20d
Yann Papouin <ypa@decgroupe.com>
2022-12-15 11:17:55 +0100

    [MIG] software_account: Migration to 14.0

--- software_account/__manifest__.py
+++ software_account/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Software account',
-    'version': "13.0.1.0.0",
+    'version': "14.0.1.0.0",
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
     'summary': '''Manage software accounts (google, steam, etc.)''',
