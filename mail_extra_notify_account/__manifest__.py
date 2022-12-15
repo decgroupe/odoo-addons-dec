4e4247f53f0ae65aac6900237bf247f6adfa4671
Yann Papouin <ypa@decgroupe.com>
2022-12-15 11:18:16 +0100

    [MIG] mail_extra_notify_account: Migration to 14.0

--- mail_extra_notify_account/__manifest__.py
+++ mail_extra_notify_account/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Mail Extra Notify (Account)',
-    'version': "13.0.1.0.0",
+    'version': "14.0.1.0.0",
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
     'summary': "Add extra data in assignation message",
