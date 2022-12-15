5ab738d35f8da91c2157f74e2580e4350ed5b33a
Yann Papouin <ypa@decgroupe.com>
2022-12-15 11:17:58 +0100

    [MIG] project_timesheet_project_type: Migration to 14.0

--- project_timesheet_project_type/__manifest__.py
+++ project_timesheet_project_type/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Project Type for Timesheet',
-    'version': "13.0.1.0.0",
+    'version': "14.0.1.0.0",
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
     'summary': "Add project type to filter view",
