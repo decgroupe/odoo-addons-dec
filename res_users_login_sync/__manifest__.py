c0935212390854d6e32343736483e2cfed748674
Yann Papouin <ypa@decgroupe.com>
2022-12-15 11:18:11 +0100

    [MIG] res_users_login_sync: Migration to 14.0

--- res_users_login_sync/__manifest__.py
+++ res_users_login_sync/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'User Login Sync',
-    'version': "13.0.1.0.0",
+    'version': "14.0.1.0.0",
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
     'category': 'Base',
