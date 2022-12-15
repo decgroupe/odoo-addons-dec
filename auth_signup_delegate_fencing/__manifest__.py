2633eec719c1b7bb8396213994460cacab10343c
Yann Papouin <ypa@decgroupe.com>
2022-12-15 11:18:08 +0100

    [MIG] auth_signup_delegate_fencing: Migration to 14.0

--- auth_signup_delegate_fencing/__manifest__.py
+++ auth_signup_delegate_fencing/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Auth Signup Delegate Fencing',
-    'version': "13.0.1.0.0",
+    'version': "14.0.1.0.0",
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
     'summary': "Enabled fencing mode when using delegated partner creation",
