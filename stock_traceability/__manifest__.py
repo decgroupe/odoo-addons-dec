b58bac9d71ca7196c78239843f00be31c1793ea6
Yann Papouin <ypa@decgroupe.com>
2022-12-15 11:18:00 +0100

    [MIG] stock_traceability: Migration to 14.0

--- stock_traceability/__manifest__.py
+++ stock_traceability/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Stock Traceability',
-    'version': "13.0.1.0.0",
+    'version': "14.0.1.0.0",
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
     'summary': "Get move final location from any move of the chain",
