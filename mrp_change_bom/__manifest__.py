b763ef849d6be08ccfce58a0d23fd08b0860e1a4
Yann Papouin <ypa@decgroupe.com>
2022-12-15 11:17:56 +0100

    [MIG] mrp_change_bom: Migration to 14.0

--- mrp_change_bom/__manifest__.py
+++ mrp_change_bom/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Manufacturing Change BoM',
-    'version': "13.0.1.0.0",
+    'version': "14.0.1.0.0",
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
     'summary': '''Allow to change BoM reference used in a production order''',
