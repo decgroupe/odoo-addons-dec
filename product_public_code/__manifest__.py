04f8d08fc6a39b97b15263bbfdeb138c9d7f8d08
Yann Papouin <ypa@decgroupe.com>
2022-12-15 11:18:02 +0100

    [MIG] product_public_code: Migration to 14.0

--- product_public_code/__manifest__.py
+++ product_public_code/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Product Public Code',
-    'version': "13.0.1.0.0",
+    'version': "14.0.1.0.0",
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
     'summary': '''Add public code on product form''',
