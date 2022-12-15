2793e7a93d641dbc63114ed17eb2b222d656bf05
Yann Papouin <ypa@decgroupe.com>
2022-12-15 11:18:03 +0100

    [MIG] account_analytic_partner_academy: Migration to 14.0

--- account_analytic_partner_academy/__manifest__.py
+++ account_analytic_partner_academy/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Account Analytic Partner Academy',
-    'version': "13.0.1.0.0",
+    'version': "14.0.1.0.0",
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
     'summary': '''Add partner academy to analytic lines''',
