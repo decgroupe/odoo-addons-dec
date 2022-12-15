c93e37809ddcf43d6aee93d0ebdd3a027d4bd3ad
Yann Papouin <ypa@decgroupe.com>
2022-12-15 11:18:05 +0100

    [MIG] hr_holidays_manager: Migration to 14.0

--- hr_holidays_manager/__manifest__.py
+++ hr_holidays_manager/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'HR Leaves Manager',
-    'version': "13.0.1.0.0",
+    'version': "14.0.1.0.0",
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
     'summary': "Get employee manager if has specific group",
