0e884a887fca9f5440fcf40216b09c3dae797e0d
Yann Papouin <ypa@decgroupe.com>
2022-12-15 11:17:57 +0100

    [MIG] base_country_no_state: Migration to 14.0

--- base_country_no_state/__manifest__.py
+++ base_country_no_state/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Hide Country State',
-    'version': "13.0.1.0.0",
+    'version': "14.0.1.0.0",
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
     'summary': "Base module that don't do anything by himself",
