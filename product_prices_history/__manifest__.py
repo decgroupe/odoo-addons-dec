c9350caddc29c58d6a8df6e7c3bd7be994133b43
Yann Papouin <ypa@decgroupe.com>
2022-12-16 11:36:35 +0100

    [FIX] product_prices_history: Missing dependency on stock module

--- product_prices_history/__manifest__.py
+++ product_prices_history/__manifest__.py
@@ -4,12 +4,16 @@
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
     'summary': "Store prices based on pricelist",
-    'depends': ['product_prices', ],
-    'data': [
-        'data/ir_cron.xml',
-        'security/ir.model.access.csv',
-        'views/product_prices_history.xml',
-        'views/product_template.xml',
+    'depends': [
+        'product_prices',
+        'stock',
     ],
+    'data':
+        [
+            'data/ir_cron.xml',
+            'security/ir.model.access.csv',
+            'views/product_prices_history.xml',
+            'views/product_template.xml',
+        ],
     'installable': True
 }
