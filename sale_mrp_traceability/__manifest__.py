6973877232d1697f929944c94ab2cd31c8ab7821
Yann Papouin <ypa@decgroupe.com>
2022-12-22 09:16:22 +0100

    [FIX] sale_mrp_traceability: Inherit view from `sale_traceability`

--- sale_mrp_traceability/__manifest__.py
+++ sale_mrp_traceability/__manifest__.py
@@ -7,7 +7,6 @@
     'summary': '''Show related stock moves on sale order line form''',
     'depends': [
         'sale_traceability',
-        'sale_row_layout',
         'stock_mrp_traceability',
     ],
     #'force_migration':'12.0.0.0.0',
