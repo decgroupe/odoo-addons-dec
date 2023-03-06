9ed42d3de6296bb084cc6edc9225859a57fd0a9d
Yann Papouin <ypa@decgroupe.com>
2023-03-06 14:37:46 +0100

    [MIG] product_prices: Migration to 14.0

--- product_prices/__manifest__.py
+++ product_prices/__manifest__.py
@@ -3,18 +3,20 @@
     'version': "14.0.1.0.0",
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
-    'summary': '''Store prices write dates''',
-    'depends': [
-        'web_widget_mermaid',
-        'product',
-        'product_seller',
-        'product_pricelist',
-    ],
+    'summary': 'Store prices write dates',
+    'depends':
+        [
+            'web_widget_mermaid',
+            'product',
+            'product_seller',
+            'product_pricelist',
+        ],
     'data':
         [
             'views/assets.xml',
             'views/product_template.xml',
             'wizard/product_price_graph.xml',
+            'security/ir.model.access.csv',
         ],
     'installable': True
 }
