e84ad75f7d6758415185223e5d835f2bc433cbe6
Yann Papouin <ypa@decgroupe.com>
2023-03-06 09:45:45 +0100

    [MIG] base_location_merge: Migration to 14.0

--- base_location_merge/__manifest__.py
+++ base_location_merge/__manifest__.py
@@ -9,6 +9,7 @@
     ],
     'data': [
         'security/res_groups.xml',
+        'security/ir.model.access.csv',
         'wizard/merge_res_city.xml',
         'wizard/merge_res_city_zip.xml',
     ],
