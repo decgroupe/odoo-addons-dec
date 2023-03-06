067732eb2910e57dc5496a859cd0a866d36f8e9a
Yann Papouin <ypa@decgroupe.com>
2023-03-06 14:37:07 +0100

    [MIG] mrp_production_request_procurement: Migration to 14.0

--- mrp_production_request_procurement/__manifest__.py
+++ mrp_production_request_procurement/__manifest__.py
@@ -3,14 +3,16 @@
     'version': "14.0.1.0.0",
     'author': 'DEC, Yann Papouin',
     'website': 'https://www.decgroupe.com',
-    'summary': '''Allow customization of the procurement group assigned to MO''',
+    'summary': "Allow customization of the procurement group assigned to MO",
     'depends': [
         'mrp',
         'mrp_production_request',
     ],
-    'data': [
-        'views/mrp_production.xml',
-        'views/mrp_production_request.xml',
-    ],
+    'data':
+        [
+            'views/mrp_production.xml',
+            'views/mrp_production_request.xml',
+            'security/ir.model.access.csv',
+        ],
     'installable': True
 }
