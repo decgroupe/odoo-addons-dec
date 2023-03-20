380605aa9981c9f650da46c92024310a4f055bc1
Yann Papouin <ypa@decgroupe.com>
2023-03-20 14:04:49 +0100

    [FIX] mail_disable_auto_subscribe: Ignore sub-types filtering when subscribing manually

--- mail_disable_auto_subscribe/models/mail_message_subtype.py
+++ mail_disable_auto_subscribe/models/mail_message_subtype.py
@@ -21,7 +21,8 @@ class MailMessageSubtype(models.Model):
     )
 
     def _filter_subtypes(self, model_name, pack_ids):
-        if model_name:
+        res = pack_ids
+        if model_name and not self.env.context.get("manual_message_subscribe"):
             domain = [('excluded_res_model_ids.model', '=', model_name)]
             excluded_ids = self.search(domain)
             if excluded_ids.ids:
@@ -29,10 +30,12 @@ class MailMessageSubtype(models.Model):
                 for ids in pack_ids:
                     ids = [x for x in ids if x not in excluded_ids.ids]
                     filtered_array_ids.append(ids)
-                return filtered_array_ids
-        return pack_ids
+                res = filtered_array_ids
+        return res
 
-    @tools.ormcache('self.env.uid', 'model_name')
+    @tools.ormcache_context(
+        'self.env.uid', 'model_name', keys=("manual_message_subscribe", )
+    )
     def _get_auto_subscription_subtypes(self, model_name):
         child_ids, def_ids, all_int_ids, parent, relation = super(
         )._get_auto_subscription_subtypes(model_name)
@@ -40,7 +43,9 @@ class MailMessageSubtype(models.Model):
         def_ids, = self._filter_subtypes(model_name, [def_ids])
         return child_ids, def_ids, all_int_ids, parent, relation
 
-    @tools.ormcache('self.env.uid', 'model_name')
+    @tools.ormcache_context(
+        'self.env.uid', 'model_name', keys=("manual_message_subscribe", )
+    )
     def _default_subtypes(self, model_name):
         subtype_ids, internal_ids, external_ids = super(
         )._default_subtypes(model_name)
