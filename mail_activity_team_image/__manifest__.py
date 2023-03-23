475e0b42c5f91f2ec7e997b5b846dbe2ec9ef85d
Yann Papouin <ypa@decgroupe.com>
2023-03-23 13:26:52 +0100

    [ADD] mail_activity_team_image: Add and display team logo in the chatter

---
+++ mail_activity_team_image/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Mail Activity Team Image',
+    'summary': "Add image to acivity team",
+    'version': '12.0.1.0.0',
+    'author': 'DEC, Yann Papouin',
+    'category': 'Social Network',
+    'website': 'https://github.com/OCA/social',
+    'license': 'AGPL-3',
+    'depends': ['mail_activity_team', ],
+    "data": ["views/mail_activity_team.xml", ],
+    'qweb': ["static/src/xml/activity.xml", ],
+    'installable': True
+}
