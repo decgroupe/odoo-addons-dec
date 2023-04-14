fe67c5ea473015be56b3aa811e4495dff31612da
Yann Papouin <ypa@decgroupe.com>
2023-04-14 14:16:04 +0200

    [IMP] procurement_run_mts: Log exceptions using `procurement_log` module

--- procurement_run_mts/__manifest__.py
+++ procurement_run_mts/__manifest__.py
@@ -1,17 +1,15 @@
 {
-    'name': 'Procurement Run MTS',
-    'version': "14.0.1.0.0",
-    'author': 'DEC, Yann Papouin',
-    'website': 'https://www.decgroupe.com',
-    'summary': '''Scheduler task extended to validate make_to_stock moves''',
-    'depends': [
-        'stock',
-        'mrp',
-        'purchase_stock',
+    "name": "Procurement Run MTS",
+    "version": "14.0.1.0.0",
+    "author": "DEC, Yann Papouin",
+    "website": "https://www.decgroupe.com",
+    "summary": "Scheduler task extended to validate make_to_stock moves",
+    "depends": [
+        "stock",
+        "mrp",
+        "purchase_stock",
+        "procurement_log",
     ],
-    'data':
-        [
-            'views/procurement.xml',
-        ],
-    'installable': True
+    "data": [],
+    "installable": True,
 }
--- procurement_run_mts/models/procurement_group.py
+++ procurement_run_mts/models/procurement_group.py
@@ -43,9 +43,7 @@ class ProcurementGroup(models.Model):
                 with self._cr.savepoint():
                     self._action_cannot_reorder_product(product_id)
             except UserError as error:
-                self.env['stock.rule']._log_next_activity(
-                    product_id, error.name
-                )
+                self._log_exception(product_id.product_tmpl_id, error.name)
         if use_new_cursor:
             self._cr.commit()
 
