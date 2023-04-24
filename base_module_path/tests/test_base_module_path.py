63f8005847480fc9ac33327aa7fda3c6fab82754
Yann Papouin <ypa@decgroupe.com>
2023-04-24 09:17:07 +0200

    [IMP] base_module_path: Test added

---
+++ base_module_path/tests/test_base_module_path.py
@@ -0,0 +1,19 @@
+from odoo import fields
+from odoo.exceptions import AccessError, UserError
+from odoo.tests.common import TransactionCase
+from odoo.tools import mute_logger
+
+
+class TestBaseModulePath(TransactionCase):
+    def setUp(self, *args, **kwargs):
+        super().setUp(*args, **kwargs)
+        self.IrModuleModule = self.env["ir.module.module"]
+        self.MODULE_NAME = "base_module_path"
+
+    def test_01_valid_paths(self):
+        """Tests of computed paths matches the `__file__` one """
+        self.IrModuleModule.action_recompute_path()
+        module_id = self.IrModuleModule.search([("name", "=", self.MODULE_NAME)])
+        self.assertTrue(len(module_id), 1)
+        self.assertTrue(__file__.startswith(module_id.addons_path))
+        self.assertTrue(__file__.startswith(module_id.path))
