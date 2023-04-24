49644de1039d9827c72d17a7ec51cc8ffbf0d5cd
Yann Papouin <ypa@decgroupe.com>
2023-04-24 09:16:54 +0200

    [IMP] base_module_path: `addons_path` field added
    black, isort, prettier

--- base_module_path/__manifest__.py
+++ base_module_path/__manifest__.py
@@ -1,16 +1,16 @@
 {
-    'name': 'Module Path',
-    'version': "14.0.1.0.0",
-    'author': 'DEC, Yann Papouin',
-    'website': 'https://www.decgroupe.com',
-    'summary': "Get module path from UI",
-    'depends': [
-        'base',
-        'mail',
+    "name": "Module Path",
+    "version": "14.0.1.0.0",
+    "author": "DEC, Yann Papouin",
+    "website": "https://www.decgroupe.com",
+    "summary": "Get module path from UI",
+    "depends": [
+        "base",
+        "web",
     ],
-    'data': [
-        'views/assets.xml',
-        'views/ir_module.xml',
+    "data": [
+        "views/assets.xml",
+        "views/ir_module.xml",
     ],
-    'installable': True
+    "installable": True,
 }
--- base_module_path/models/ir_module.py
+++ base_module_path/models/ir_module.py
@@ -1,21 +1,34 @@
 # Copyright (C) DEC SARL, Inc - All Rights Reserved.
 # Written by Yann Papouin <ypa at decgroupe.com>, Jul 2021
 
-from odoo import _, api, models, fields
+from odoo import _, api, fields, models, addons
 from odoo.modules.module import get_module_path
 
 
 class IrModule(models.Model):
     _inherit = "ir.module.module"
 
+    addons_path = fields.Char(
+        compute="_compute_path",
+        store=True,
+    )
     path = fields.Char(
         compute="_compute_path",
         store=True,
     )
 
     def _compute_path(self):
+        self.path = ""
+        self.addons_path = ""
         for rec in self:
             rec.path = get_module_path(rec.name)
+            if rec.path:
+                for adp in addons.__path__:
+                    if rec.path.startswith(adp):
+                        rec.addons_path = adp
+                        # we assert that nested addons paths are not possible so we
+                        # take the first match
+                        break
 
     def action_recompute_path(self):
         self._compute_path()
