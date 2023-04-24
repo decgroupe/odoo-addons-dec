d20fb2f34282be6167290ff6e09462eb1a9339cb
Yann Papouin <ypa@decgroupe.com>
2023-04-24 09:30:29 +0200

    [IMP] base_module_search: black, isort, prettier, readme, icon

--- base_module_search/__manifest__.py
+++ base_module_search/__manifest__.py
@@ -1,10 +1,13 @@
 {
-    'name': 'Module Search',
-    'version': "14.0.1.0.0",
-    'author': 'DEC, Yann Papouin',
-    'website': 'https://www.decgroupe.com',
-    'summary': "Improve usability",
-    'depends': ['base', ],
-    'data': ['views/ir_module.xml', ],
-    'installable': True
+    "name": "Module Search",
+    "version": "14.0.1.0.0",
+    "author": "DEC",
+    "website": "https://www.decgroupe.com",
+    "depends": [
+        "base",
+    ],
+    "data": [
+        "views/ir_module.xml",
+    ],
+    "installable": True,
 }
---
+++ base_module_search/readme/DESCRIPTION.rst
@@ -0,0 +1,2 @@
+This module replace the default "Learn More" button in the Apps view, that redirect to
+an URL, with the "Module Info" the open the module form page.
