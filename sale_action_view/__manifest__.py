1a25c46c7a5045d7ec07092f116da055bc9fd844
Yann Papouin <ypa@decgroupe.com>
2023-04-26 17:00:40 +0200

    [IMP] sale_action_view: black, isort, prettier, readme, icon

--- sale_action_view/__manifest__.py
+++ sale_action_view/__manifest__.py
@@ -1,10 +1,11 @@
 {
-    'name': 'Sale Action View',
-    'version': "14.0.1.0.0",
-    'author': 'DEC, Yann Papouin',
-    'website': 'https://www.decgroupe.com',
-    'summary': "Code shortcuts to open base views",
-    'depends': ['sale', ],
-    'data': [],
-    'installable': True
+    "name": "Sale Action View",
+    "version": "14.0.1.0.0",
+    "author": "DEC",
+    "website": "https://www.decgroupe.com",
+    "depends": [
+        "sale",
+    ],
+    "data": [],
+    "installable": True,
 }
--- sale_action_view/models/sale_order.py
+++ sale_action_view/models/sale_order.py
@@ -1,31 +1,30 @@
 # Copyright (C) DEC SARL, Inc - All Rights Reserved.
 # Written by Yann Papouin <ypa at decgroupe.com>, Sep 2022
 
-from odoo import models, api
+from odoo import api, models
 
 
 class SaleOrder(models.Model):
-    _inherit = 'sale.order'
+    _inherit = "sale.order"
 
     @api.model
     def action_view_base(self):
-        return self.env.ref('sale.action_orders').read()[0]
+        return self.env.ref("sale.action_orders").sudo().read()[0]
 
     def action_view(self):
         action = self.action_view_base()
         if not self.ids:
             pass
         elif len(self.ids) > 1:
-            action['domain'] = [('id', 'in', self.ids)]
+            action["domain"] = [("id", "in", self.ids)]
         else:
-            form = self.env.ref('sale.view_order_form', False)
-            form_view = [(form and form.id or False, 'form')]
-            if 'views' in action:
-                action['views'] = form_view + [
-                    (state, view)
-                    for state, view in action['views'] if view != 'form'
+            form = self.env.ref("sale.view_order_form", False)
+            form_view = [(form and form.id or False, "form")]
+            if "views" in action:
+                action["views"] = form_view + [
+                    (state, view) for state, view in action["views"] if view != "form"
                 ]
             else:
-                action['views'] = form_view
-            action['res_id'] = self.ids[0]
+                action["views"] = form_view
+            action["res_id"] = self.ids[0]
         return action
