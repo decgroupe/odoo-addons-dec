5984510fd2051b79e0db7c5c39fe2e188f0afe8b
Yann Papouin <ypa@decgroupe.com>
2023-04-26 16:54:02 +0200

    [IMP] mrp_production_request_action_view: black, isort, prettier, readme, icon

--- mrp_production_request_action_view/__manifest__.py
+++ mrp_production_request_action_view/__manifest__.py
@@ -1,10 +1,11 @@
 {
-    'name': 'Manufacturing Production Request Action View',
-    'version': "14.0.1.0.0",
-    'author': 'DEC, Yann Papouin',
-    'website': 'https://www.decgroupe.com',
-    'summary': "Code shortcuts to open base views",
-    'depends': ['mrp_production_request', ],
-    'data': [],
-    'installable': True
+    "name": "Manufacturing Production Request Action View",
+    "version": "14.0.1.0.0",
+    "author": "DEC",
+    "website": "https://www.decgroupe.com",
+    "depends": [
+        "mrp_production_request",
+    ],
+    "data": [],
+    "installable": True,
 }
--- mrp_production_request_action_view/models/mrp_production_request.py
+++ mrp_production_request_action_view/models/mrp_production_request.py
@@ -1,28 +1,30 @@
 # Copyright (C) DEC SARL, Inc - All Rights Reserved.
 # Written by Yann Papouin <ypa at decgroupe.com>, Sep 2022
 
-from odoo import models, api
+from odoo import api, models
 
 
 class MrpProductionRequest(models.Model):
-    _inherit = 'mrp.production.request'
+    _inherit = "mrp.production.request"
 
     @api.model
     def action_view_base(self):
-        return self.env.ref(
-            'mrp_production_request.mrp_production_request_action'
-        ).read()[0]
+        return (
+            self.env.ref("mrp_production_request.mrp_production_request_action")
+            .sudo()
+            .read()[0]
+        )
 
     def action_view(self):
         action = self.action_view_base()
         if not self.ids:
             pass
         elif len(self.ids) > 1:
-            action['domain'] = [('id', 'in', self.ids)]
+            action["domain"] = [("id", "in", self.ids)]
         else:
             form = self.env.ref(
-                'mrp_production_request.view_mrp_production_request_form'
+                "mrp_production_request.view_mrp_production_request_form"
             )
-            action['views'] = [(form.id, 'form')]
-            action['res_id'] = self.ids[0]
+            action["views"] = [(form.id, "form")]
+            action["res_id"] = self.ids[0]
         return action
