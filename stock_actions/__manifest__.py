1d237d8104b11e35a088e532ac6f556e33abb4ee
Yann Papouin <ypa@decgroupe.com>
2023-04-27 11:29:03 +0200

    [IMP] stock_action: black, isort, prettier, readme, icon

--- stock_actions/__manifest__.py
+++ stock_actions/__manifest__.py
@@ -1,14 +1,12 @@
 {
-    'name': 'Stock Actions',
-    'version': "14.0.1.0.0",
-    'author': 'DEC, Yann Papouin',
-    'website': 'https://www.decgroupe.com',
-    'summary': "Public methods to control stock moves",
-    'depends': ['stock'],
-    #'force_migration':'12.0.0.0.0',
-    'data': [
-        'views/stock_move.xml',
-        'views/stock_picking.xml',
+    "name": "Stock Actions",
+    "version": "14.0.1.0.0",
+    "author": "DEC",
+    "website": "https://www.decgroupe.com",
+    "depends": ["stock"],
+    "data": [
+        "views/stock_move.xml",
+        "views/stock_picking.xml",
     ],
-    'installable': True
+    "installable": True,
 }
--- stock_actions/models/stock_move.py
+++ stock_actions/models/stock_move.py
@@ -1,7 +1,7 @@
 # Copyright (C) DEC SARL, Inc - All Rights Reserved.
 # Written by Yann Papouin <ypa at decgroupe.com>, Sep 2020
 
-from odoo import fields, models, api
+from odoo import api, fields, models
 
 
 class StockMove(models.Model):
@@ -9,7 +9,7 @@ class StockMove(models.Model):
 
     action_reassign_visible = fields.Boolean(
         string="Shows button to reassign",
-        compute='_compute_action_reassign_visible',
+        compute="_compute_action_reassign_visible",
         readonly=True,
     )
 
@@ -20,61 +20,59 @@ class StockMove(models.Model):
 
     def _compute_action_reassign_visible(self):
         for move in self:
-            visible = move.quantity_done < move.product_uom_qty \
-                and not move.is_locked \
-                and move.state in [
-                    'confirmed',
-                    'waiting',
-                    'partially_available',
-                    'assigned'
-                ]
+            visible = (
+                move.quantity_done < move.product_uom_qty
+                and not move.is_locked
+                and move.state
+                in ["confirmed", "waiting", "partially_available", "assigned"]
+            )
             move.action_reassign_visible = visible
 
-    @api.depends('procure_method', 'state')
+    @api.depends("procure_method", "state")
     def _compute_is_cancellable(self):
         self.is_cancellable = False
         for move in self:
-            if move.procure_method == 'make_to_stock':
-                if move.state not in ('done', 'cancel'):
+            if move.procure_method == "make_to_stock":
+                if move.state not in ("done", "cancel"):
                     move.is_cancellable = True
-            if move.procure_method == 'make_to_order':
-                if move.state in ('draft'):
+            if move.procure_method == "make_to_order":
+                if move.state in ("draft"):
                     move.is_cancellable = True
-                elif move.state in ('confirmed') and not move.move_orig_ids:
+                elif move.state in ("confirmed") and not move.move_orig_ids:
                     move.is_cancellable = True
 
     def action_confirm(self):
-        """ Inputs:
-                - 'draft'
-            Outputs:
-                - 'confirmed'
-                - 'waiting'
+        """Inputs:
+            - 'draft'
+        Outputs:
+            - 'confirmed'
+            - 'waiting'
         """
-        for move in self.filtered(lambda m: m.state in ['draft']):
+        for move in self.filtered(lambda m: m.state in ["draft"]):
             move._action_confirm()
 
     def action_assign(self):
-        """ Inputs:
-                - 'confirmed'
-                - 'waiting'
-                - 'partially_available'
-            Outputs:
-                - 'assigned'
-                - 'partially_available'
+        """Inputs:
+            - 'confirmed'
+            - 'waiting'
+            - 'partially_available'
+        Outputs:
+            - 'assigned'
+            - 'partially_available'
         """
         for move in self:
             move._action_assign()
 
     def action_reassign(self):
-        """ Inputs:
-                - 'assigned' --> unreserve --> 'confirmed'
-                - 'partially_available' --> unreserve --> 'confirmed'
-                - 'confirmed'
-                - 'waiting'
-                - 'partially_available'
-            Outputs:
-                - 'assigned'
-                - 'partially_available'
+        """Inputs:
+            - 'assigned' --> unreserve --> 'confirmed'
+            - 'partially_available' --> unreserve --> 'confirmed'
+            - 'confirmed'
+            - 'waiting'
+            - 'partially_available'
+        Outputs:
+            - 'assigned'
+            - 'partially_available'
         """
         for move in self:
             move._do_unreserve()
@@ -84,8 +82,8 @@ class StockMove(models.Model):
         self._recompute_state()
 
     def action_force_state_confirmed_to_assigned(self):
-        for move in self.filtered(lambda m: m.state == 'confirmed'):
-            move.write({'state': 'assigned'})
+        for move in self.filtered(lambda m: m.state == "confirmed"):
+            move.write({"state": "assigned"})
 
     def _action_cancel_stream(self, downstream=False, upstream=False):
         def get_downstream_moves(move):
@@ -104,7 +102,7 @@ class StockMove(models.Model):
                         res += get_upstream_moves(m)
             return res
 
-        moves_to_cancel = self.env['stock.move']
+        moves_to_cancel = self.env["stock.move"]
         for move in self:
             moves_to_cancel += move
             if downstream:
@@ -113,18 +111,18 @@ class StockMove(models.Model):
                 moves_to_cancel += get_upstream_moves(move)
 
         moves_to_cancel = moves_to_cancel.filtered(
-            lambda x: x.state not in ('done', 'cancel')
+            lambda x: x.state not in ("done", "cancel")
         )
         if moves_to_cancel:
             moves_to_cancel._action_cancel()
 
     def action_cancel(self):
-        """ Inputs:
-                - 'assigned' --> unreserve --> 'confirmed'
-                - 'partially_available' --> unreserve --> 'confirmed'
-                - 'confirmed'
-            Outputs:
-                - 'cancel'
+        """Inputs:
+            - 'assigned' --> unreserve --> 'confirmed'
+            - 'partially_available' --> unreserve --> 'confirmed'
+            - 'confirmed'
+        Outputs:
+            - 'cancel'
         """
         self._action_cancel_stream()
 
@@ -135,10 +133,10 @@ class StockMove(models.Model):
         self._action_cancel_stream(upstream=True)
 
     def action_done(self):
-        """ Inputs:
-                - 'draft' --> confirm --> 'confirmed'
-                - 'product_uom_qty' == 'quantity_done'
-            Outputs:
-                - 'done'
+        """Inputs:
+            - 'draft' --> confirm --> 'confirmed'
+            - 'product_uom_qty' == 'quantity_done'
+        Outputs:
+            - 'done'
         """
         self._action_done()
