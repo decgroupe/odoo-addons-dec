eb79e9aa22575abd909d3b03c9ce14f46d04940f
Yann Papouin <ypa@decgroupe.com>
2023-05-04 08:39:30 +0200

    [IMP] helpdesk_merge: black, isort, prettier, readme, icon

--- helpdesk_merge/__manifest__.py
+++ helpdesk_merge/__manifest__.py
@@ -1,16 +1,16 @@
 {
-    'name': 'Merge Helpdesk Tickets',
-    'version': "14.0.1.0.0",
-    'author': 'DEC, Yann Papouin ',
-    'website': 'https://www.decgroupe.com',
-    'depends': [
-        'helpdesk_mgmt',
-        'deltatech_merge',
+    "name": "Merge Helpdesk Tickets",
+    "version": "14.0.1.0.0",
+    "author": "DEC",
+    "website": "https://www.decgroupe.com",
+    "depends": [
+        "helpdesk_mgmt",
+        "deltatech_merge",
     ],
-    'data': [
-        'security/res_groups.xml',
-        'security/ir.model.access.csv',
-        'wizard/merge_helpdesk_ticket.xml',
+    "data": [
+        "security/res_groups.xml",
+        "security/ir.model.access.csv",
+        "wizard/merge_helpdesk_ticket.xml",
     ],
-    'installable': True
+    "installable": True,
 }
--- helpdesk_merge/wizard/merge_helpdesk_ticket.py
+++ helpdesk_merge/wizard/merge_helpdesk_ticket.py
@@ -1,7 +1,7 @@
 # Copyright (C) DEC SARL, Inc - All Rights Reserved.
 # Written by Yann Papouin <ypa at decgroupe.com>, Jun 2022
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class MergeHelpdeskTicket(models.TransientModel):
@@ -14,17 +14,16 @@ class MergeHelpdeskTicket(models.TransientModel):
     object_ids = fields.Many2many(_model_merge, string="Helpdesk Ticket")
     dst_object_id = fields.Many2one(_model_merge, string="Helpdesk Ticket")
     company_id = fields.Many2one(
-        'res.company',
-        related='dst_object_id.company_id',
-        string='Company',
+        comodel_name="res.company",
+        related="dst_object_id.company_id",
+        string="Company",
         readonly=True,
-        default=lambda self: self.env.user.company_id
+        default=lambda self: self.env.user.company_id,
     )
 
     def _merge(self, object_ids, dst_object=None, extra_checks=True):
         return super()._merge(
             object_ids,
             dst_object.with_context(mail_auto_subscribe_no_notify=True),
-            extra_checks
+            extra_checks,
         )
-
