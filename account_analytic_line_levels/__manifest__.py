cdfc8765d2351539927c4aed5d434dad7d03e745
Yann Papouin <ypa@decgroupe.com>
2023-05-10 17:06:16 +0200

    [IMP] account_analytic_line_levels: black, isort, prettier, readme, icon

--- account_analytic_line_levels/__manifest__.py
+++ account_analytic_line_levels/__manifest__.py
@@ -1,10 +1,11 @@
 {
-    'name': 'Account Analytic Line Levels',
-    'version': "14.0.1.0.0",
-    'author': 'DEC, Yann Papouin',
-    'website': 'https://www.decgroupe.com',
-    'summary': "Create three primary level",
-    'depends': ['account_analytic_parent', ],
-    'data': [],
-    'installable': True
+    "name": "Account Analytic Line Levels",
+    "version": "14.0.1.0.0",
+    "author": "DEC",
+    "website": "https://www.decgroupe.com",
+    "depends": [
+        "account_analytic_parent",
+    ],
+    "data": [],
+    "installable": True,
 }
--- account_analytic_line_levels/models/account_analytic_line.py
+++ account_analytic_line_levels/models/account_analytic_line.py
@@ -1,34 +1,32 @@
 # Copyright (C) DEC SARL, Inc - All Rights Reserved.
 # Written by Yann Papouin <ypa at decgroupe.com>, Feb 2021
 
-import progressbar
-
 from odoo import fields, models, api
 
 
 class AccountAnalyticLine(models.Model):
-    _inherit = 'account.analytic.line'
+    _inherit = "account.analytic.line"
 
     account_primary_id = fields.Many2one(
-        'account.analytic.account',
-        string='Analytic Account (Primary)',
-        compute='_compute_analytic_account_level',
-        store=True
+        comodel_name="account.analytic.account",
+        string="Analytic Account (Primary)",
+        compute="_compute_analytic_account_level",
+        store=True,
     )
     account_secondary_id = fields.Many2one(
-        'account.analytic.account',
-        string='Analytic Account (Secondary)',
-        compute='_compute_analytic_account_level',
-        store=True
+        comodel_name="account.analytic.account",
+        string="Analytic Account (Secondary)",
+        compute="_compute_analytic_account_level",
+        store=True,
     )
     account_tertiary_id = fields.Many2one(
-        'account.analytic.account',
-        string='Analytic Account (Tertiary)',
-        compute='_compute_analytic_account_level',
-        store=True
+        comodel_name="account.analytic.account",
+        string="Analytic Account (Tertiary)",
+        compute="_compute_analytic_account_level",
+        store=True,
     )
 
-    @api.depends('account_id', 'account_id.parent_id')
+    @api.depends("account_id", "account_id.parent_id")
     def _compute_analytic_account_level(self):
         for rec in self:
             accounts = []
@@ -37,7 +35,7 @@ class AccountAnalyticLine(models.Model):
                 accounts.insert(0, account_id)
                 account_id = account_id.parent_id
             # Fill missing value with False to unset them
-            while(len(accounts) < 3):
+            while len(accounts) < 3:
                 accounts.append(False)
             rec.account_primary_id = accounts[0]
             rec.account_secondary_id = accounts[1]
