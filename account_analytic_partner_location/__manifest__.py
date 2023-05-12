f3dd7e7f809181959264d64caacb0336c0a4eb8a
Yann Papouin <ypa@decgroupe.com>
2023-05-12 14:38:48 +0200

    [IMP] account_analytic_partner_location: black, isort, prettier, readme, icon

--- account_analytic_partner_location/__manifest__.py
+++ account_analytic_partner_location/__manifest__.py
@@ -1,13 +1,14 @@
 {
-    'name': 'Account Analytic Partner Location',
-    'version': "14.0.1.0.0",
-    'author': 'DEC, Yann Papouin',
-    'website': 'https://www.decgroupe.com',
-    'summary': '''Add partner location to analytic lines''',
-    'depends': [
-        'account',
-        'base_location',
+    "name": "Account Analytic Partner Location",
+    "version": "14.0.1.0.0",
+    "author": "DEC",
+    "website": "https://www.decgroupe.com",
+    "depends": [
+        "account",
+        "base_location",
     ],
-    'data': ['views/account_analytic_line.xml', ],
-    'installable': True
+    "data": [
+        "views/account_analytic_line.xml",
+    ],
+    "installable": True,
 }
--- account_analytic_partner_location/models/account_analytic_line.py
+++ account_analytic_partner_location/models/account_analytic_line.py
@@ -5,17 +5,17 @@ from odoo import fields, models
 
 
 class AccountAnalyticLine(models.Model):
-    _inherit = 'account.analytic.line'
+    _inherit = "account.analytic.line"
 
     partner_zip_id = fields.Many2one(
-        'res.city.zip',
-        related='partner_id.zip_id',
+        comodel_name="res.city.zip",
+        related="partner_id.zip_id",
         string="Partner's ZIP",
         store=True,
     )
     partner_city_id = fields.Many2one(
-        'res.city',
-        related='partner_id.city_id',
+        comodel_name="res.city",
+        related="partner_id.city_id",
         string="Partner's City",
         store=True,
     )
