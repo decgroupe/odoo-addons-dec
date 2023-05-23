8fe3f1561e8d3de6034b50101d42fb5f91c7ee83
Yann Papouin <ypa@decgroupe.com>
2023-05-23 11:04:32 +0200

    [IMP] crm_lead_partner_location_department: black, isort, prettier, readme, icon

--- crm_lead_partner_location_department/__manifest__.py
+++ crm_lead_partner_location_department/__manifest__.py
@@ -1,13 +1,14 @@
 {
-    'name': 'CRM Partner State and Department',
-    'version': "14.0.1.0.0",
-    'author': 'DEC, Yann Papouin',
-    'website': 'https://www.decgroupe.com',
-    'summary': '''Add partner state and department to CRM''',
-    'depends': [
-        'crm_lead_partner_location',
-        'l10n_fr_base_location_department',
+    "name": "CRM Partner State and Department",
+    "version": "14.0.1.0.0",
+    "author": "DEC",
+    "website": "https://www.decgroupe.com",
+    "depends": [
+        "crm_lead_partner_location",
+        "l10n_fr_base_location_department",
     ],
-    'data': ['views/crm_lead.xml', ],
-    'installable': True
+    "data": [
+        "views/crm_lead.xml",
+    ],
+    "installable": True,
 }
--- crm_lead_partner_location_department/models/crm_lead.py
+++ crm_lead_partner_location_department/models/crm_lead.py
@@ -1,34 +1,34 @@
 # Copyright (C) DEC SARL, Inc - All Rights Reserved.
 # Written by Yann Papouin <ypa at decgroupe.com>, May 2021
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class CrmLead(models.Model):
-    _inherit = 'crm.lead'
+    _inherit = "crm.lead"
 
     partner_department_id = fields.Many2one(
-        comodel_name='res.country.department',
-        related='partner_id.department_id',
+        comodel_name="res.country.department",
+        related="partner_id.department_id",
         string="Department",
         store=True,
     )
     partner_state_id = fields.Many2one(
-        comodel_name='res.country.state',
-        related='partner_id.state_id',
+        comodel_name="res.country.state",
+        related="partner_id.state_id",
         string="Partner's State",
         store=True,
     )
 
     partner_shipping_department_id = fields.Many2one(
-        comodel_name='res.country.department',
-        related='partner_shipping_id.department_id',
+        comodel_name="res.country.department",
+        related="partner_shipping_id.department_id",
         string="Shipping Partner's Department",
         store=True,
     )
     partner_shipping_state_id = fields.Many2one(
-        comodel_name='res.country.state',
-        related='partner_shipping_id.state_id',
+        comodel_name="res.country.state",
+        related="partner_shipping_id.state_id",
         string="Shipping Partner's State",
         store=True,
     )
