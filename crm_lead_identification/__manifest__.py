ecd2fc3961374c8b6217787d88545d35289b768c
Yann Papouin <ypa@decgroupe.com>
2023-05-23 11:10:01 +0200

    [IMP] crm_lead_identification: black, isort, prettier, readme, icon

--- crm_lead_identification/__manifest__.py
+++ crm_lead_identification/__manifest__.py
@@ -1,16 +1,13 @@
 {
-    'name': 'CRM (Identification)',
-    'version': "14.0.1.0.0",
-    'author': 'DEC, Yann Papouin',
-    'website': 'https://www.decgroupe.com',
-    'summary':
-        "Customize name_search and add partner informations "
-        "on CRM",
-    'depends': [
-        'partner_identification_base',
-        'crm_lead_number',
-        'crm_lead_partner_location',
+    "name": "CRM (Identification)",
+    "version": "14.0.1.0.0",
+    "author": "DEC",
+    "website": "https://www.decgroupe.com",
+    "depends": [
+        "partner_identification_base",
+        "crm_lead_number",
+        "crm_lead_partner_location",
     ],
-    'data': [],
-    'installable': True
+    "data": [],
+    "installable": True,
 }
--- crm_lead_identification/models/crm_lead.py
+++ crm_lead_identification/models/crm_lead.py
@@ -3,63 +3,63 @@
 
 import logging
 
-from odoo import fields, api, models
+from odoo import api, models
 
 _logger = logging.getLogger(__name__)
 
-SEARCH_SEPARATOR = ' →'
+SEARCH_SEPARATOR = " →"
 
 
 class CrmLead(models.Model):
-    _inherit = 'crm.lead'
+    _inherit = "crm.lead"
 
     @api.model
-    def name_search(self, name, args=None, operator='ilike', limit=100):
+    def name_search(self, name, args=None, operator="ilike", limit=100):
         if SEARCH_SEPARATOR in name:
             name = name.partition(SEARCH_SEPARATOR)[0]
-        names = super(CrmLead, self.with_context(
-            name_search=True
-        )).name_search(name=name, args=args, operator=operator, limit=limit)
+        names = super(CrmLead, self.with_context(name_search=True)).name_search(
+            name=name, args=args, operator=operator, limit=limit
+        )
         return names
 
     def name_get(self):
-        if self.env.context.get('name_search'):
+        if self.env.context.get("name_search"):
             return self.name_get_from_search()
         else:
             return super().name_get()
 
-    @api.depends('number', 'name')
+    @api.depends("number", "name")
     def name_get_from_search(self):
-        """ Custom naming to quickly identify a lead
-        """
+        """Custom naming to quickly identify a lead"""
         res = []
         for rec in self:
-            identification = ' '.join(rec._get_name_identifications())
-            name = '[%s] %s%s %s' % (
-                rec.number, rec.name, SEARCH_SEPARATOR, identification
-            )
+            name = "[%s] %s" % (rec.number, rec.name)
+            identification = " ".join(rec._get_name_identifications())
+            if identification:
+                name = "%s%s %s" % (name, SEARCH_SEPARATOR, identification)
             if rec.stage_id and not rec.stage_id.name[0].isalpha():
                 emoji = rec.stage_id.name[0]
-                name = '%s %s' % (emoji, name)
+                name = "%s %s" % (emoji, name)
             res.append((rec.id, name))
         return res
 
-    @api.depends('partner_id.name', 'partner_zip_id')
+    @api.depends("partner_id.name", "partner_zip_id")
     def _get_name_identifications(self):
         self.ensure_one()
         res = []
         if self.partner_id:
             res.append(
-                '%s %s' % (
+                "%s %s"
+                % (
                     self.partner_id._get_contact_type_emoji(),
-                    self.partner_id.display_name
+                    self.partner_id.display_name,
                 )
             )
         if self.partner_zip_id:
-            res.append('🗺️ %s' % (self.partner_zip_id.display_name))
+            res.append("🗺️ %s" % (self.partner_zip_id.display_name))
         return res
 
-    @api.depends('partner_zip_id', 'partner_zip_id.name')
+    @api.depends("partner_zip_id", "partner_zip_id.name")
     def _compute_names(self):
         super()._compute_names()
         for rec in self:
