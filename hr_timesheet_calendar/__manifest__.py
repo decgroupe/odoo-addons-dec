5aee223645ec3db6ebb0ea7e79ce301d7a95485f
Yann Papouin <ypa@decgroupe.com>
2023-05-23 12:04:09 +0200

    [IMP] hr_timesheet_calendar: black, isort, prettier, readme, icon

--- hr_timesheet_calendar/__manifest__.py
+++ hr_timesheet_calendar/__manifest__.py
@@ -1,17 +1,14 @@
 {
-    'name': 'HR Timesheet Calendar',
-    'version': "14.0.1.0.0",
-    'author': 'DEC, Yann Papouin',
-    'website': 'https://www.decgroupe.com',
-    'summary':
-        "Add calendar view for Timesheet lines",
-    'depends': [
-        'hr_timesheet',
-        'project_timesheet_time_control'
+    "name": "HR Timesheet Calendar",
+    "version": "14.0.1.0.0",
+    "author": "DEC",
+    "website": "https://www.decgroupe.com",
+    "depends": [
+        "hr_timesheet",
+        "project_timesheet_time_control",
     ],
-    'data': [
-        'views/account_analytic_line.xml',
-
+    "data": [
+        "views/account_analytic_line.xml",
     ],
-    'installable': True
+    "installable": True,
 }
--- hr_timesheet_calendar/models/account_analytic_line.py
+++ hr_timesheet_calendar/models/account_analytic_line.py
@@ -5,26 +5,26 @@ from odoo import api, fields, models
 
 
 class AccountAnalyticLine(models.Model):
-    _inherit = 'account.analytic.line'
+    _inherit = "account.analytic.line"
 
     same_task_name = fields.Boolean(
-        compute='_compute_same_task_name',
-        help='Technical field to apply a filter when the task name equals '
-        'the analytic one',
+        compute="_compute_same_task_name",
+        help="Technical field to apply a filter when the task name equals "
+        "the analytic one",
     )
     calendar_name = fields.Char(
-        compute='_compute_same_task_name',
-        help='Technical field to hide the name from calendar view since '
-        'there is not support for invisible attrs in this kind of view',
+        compute="_compute_same_task_name",
+        help="Technical field to hide the name from calendar view since "
+        "there is not support for invisible attrs in this kind of view",
     )
 
-    @api.depends('name', 'task_id.name')
+    @api.depends("name", "task_id.name")
     def _compute_same_task_name(self):
         for rec in self:
             rec.calendar_name = rec.name
             if rec.task_id:
-                rec.same_task_name = (rec.name == rec.task_id.name)
+                rec.same_task_name = rec.name == rec.task_id.name
                 if rec.same_task_name:
-                    rec.calendar_name = ''
+                    rec.calendar_name = ""
             else:
                 rec.same_task_name = False
