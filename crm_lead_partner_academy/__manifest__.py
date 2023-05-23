cd25747ae4bf9490335c011a7e675323f0cdf537
Yann Papouin <ypa@decgroupe.com>
2023-05-23 11:30:08 +0200

    [IMP] crm_lead_partner_academy: black, isort, prettier, readme, icon

--- crm_lead_partner_academy/__manifest__.py
+++ crm_lead_partner_academy/__manifest__.py
@@ -1,13 +1,14 @@
 {
-    'name': 'CRM Partner Academy',
-    'version': "14.0.1.0.0",
-    'author': 'DEC, Yann Papouin',
-    'website': 'https://www.decgroupe.com',
-    'summary': '''Add partner academy to CRM''',
-    'depends': [
-        'crm',
-        'partner_academy',
+    "name": "CRM Partner Academy",
+    "version": "14.0.1.0.0",
+    "author": "DEC",
+    "website": "https://www.decgroupe.com",
+    "depends": [
+        "crm",
+        "partner_academy",
     ],
-    'data': ['views/crm_lead.xml', ],
-    'installable': True
+    "data": [
+        "views/crm_lead.xml",
+    ],
+    "installable": True,
 }
--- crm_lead_partner_academy/models/crm_lead.py
+++ crm_lead_partner_academy/models/crm_lead.py
@@ -1,15 +1,15 @@
 # Copyright (C) DEC SARL, Inc - All Rights Reserved.
 # Written by Yann Papouin <ypa at decgroupe.com>, May 2021
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class CrmLead(models.Model):
-    _inherit = 'crm.lead'
+    _inherit = "crm.lead"
 
     partner_academy_id = fields.Many2one(
-        'res.partner.academy',
-        related='partner_id.academy_id',
+        comodel_name="res.partner.academy",
+        related="partner_id.academy_id",
         string="Partner's Academy",
         store=True,
     )
