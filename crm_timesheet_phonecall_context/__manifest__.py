569ef46b22f1ea3f07401e978efdbe908838fb4a
Yann Papouin <ypa@decgroupe.com>
2023-05-25 10:27:49 +0200

    [IMP] crm_timesheet_phonecall_context: black, isort, prettier, readme, icon

--- crm_timesheet_phonecall_context/__manifest__.py
+++ crm_timesheet_phonecall_context/__manifest__.py
@@ -1,17 +1,14 @@
 {
-    'name': 'CRM Timesheet Phonecall Context',
-    'version': "14.0.1.0.0",
-    'author': 'DEC, Yann Papouin',
-    'website': 'https://www.decgroupe.com',
-    'summary':
-        "Fix missing default project when creating a phonecall",
-    'depends': [
-        'crm_timesheet',
-        'crm_phonecall_timesheet',
+    "name": "CRM Timesheet Phonecall Context",
+    "version": "14.0.1.0.0",
+    "author": "DEC",
+    "website": "https://www.decgroupe.com",
+    "depends": [
+        "crm_timesheet",
+        "crm_phonecall_timesheet",
     ],
-    'data': [
-        'views/account_analytic_line.xml',
-
+    "data": [
+        "views/account_analytic_line.xml",
     ],
-    'installable': True
+    "installable": True,
 }
--- crm_timesheet_phonecall_context/models/account_analytic_line.py
+++ crm_timesheet_phonecall_context/models/account_analytic_line.py
@@ -5,14 +5,14 @@ from odoo import api, models
 
 
 class AccountAnalyticLine(models.Model):
-    _inherit = 'account.analytic.line'
+    _inherit = "account.analytic.line"
 
-    @api.onchange('project_id')
+    @api.onchange("project_id")
     def _onchange_project_id(self):
         res = super()._onchange_project_id()
-        if 'domain' in res:
+        if "domain" in res:
             filter = []
             if self.project_id:
-                filter = [('project_id', '=', self.project_id.id)]
-            res['domain']['phonecall_id'] = filter
+                filter = [("project_id", "=", self.project_id.id)]
+            res["domain"]["phonecall_id"] = filter
         return res
