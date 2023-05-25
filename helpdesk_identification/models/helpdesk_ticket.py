d895063b019b9154a7b70cd06620dec66d488de6
Yann Papouin <ypa@decgroupe.com>
2023-05-25 14:24:36 +0200

    [IMP] helpdesk_identification: black, isort, prettier, readme, icon

--- helpdesk_identification/models/helpdesk_ticket.py
+++ helpdesk_identification/models/helpdesk_ticket.py
@@ -1,16 +1,16 @@
 # Copyright (C) DEC SARL, Inc - All Rights Reserved.
 # Written by Yann Papouin <ypa at decgroupe.com>, Nov 2021
 
-from odoo import models, api, fields
+from odoo import api, models
 
-SEARCH_SEPARATOR = ' →'
+SEARCH_SEPARATOR = " →"
 
 
 class HelpdeskTicket(models.Model):
-    _inherit = 'helpdesk.ticket'
+    _inherit = "helpdesk.ticket"
 
     @api.model
-    def name_search(self, name, args=None, operator='ilike', limit=100):
+    def name_search(self, name, args=None, operator="ilike", limit=100):
         # Make a search with default criteria
         names = super().name_search(
             name=name, args=args, operator=operator, limit=limit
@@ -20,11 +20,11 @@ class HelpdeskTicket(models.Model):
         for item in names:
             rec = self.browse(item[0])[0]
             rec = rec.with_context(idf_include_name_search=True)
-            identification = ' '.join(rec._get_name_identifications())
+            identification = " ".join(rec._get_name_identifications())
             result.append((item[0], identification))
         return result
 
-    @api.depends('team_id', 'stage_id')
+    @api.depends("team_id", "stage_id")
     def _get_name_identifications(self):
         self.ensure_one()
         res = []
@@ -32,17 +32,18 @@ class HelpdeskTicket(models.Model):
             res.append(self.team_id.name)
         if self.partner_id:
             res.append(
-                '%s %s' % (
+                "%s %s"
+                % (
                     self.partner_id._get_contact_type_emoji(),
-                    self.partner_id.display_name
+                    self.partner_id.display_name,
                 )
             )
         if self.partner_zip_id:
-            res.append('🗺️ %s' % (self.partner_zip_id.display_name))
-        if self.env.context.get('idf_include_name_search'):
+            res.append("🗺️ %s" % (self.partner_zip_id.display_name))
+        if self.env.context.get("idf_include_name_search"):
             name = self.display_name
             if res:
-                res.insert(0, '%s%s' % (name, SEARCH_SEPARATOR))
+                res.insert(0, "%s%s" % (name, SEARCH_SEPARATOR))
             else:
                 res.insert(0, name)
         if self.stage_id and not self.stage_id.name[0].isalpha():
---
+++ helpdesk_identification/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+Customize name_search and add ticket informations
