1d81a46004c467f065de0816b109121ab4a96634
Yann Papouin <ypa@decgroupe.com>
2023-05-25 13:56:45 +0200

    [IMP] helpdesk_timesheet_autofill: black, isort, prettier, readme, icon

--- helpdesk_timesheet_autofill/__manifest__.py
+++ helpdesk_timesheet_autofill/__manifest__.py
@@ -1,13 +1,12 @@
 {
-    'name': 'Helpdesk Timesheet Auto-fill',
-    'version': "14.0.1.0.0",
-    'author': 'DEC, Yann Papouin',
-    'website': 'https://www.decgroupe.com',
-    'summary': "Fill data with previously entered values",
-    'depends': [
-        'helpdesk_mgmt_timesheet',
-        'hr_timesheet_autofill',
+    "name": "Helpdesk Timesheet Auto-fill",
+    "version": "14.0.1.0.0",
+    "author": "DEC",
+    "website": "https://www.decgroupe.com",
+    "depends": [
+        "helpdesk_mgmt_timesheet",
+        "hr_timesheet_autofill",
     ],
-    'data': [],
-    'installable': True
+    "data": [],
+    "installable": True,
 }
--- helpdesk_timesheet_autofill/models/account_analytic_line.py
+++ helpdesk_timesheet_autofill/models/account_analytic_line.py
@@ -1,13 +1,13 @@
 # Copyright (C) DEC SARL, Inc - All Rights Reserved.
 # Written by Yann Papouin <ypa at decgroupe.com>, Feb 2021
 
-from odoo import api, fields, models
+from odoo import api, models
 
 
 class AccountAnalyticLine(models.Model):
-    _inherit = 'account.analytic.line'
+    _inherit = "account.analytic.line"
 
     @api.model
     def get_autofill_fields(self):
         res = super().get_autofill_fields()
-        return res + ['ticket_id']
+        return res + ["ticket_id"]
