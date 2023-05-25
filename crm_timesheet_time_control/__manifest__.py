1d4b37d4f1263a379e33a932a8b6768af8594468
Yann Papouin <ypa@decgroupe.com>
2023-05-25 13:45:29 +0200

    [IMP] crm_timesheet_time_control: black, isort, prettier, readme, icon

--- crm_timesheet_time_control/__manifest__.py
+++ crm_timesheet_time_control/__manifest__.py
@@ -1,17 +1,14 @@
 {
-    'name': 'CRM Timesheet Time Control',
-    'version': "14.0.1.0.0",
-    'author': 'DEC, Yann Papouin',
-    'website': 'https://www.decgroupe.com',
-    'summary':
-        "Exact start time for a timesheet line",
-    'depends': [
-        'crm_timesheet',
-        'project_timesheet_time_control',
+    "name": "CRM Timesheet Time Control",
+    "version": "14.0.1.0.0",
+    "author": "DEC",
+    "website": "https://www.decgroupe.com",
+    "depends": [
+        "crm_timesheet",
+        "project_timesheet_time_control",
     ],
-    'data': [
-        'views/crm_lead.xml',
-
+    "data": [
+        "views/crm_lead.xml",
     ],
-    'installable': True
+    "installable": True,
 }
