386f6f2a90af2497765ee943d38ec52a61be3a65
Yann Papouin <ypa@decgroupe.com>
2023-05-29 15:42:10 +0200

    [IMP] document_page_default: black, isort, prettier, readme, icon

--- document_page_default/__manifest__.py
+++ document_page_default/__manifest__.py
@@ -1,14 +1,13 @@
 {
-    'name': 'Document Page Default',
-    'version': "14.0.1.0.0",
-    'author': 'DEC, Yann Papouin',
-    'website': 'https://www.decgroupe.com',
-    'summary': '''Set default draft name and summary''',
-    'depends': [
-        'document_page',
+    "name": "Document Page Default",
+    "version": "14.0.1.0.0",
+    "author": "DEC",
+    "website": "https://www.decgroupe.com",
+    "depends": [
+        "document_page",
     ],
-    'data': [
-        'views/document_page.xml',
+    "data": [
+        "views/document_page.xml",
     ],
-    'installable': True
+    "installable": True,
 }
--- document_page_default/models/document_page.py
+++ document_page_default/models/document_page.py
@@ -1,14 +1,12 @@
 # Copyright (C) DEC SARL, Inc - All Rights Reserved.
 # Written by Yann Papouin <ypa at decgroupe.com>, Feb 2021
 
-import re
-import string
 
-from odoo import models, fields, api, _
+from odoo import _, api, fields, models
 
 
 class DocumentPage(models.Model):
-    _inherit = 'document.page'
+    _inherit = "document.page"
 
     @api.model
     def _default_draft_name(self):
@@ -22,7 +20,7 @@ class DocumentPage(models.Model):
     draft_summary = fields.Char(default=_default_draft_summary)
 
     def write(self, vals):
-        if not 'draft_name' in vals:
-            vals['draft_name'] = "Rev {:02d}".format(len(self.history_ids) + 1)
+        if not "draft_name" in vals:
+            vals["draft_name"] = "Rev {:02d}".format(len(self.history_ids) + 1)
         result = super(DocumentPage, self).write(vals)
         return result
