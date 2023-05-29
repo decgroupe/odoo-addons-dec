e057584ef52b4848e588cc6c7422c7dc9bf52f27
Yann Papouin <ypa@decgroupe.com>
2023-05-29 15:35:48 +0200

    [IMP] document_page_category_name: black, isort, prettier, readme, icon

--- document_page_category_name/__manifest__.py
+++ document_page_category_name/__manifest__.py
@@ -1,10 +1,11 @@
 {
-    'name': 'Document Page Category Complete Name',
-    'version': "14.0.1.0.0",
-    'author': 'DEC, Yann Papouin',
-    'website': 'https://www.decgroupe.com',
-    'summary': "Displays the complete category",
-    'depends': ['document_page', ],
-    'data': ['views/document_page.xml', ],
-    'installable': True
+    "name": "Document Page Category Complete Name",
+    "version": "14.0.1.0.0",
+    "author": "DEC",
+    "website": "https://www.decgroupe.com",
+    "depends": [
+        "document_page",
+    ],
+    "data": [],
+    "installable": True,
 }
--- document_page_category_name/models/document_page.py
+++ document_page_category_name/models/document_page.py
@@ -1,33 +1,34 @@
 # Copyright (C) DEC SARL, Inc - All Rights Reserved.
 # Written by Yann Papouin <ypa at decgroupe.com>, May 2021
 
-from odoo import models, fields, api, _
+from odoo import api, fields, models
 
 
 class DocumentPage(models.Model):
-    _inherit = 'document.page'
+    _inherit = "document.page"
     _parent_name = "parent_id"
     _parent_store = True
-    _order = 'complete_name'
+    _order = "complete_name"
 
     parent_path = fields.Char(index=True)
     complete_name = fields.Char(
-        'Complete Name',
-        compute='_compute_complete_name',
+        string="Complete Name",
+        compute="_compute_complete_name",
         store=True,
     )
 
-    @api.depends('name', 'parent_id.complete_name')
+    @api.depends("name", "parent_id.complete_name")
     def _compute_complete_name(self):
         for rec in self:
-            if rec.parent_id and rec.type == 'category':
-                rec.complete_name = '%s / %s' % (
-                    rec.parent_id.complete_name, rec.name,
+            if rec.parent_id and rec.type == "category":
+                rec.complete_name = "%s / %s" % (
+                    rec.parent_id.complete_name,
+                    rec.name,
                 )
             else:
                 rec.complete_name = rec.name
 
-    @api.depends('name', 'complete_name', 'type')
+    @api.depends("name", "complete_name", "type")
     def name_get(self):
         result = []
         for rec in self:
