04f056c17b7de8049d912424575ae52b0173942f
Yann Papouin <ypa@decgroupe.com>
2023-05-30 16:50:09 +0200

    [IMP] mail_action_view: black, isort, prettier, readme, icon

--- mail_action_view/__manifest__.py
+++ mail_action_view/__manifest__.py
@@ -1,10 +1,11 @@
 {
-    'name': 'Social Action View',
-    'version': "14.0.1.0.0",
-    'author': 'DEC, Yann Papouin',
-    'website': 'https://www.decgroupe.com',
-    'summary': "Code shortcuts to open base views",
-    'depends': ['mail', ],
-    'data': [],
-    'installable': True
+    "name": "Social Action View",
+    "version": "14.0.1.0.0",
+    "author": "DEC",
+    "website": "https://www.decgroupe.com",
+    "depends": [
+        "mail",
+    ],
+    "data": [],
+    "installable": True,
 }
--- mail_action_view/models/mail_activity.py
+++ mail_action_view/models/mail_activity.py
@@ -1,26 +1,26 @@
 # Copyright (C) DEC SARL, Inc - All Rights Reserved.
 # Written by Yann Papouin <ypa at decgroupe.com>, Sep 2022
 
-from odoo import models, api
+from odoo import api, models
 
 
 class MailActivity(models.Model):
-    _inherit = 'mail.activity'
+    _inherit = "mail.activity"
 
     @api.model
     def action_view_base(self):
-        #action = self.env.ref('mail.mail_activity_action').read()[0]
+        # action = self.env.ref('mail.mail_activity_action').read()[0]
         action = {
-            'type': 'ir.actions.act_window',
-            'name': 'My Action Name',
-            'display_name': 'Activities',
-            'res_model': 'mail.activity',
-            'context': '{}',
-            'domain': '[]',
-            'filter': False,
-            'target': 'current',
-            'view_mode': 'form',
-            'view_type': 'form',
+            "type": "ir.actions.act_window",
+            "name": "My Action Name",
+            "display_name": "Activities",
+            "res_model": "mail.activity",
+            "context": "{}",
+            "domain": "[]",
+            "filter": False,
+            "target": "current",
+            "view_mode": "form",
+            "view_type": "form",
         }
         return action
 
@@ -29,9 +29,9 @@ class MailActivity(models.Model):
         if not self.ids:
             pass
         elif len(self.ids) > 1:
-            action['domain'] = [('id', 'in', self.ids)]
+            action["domain"] = [("id", "in", self.ids)]
         else:
-            form = self.env.ref('mail.mail_activity_view_form_popup')
-            action['views'] = [(form.id, 'form')]
-            action['res_id'] = self.ids[0]
+            form = self.env.ref("mail.mail_activity_view_form_popup")
+            action["views"] = [(form.id, "form")]
+            action["res_id"] = self.ids[0]
         return action
