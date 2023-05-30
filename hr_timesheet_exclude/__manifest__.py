1170ed39a5fa47d3b0426ed5513ef8bc9a6ebcee
Yann Papouin <ypa@decgroupe.com>
2023-05-30 15:04:49 +0200

    [IMP] hr_timesheet_exclude: Use dedicated view from `hr_timesheet_autofill` for timesheet entry

--- hr_timesheet_exclude/__manifest__.py
+++ hr_timesheet_exclude/__manifest__.py
@@ -4,7 +4,7 @@
     "author": "DEC",
     "website": "https://www.decgroupe.com",
     "depends": [
-        "hr_timesheet",
+        "hr_timesheet_autofill",
         "sale_timesheet_line_exclude",
         # 'sale_timesheet_task_exclude', [MIG] 14.0: Not needed anymore acoording to https://github.com/OCA/timesheet/pull/440#issuecomment-1235611830
     ],
