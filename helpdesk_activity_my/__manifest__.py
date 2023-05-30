3e2c98eebcb04eb068a7c72ebca32af85716afd0
Yann Papouin <ypa@decgroupe.com>
2023-05-30 13:45:17 +0200

    [IMP] helpdesk_activity_my: black, isort, prettier, readme, icon

--- helpdesk_activity_my/__manifest__.py
+++ helpdesk_activity_my/__manifest__.py
@@ -1,17 +1,15 @@
 {
-    'name': 'Helpdesk My Activities',
-    'version': "14.0.1.0.0",
-    'author': 'DEC, Yann Papouin',
-    'website': 'https://www.decgroupe.com',
-    'summary':
-        "Backport next activity workflow from Odoo 14.0 but use current"
-        "user",
-    'depends':
-        [
-            'helpdesk_mgmt',
-            'web',
-            'mail_activity_my',
-        ],
-    'data': ['views/helpdesk_ticket.xml', ],
-    'installable': True
+    "name": "Helpdesk My Activities",
+    "version": "14.0.1.0.0",
+    "author": "DEC",
+    "website": "https://www.decgroupe.com",
+    "depends": [
+        "helpdesk_mgmt",
+        "web",
+        "mail_activity_my",
+    ],
+    "data": [
+        "views/helpdesk_ticket.xml",
+    ],
+    "installable": True,
 }
--- helpdesk_activity_my/models/helpdesk_ticket.py
+++ helpdesk_activity_my/models/helpdesk_ticket.py
@@ -1,15 +1,10 @@
 # Copyright (C) DEC SARL, Inc - All Rights Reserved.
 # Written by Yann Papouin <ypa at decgroupe.com>, Feb 2022
 
-import logging
 
-from datetime import date, datetime, timedelta
-
-from odoo import api, models
-
-_logger = logging.getLogger(__name__)
+from odoo import models
 
 
 class HelpdeskTicket(models.Model):
-    _name = 'helpdesk.ticket'
-    _inherit = ['helpdesk.ticket', 'mail.activity.my.mixin']
+    _name = "helpdesk.ticket"
+    _inherit = ["helpdesk.ticket", "mail.activity.my.mixin"]
