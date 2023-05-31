579018d268a6bff09b1da047b062e15075b0b8e4
Yann Papouin <ypa@decgroupe.com>
2023-05-31 17:05:19 +0200

    [IMP] mail_qweb: black, isort, prettier, readme, icon

--- mail_qweb/__manifest__.py
+++ mail_qweb/__manifest__.py
@@ -1,16 +1,17 @@
 {
-    'name': 'Mail QWeb',
-    'version': "14.0.1.0.0",
-    'author': 'DEC, Yann Papouin',
-    'website': 'https://www.decgroupe.com',
-    'summary': "Default templates",
+    "name": "Mail QWeb",
+    "version": "14.0.1.0.0",
+    "author": "DEC",
+    "website": "https://www.decgroupe.com",
     # TODO: rewrite templates of message_notification_email/mail_notification_light from odoo/addons/mail/data/mail_data.xml
-    'depends': [
-        'mail',
-        'mail_inline_css',
-        'email_template_qweb',
-        'partner_company_type',
+    "depends": [
+        "mail",
+        "mail_inline_css",
+        "email_template_qweb",
+        "partner_company_type",
     ],
-    'data': ['data/ir_ui_view.xml', ],
-    'installable': True
+    "data": [
+        "data/ir_ui_view.xml",
+    ],
+    "installable": True,
 }
