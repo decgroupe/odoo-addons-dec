e2923d63e94014486e586f51ee65fdefbf882083
Yann Papouin <ypa@decgroupe.com>
2023-05-31 17:05:36 +0200

    [IMP] mail_remove_gmail_signature: black, isort, prettier, readme, icon

--- mail_remove_gmail_signature/__manifest__.py
+++ mail_remove_gmail_signature/__manifest__.py
@@ -1,12 +1,11 @@
 {
-    'name': 'Mail Remove GMail Signature',
-    'version': "14.0.1.0.0",
-    'author': 'DEC, Yann Papouin',
-    'website': 'https://www.decgroupe.com',
-    'summary':
-        "We don't need our company signature as we already "
-        "know each other",
-    'depends': ['mail', ],
-    'data': [],
-    'installable': True
+    "name": "Mail Remove GMail Signature",
+    "version": "14.0.1.0.0",
+    "author": "DEC",
+    "website": "https://www.decgroupe.com",
+    "depends": [
+        "mail",
+    ],
+    "data": [],
+    "installable": True,
 }
--- mail_remove_gmail_signature/models/mail_thread.py
+++ mail_remove_gmail_signature/models/mail_thread.py
@@ -1,32 +1,30 @@
 # Copyright (C) DEC SARL, Inc - All Rights Reserved.
 # Written by Yann Papouin <ypa at decgroupe.com>, Jan 2021
 
-import logging
-import lxml
 
 from email.utils import parseaddr
-from odoo import api, models, _, tools
-from odoo.tools import pycompat
 
-_logger = logging.getLogger(__name__)
+import lxml
+
+from odoo import models, tools
+from odoo.tools import pycompat
 
 
 class MailThread(models.AbstractModel):
-    _inherit = 'mail.thread'
+    _inherit = "mail.thread"
 
     def _message_extract_payload(self, message, save_original=False):
-        body, attachments = super()._message_extract_payload(
-            message, save_original
-        )
+        body, attachments = super()._message_extract_payload(message, save_original)
         if body:
             # Only remove signature for an e-mail coming from our domain
-            email_from = tools.decode_smtp_header(
-                message.get('from'), quoted=True
-            )
+            email_from = tools.decode_smtp_header(message.get("from"), quoted=True)
             email_from = parseaddr(email_from)[1]
-            email_domain = email_from.partition('@')[2]
-            catchall_domain = self.env['ir.config_parameter'].with_user(
-            ).get_param("mail.catchall.domain")
+            email_domain = email_from.partition("@")[2]
+            catchall_domain = (
+                self.env["ir.config_parameter"]
+                .with_user()
+                .get_param("mail.catchall.domain")
+            )
             if email_domain == catchall_domain:
                 body = self._remove_gmail_signatures(body)
         return body, attachments
@@ -39,14 +37,14 @@ class MailThread(models.AbstractModel):
         except ValueError:
             # In case the email client sent XHTML, fromstring will fail because 'Unicode strings
             # with encoding declaration are not supported'.
-            root = lxml.html.fromstring(body.encode('utf-8'))
+            root = lxml.html.fromstring(body.encode("utf-8"))
 
         postprocessed = False
         to_remove = []
         for node in root.iter():
             # TODO: we should probably remove only the first occurence, so
             # a context var could be used to force remove all
-            if 'gmail_signature' in (node.get('data-smartmail') or ''):
+            if "gmail_signature" in (node.get("data-smartmail") or ""):
                 postprocessed = True
                 if node.getparent() is not None:
                     to_remove.append(node)
@@ -54,8 +52,6 @@ class MailThread(models.AbstractModel):
         for node in to_remove:
             node.getparent().remove(node)
         if postprocessed:
-            body = lxml.etree.tostring(
-                root, pretty_print=False, encoding='UTF-8'
-            )
+            body = lxml.etree.tostring(root, pretty_print=False, encoding="UTF-8")
             body = pycompat.to_native(body)
         return body
