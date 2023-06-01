2dc602be822640f1b3c692a08307806f8f0b55cf
Yann Papouin <ypa@decgroupe.com>
2023-06-01 10:02:07 +0200

    [IMP] mrp_activity_my: black, isort, prettier, readme, icon

--- mrp_activity_my/__manifest__.py
+++ mrp_activity_my/__manifest__.py
@@ -1,18 +1,16 @@
 {
-    'name': 'MRP My Activities',
-    'version': "14.0.1.0.0",
-    'author': 'DEC, Yann Papouin',
-    'website': 'https://www.decgroupe.com',
-    'summary':
-        "Backport next activity workflow from Odoo 14.0 but use current"
-        "user",
-    'depends':
-        [
-            'mrp',
-            'mrp_stage',
-            'web',
-            'mail_activity_my',
-        ],
-    'data': ['views/mrp_production.xml', ],
-    'installable': True
+    "name": "MRP My Activities",
+    "version": "14.0.1.0.0",
+    "author": "DEC",
+    "website": "https://www.decgroupe.com",
+    "depends": [
+        "mrp",
+        "mrp_stage",
+        "web",
+        "mail_activity_my",
+    ],
+    "data": [
+        "views/mrp_production.xml",
+    ],
+    "installable": True,
 }
--- mrp_activity_my/models/mrp_production.py
+++ mrp_activity_my/models/mrp_production.py
@@ -1,9 +1,9 @@
 # Copyright (C) DEC SARL, Inc - All Rights Reserved.
 # Written by Yann Papouin <ypa at decgroupe.com>, Feb 2022
 
-from odoo import models, api, fields
+from odoo import models
 
 
 class MrpProduction(models.Model):
-    _name = 'mrp.production'
-    _inherit = ['mrp.production', 'mail.activity.my.mixin']
+    _name = "mrp.production"
+    _inherit = ["mrp.production", "mail.activity.my.mixin"]
