fbf819a3eb07ee58a81849d75d6d0f4d79ce3cfe
Yann Papouin <ypa@decgroupe.com>
2023-06-12 11:33:13 +0200

    [IMP] product_pricelist_analysis: Migration to 14.0

--- product_pricelist_analysis/models/product_product.py
+++ product_pricelist_analysis/models/product_product.py
@@ -12,6 +12,5 @@ class Product(models.Model):
             "product_pricelist_analysis.act_window_product_pricelist_item"
         ).read()[0]
         action["context"] = dict(self.env.context)
-        action["context"]["search_default_product_tmpl_id"] = self.product_tmpl_id.id
         action["context"]["search_default_product_id"] = self.id
         return action
