664812a813aac543a9b1ba65675084b8af35cf9b
Yann Papouin <ypa@decgroupe.com>
2023-06-13 11:31:06 +0200

    [IMP] purchase_pricelist_analysis: Also compute for product's default UoM

--- purchase_pricelist_analysis/models/product_supplierinfo.py
+++ purchase_pricelist_analysis/models/product_supplierinfo.py
@@ -10,28 +10,52 @@ class ProductSupplierinfo(models.Model):
     list_price_graph = fields.Char(
         compute="_compute_list_price",
         string="Computation Graph",
+        help="Mermaid type graph to explain seller price computation (Purchase UoM)",
     )
     list_price_steps = fields.Char(
         compute="_compute_list_price",
         string="Computation Steps",
+        help="Text enumeration to explain seller price computation (Purchase UoM)",
     )
+    list_price_unit_graph = fields.Char(
+        compute="_compute_list_price_unit",
+        string="Computation Graph (Default UoM) ",
+        help="Mermaid type graph to explain seller price computation (Default UoM)",
+    )
+    list_price_unit_steps = fields.Char(
+        compute="_compute_list_price_unit",
+        string="Computation Steps (Default UoM)",
+        help="Text enumeration to explain seller price computation (Default UoM)",
+    )
+
+    def _get_graph_steps(self, history):
+        self.ensure_one()
+        # Product can be a template or a variant
+        product_id = self.product_id or self.product_tmpl_id
+        # Convert quantities to default product UoM
+        qty = self.product_uom._compute_quantity(self.min_qty or 1.0, product_id.uom_id)
+        partner = self.name
+        hkey = (product_id, qty, partner)
+        if hkey in history:
+            graph = history[hkey]["graph"]["header"] + history[hkey]["graph"]["body"]
+            steps = history[hkey]["steps"]
+            return "\n".join(graph), "\n".join(steps)
+        return False, False
 
     def _compute_list_price(self):
         history = {}
-        super(ProductSupplierinfo, self.with_context(history=history))._compute_list_price()
-        self.list_price_graph = False
-        self.list_price_steps = False
+        super(
+            ProductSupplierinfo, self.with_context(history=history)
+        )._compute_list_price()
         for rec in self:
-            # Product can be a template or a variant
-            product_id = rec.product_id or rec.product_tmpl_id
-            # Convert quantities to default product UoM
-            qty = rec.product_uom._compute_quantity(rec.min_qty or 1.0, product_id.uom_id)
-            partner = rec.name
-            hkey = (product_id, qty, partner)
-            if hkey in history:
-                graph = (
-                    history[hkey]["graph"]["header"] + history[hkey]["graph"]["body"]
-                )
-                rec.list_price_graph = "\n".join(graph)
-                steps = history[hkey]["steps"]
-                rec.list_price_steps = "\n".join(steps)
+            rec.list_price_graph, rec.list_price_steps = rec._get_graph_steps(history)
+
+    def _compute_list_price_unit(self):
+        history = {}
+        super(
+            ProductSupplierinfo, self.with_context(history=history)
+        )._compute_list_price_unit()
+        for rec in self:
+            rec.list_price_unit_graph, rec.list_price_unit_steps = rec._get_graph_steps(
+                history
+            )
