942143de90200751d288e72008281f3e9bffe608
Yann Papouin <ypa@decgroupe.com>
2023-06-15 14:58:32 +0200

    [FIX] mrp_change_bom: `_compute_availability` is no more
    Note that this file is a wrong copy/paste from `mrp_produce_empty` and should not exist in this module

--- mrp_change_bom/models/mrp_production.py
+++ mrp_change_bom/models/mrp_production.py
@@ -1,24 +1,8 @@
 # Copyright (C) DEC SARL, Inc - All Rights Reserved.
 # Written by Yann Papouin <ypa at decgroupe.com>, Aug 2020
 
-from datetime import datetime
-
-from odoo import api, fields, models, _
-from odoo.exceptions import UserError
-from odoo.tools import float_compare, float_round
+from odoo import models
 
 
 class MrpProduction(models.Model):
     _inherit = "mrp.production"
-
-    @api.depends(
-        "move_raw_ids.state", "workorder_ids.move_raw_ids", "bom_id.ready_to_produce"
-    )
-    def _compute_availability(self):
-        super()._compute_availability()
-        for order in self:
-            if not order.move_raw_ids:
-                # Override availability from 'none' (min) to 'assigned' (max)
-                # This will allow us to display the 'Produce' button
-                order.availability = "assigned"
-                continue
