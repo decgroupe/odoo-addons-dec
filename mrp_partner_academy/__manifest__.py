609ccc29b612c517a17716d69a89c65a95ebc05d
Yann Papouin <ypa@decgroupe.com>
2023-06-15 15:09:24 +0200

    [IMP] mrp_partner_academy: black, isort, prettier, readme, icon

--- mrp_partner_academy/__manifest__.py
+++ mrp_partner_academy/__manifest__.py
@@ -1,13 +1,14 @@
 {
-    'name': 'Manufacturing Partner Academy',
-    'version': "14.0.1.0.0",
-    'author': 'DEC, Yann Papouin',
-    'website': 'https://www.decgroupe.com',
-    'summary': '''Add partner academy to production orders''',
-    'depends': [
-        'mrp_partner',
-        'partner_academy',
+    "name": "Manufacturing Partner Academy",
+    "version": "14.0.1.0.0",
+    "author": "DEC",
+    "website": "https://www.decgroupe.com",
+    "depends": [
+        "mrp_partner",
+        "partner_academy",
     ],
-    'data': ['views/mrp_production.xml', ],
-    'installable': True
+    "data": [
+        "views/mrp_production.xml",
+    ],
+    "installable": True,
 }
--- mrp_partner_academy/models/mrp_production.py
+++ mrp_partner_academy/models/mrp_production.py
@@ -1,15 +1,15 @@
 # Copyright (C) DEC SARL, Inc - All Rights Reserved.
 # Written by Yann Papouin <ypa at decgroupe.com>, May 2021
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class MrpProduction(models.Model):
     _inherit = "mrp.production"
 
     partner_academy_id = fields.Many2one(
-        comodel_name='res.partner.academy',
-        related='commercial_partner_id.academy_id',
+        comodel_name="res.partner.academy",
+        related="commercial_partner_id.academy_id",
         string="Partner's Academy",
         store=True,
     )
