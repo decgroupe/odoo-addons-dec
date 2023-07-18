926c4776a414339891c9fdeea25bd1ffbf6ce3c5
Yann Papouin <ypa@decgroupe.com>
2023-07-18 11:22:53 +0200

    [IMP] mrp_stock_cancel: black, isort, prettier, readme, icon

--- mrp_stock_cancel/__manifest__.py
+++ mrp_stock_cancel/__manifest__.py
@@ -1,16 +1,15 @@
 {
-    'name': 'Manufacturing Stock Cancel',
-    'version': "14.0.1.0.0",
-    'author': 'DEC, Yann Papouin',
-    'website': 'https://www.decgroupe.com',
-    'summary':
-        "Allow to cancel a stock move from a production order "
-        "when unlocked",
-    'depends': [
-        'mrp',
-        'sale_stock',
-        'stock_actions',
+    "name": "Manufacturing Stock Cancel",
+    "version": "14.0.1.0.0",
+    "author": "DEC",
+    "website": "https://www.decgroupe.com",
+    "depends": [
+        "mrp",
+        "sale_stock",
+        "stock_actions",
     ],
-    'data': ['views/mrp_production.xml', ],
-    'installable': True
+    "data": [
+        "views/mrp_production.xml",
+    ],
+    "installable": True,
 }
---
+++ mrp_stock_cancel/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+Allow to cancel a stock move from a production order when unlocked (locking needs `mrp.group_locked_by_default` group.
