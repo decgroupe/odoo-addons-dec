f48eb797e8e71857b30df6aa8bf960247999e0cf
Yann Papouin <ypa@decgroupe.com>
2023-07-18 11:55:45 +0200

    [IMP] mrp_stock_cancel: Merged with `mrp_stock_operations`
    `action_show_details` is now supported natively

--- mrp_stock_operations/__manifest__.py
+++ mrp_stock_operations/__manifest__.py
@@ -1,14 +1,16 @@
 {
-    'name': 'Manufacturing Stock Operations',
-    'version': "14.0.1.0.0",
-    'author': 'DEC, Yann Papouin',
-    'website': 'https://www.decgroupe.com',
-    'summary': "Allow to open an advance view to edit moves like pickings",
-    'depends': [
-        'mrp',
-        'sale_stock',
-        'stock_actions',
+    "name": "Manufacturing Stock Operations",
+    "version": "14.0.1.0.0",
+    "author": "DEC, Yann Papouin",
+    "website": "https://www.decgroupe.com",
+    "summary": "Allow to open an advance view to edit moves like pickings",
+    "depends": [
+        "mrp",
+        "sale_stock",
+        "stock_actions",
     ],
-    'data': ['views/mrp_production.xml', ],
-    'installable': True
+    "data": [
+        "views/mrp_production.xml",
+    ],
+    "installable": True,
 }
