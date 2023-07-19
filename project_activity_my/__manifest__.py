3408da60a1c4a0bd2ab1e36005a2d0c1692b8c25
Yann Papouin <ypa@decgroupe.com>
2023-07-19 16:32:28 +0200

    [IMP] project_activity_my: black, isort, prettier, readme, icon

--- project_activity_my/__manifest__.py
+++ project_activity_my/__manifest__.py
@@ -1,22 +1,18 @@
 {
-    'name': 'Project My Activities',
-    'version': "14.0.1.0.0",
-    'author': 'DEC, Yann Papouin',
-    'website': 'https://www.decgroupe.com',
-    'summary':
-        "Backport next activity workflow from Odoo 14.0 but use current"
-        "user",
-    'depends':
-        [
-            'project',
-            'project_activity',
-            'project_user',
-            'web',
-            'mail_activity_my',
-        ],
-    'data': [
-        'views/project_task.xml',
-        'views/project_project.xml',
+    "name": "Project My Activities",
+    "version": "14.0.1.0.0",
+    "author": "DEC",
+    "website": "https://www.decgroupe.com",
+    "depends": [
+        "project",
+        "project_activity",
+        "project_user",
+        "web",
+        "mail_activity_my",
     ],
-    'installable': True
+    "data": [
+        "views/project_task.xml",
+        "views/project_project.xml",
+    ],
+    "installable": True,
 }
--- project_activity_my/models/project_project.py
+++ project_activity_my/models/project_project.py
@@ -1,9 +1,9 @@
 # Copyright (C) DEC SARL, Inc - All Rights Reserved.
 # Written by Yann Papouin <ypa at decgroupe.com>, Sep 2022
 
-from odoo import models, api, fields
+from odoo import models
 
 
 class ProjectProject(models.Model):
     _name = "project.project"
-    _inherit = ['project.project', 'mail.activity.my.mixin']
+    _inherit = ["project.project", "mail.activity.my.mixin"]
