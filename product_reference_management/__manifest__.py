177ab3d983807b112fd298c9fe67cfc367f402a1
Yann Papouin <ypa@decgroupe.com>
2023-07-19 15:42:24 +0200

    [IMP] product_reference_management: prettier, readme

--- product_reference_management/__manifest__.py
+++ product_reference_management/__manifest__.py
@@ -1,17 +1,15 @@
 {
-    'name': 'Product Reference Management',
-    'version': "14.0.1.0.0",
-    'author': 'DEC, Yann Papouin',
-    'website': 'https://www.decgroupe.com',
-    'summary': "Root module for reference management (menu/security)",
-    'depends': [
-        'product',
+    "name": "Product Reference Management",
+    "version": "14.0.1.0.0",
+    "author": "DEC",
+    "website": "https://www.decgroupe.com",
+    "depends": [
+        "product",
     ],
-    'data':
-        [
-            'security/model_security.xml',
-            'security/ir.model.access.csv',
-            'views/menu.xml',
-        ],
-    'installable': True
+    "data": [
+        "security/model_security.xml",
+        "security/ir.model.access.csv",
+        "views/menu.xml",
+    ],
+    "installable": True,
 }
---
+++ product_reference_management/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+Root module for reference management (menu/security)
