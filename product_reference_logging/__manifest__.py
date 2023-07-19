ca23bcfddf2eaabeb16e56584d924a4dffa9eea7
Yann Papouin <ypa@decgroupe.com>
2023-07-19 15:39:15 +0200

    [IMP] product_reference_logging: black, isort, prettier, readme, icon

--- product_reference_logging/__manifest__.py
+++ product_reference_logging/__manifest__.py
@@ -1,18 +1,16 @@
 {
-    'name': 'Product Reference Logging',
-    'version': "14.0.1.0.0",
-    'author': 'DEC, Yann Papouin',
-    'website': 'https://www.decgroupe.com',
-    'summary': '''Logging for REFManager user actions''',
-    'depends': [
-        'product_reference_management',
+    "name": "Product Reference Logging",
+    "version": "14.0.1.0.0",
+    "author": "DEC",
+    "website": "https://www.decgroupe.com",
+    "depends": [
+        "product_reference_management",
     ],
-    'data':
-        [
-            'security/model_security.xml',
-            'security/ir.model.access.csv',
-            'views/ref_log.xml',
-            'views/menu.xml',
-        ],
-    'installable': True
+    "data": [
+        "security/model_security.xml",
+        "security/ir.model.access.csv",
+        "views/ref_log.xml",
+        "views/menu.xml",
+    ],
+    "installable": True,
 }
--- product_reference_logging/models/ref_log.py
+++ product_reference_logging/models/ref_log.py
@@ -5,16 +5,34 @@ from odoo import fields, models
 
 
 class RefLog(models.Model):
-    """ Reference log for all operations """
+    """Reference log for all operations"""
 
-    _name = 'ref.log'
-    _description = 'Log'
-    _rec_name = 'operation'
-    _order = 'id desc'
+    _name = "ref.log"
+    _description = "Log"
+    _rec_name = "operation"
+    _order = "id desc"
 
-    operation = fields.Text('Operation', required=True)
-    username = fields.Text('User', required=True)
-    localcomputername = fields.Text('Computer', required=True)
-    localusername = fields.Text('Local Username', required=True)
-    ipaddress = fields.Text('IP Address', required=True)
-    datetime = fields.Datetime('Modification date', default=fields.Datetime.now)
+    operation = fields.Text(
+        string="Operation",
+        required=True,
+    )
+    username = fields.Text(
+        string="User",
+        required=True,
+    )
+    localcomputername = fields.Text(
+        string="Computer",
+        required=True,
+    )
+    localusername = fields.Text(
+        string="Local Username",
+        required=True,
+    )
+    ipaddress = fields.Text(
+        string="IP Address",
+        required=True,
+    )
+    datetime = fields.Datetime(
+        string="Modification date",
+        default=fields.Datetime.now,
+    )
