a662d0b18f50e9177e2a90073efe1cb3b8d125e6
Yann Papouin <ypa@decgroupe.com>
2023-07-21 14:03:23 +0200

    [FIX] stock_assign: Module is empty, should be removed

--- stock_assign/__manifest__.py
+++ stock_assign/__manifest__.py
@@ -1,11 +1,10 @@
 {
-    'name': 'Stock Assign',
-    'version': "14.0.1.0.0",
-    'author': 'DEC, Yann Papouin',
-    'website': 'https://www.decgroupe.com',
-    'summary': "Give back control of stock moves states",
-    'depends': ['stock'],
-    #'force_migration':'12.0.0.0.0',
-    'data': [],
-    'installable': True
+    "name": "Stock Assign",
+    "version": "14.0.1.0.0",
+    "author": "DEC",
+    "website": "https://www.decgroupe.com",
+    "summary": "Give back control of stock moves states",
+    "depends": ["stock"],
+    "data": [],
+    "installable": True,
 }
