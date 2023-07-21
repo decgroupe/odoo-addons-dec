93e2c435f3b4ad3bfb44a4a9834d374bafca7f6a
Yann Papouin <ypa@decgroupe.com>
2023-07-21 11:27:54 +0200

    [IMP] software: black, isort, prettier, readme, icon

--- software/__manifest__.py
+++ software/__manifest__.py
@@ -1,16 +1,14 @@
 {
-    'name': 'Software',
-    'version': "14.0.1.0.0",
-    'author': 'DEC, Yann Papouin',
-    'website': 'https://www.decgroupe.com',
-    'summary': '''Software base''',
-    'depends': [
-        'base',
+    "name": "Software",
+    "version": "14.0.1.0.0",
+    "author": "DEC",
+    "website": "https://www.decgroupe.com",
+    "depends": [
+        "base",
     ],
-    'data':
-        [
-            'security/model_security.xml',
-            'views/menu.xml',
-        ],
-    'installable': True
+    "data": [
+        "security/model_security.xml",
+        "views/menu.xml",
+    ],
+    "installable": True,
 }
