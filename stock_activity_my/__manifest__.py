c901246bfe36b7bb97bbbab0b5aef0f65575be91
Yann Papouin <ypa@decgroupe.com>
2023-07-21 14:00:36 +0200

    [IMP] stock_activity_my: black, isort, prettier, readme, icon

--- stock_activity_my/__manifest__.py
+++ stock_activity_my/__manifest__.py
@@ -1,17 +1,15 @@
 {
-    'name': 'Stock My Activities',
-    'version': "14.0.1.0.0",
-    'author': 'DEC, Yann Papouin',
-    'website': 'https://www.decgroupe.com',
-    'summary':
-        "Backport next activity workflow from Odoo 14.0 but use current"
-        "user",
-    'depends':
-        [
-            'stock',
-            'web',
-            'mail_activity_my',
-        ],
-    'data': ['views/stock_picking.xml', ],
-    'installable': True
+    "name": "Stock My Activities",
+    "version": "14.0.1.0.0",
+    "author": "DEC",
+    "website": "https://www.decgroupe.com",
+    "depends": [
+        "stock",
+        "web",
+        "mail_activity_my",
+    ],
+    "data": [
+        "views/stock_picking.xml",
+    ],
+    "installable": True,
 }
--- stock_activity_my/models/stock_picking.py
+++ stock_activity_my/models/stock_picking.py
@@ -1,9 +1,9 @@
 # Copyright (C) DEC SARL, Inc - All Rights Reserved.
 # Written by Yann Papouin <ypa at decgroupe.com>, Feb 2022
 
-from odoo import models, api, fields
+from odoo import models
 
 
 class StockPicking(models.Model):
-    _name = 'stock.picking'
-    _inherit = ['stock.picking', 'mail.activity.my.mixin']
+    _name = "stock.picking"
+    _inherit = ["stock.picking", "mail.activity.my.mixin"]
