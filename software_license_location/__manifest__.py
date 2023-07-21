4c89fdd12fa8a0178bab124f6a03a822ae8ec744
Yann Papouin <ypa@decgroupe.com>
2023-07-21 13:43:09 +0200

    [IMP] software_license_location: black, isort, prettier, readme, icon

--- software_license_location/__manifest__.py
+++ software_license_location/__manifest__.py
@@ -1,13 +1,14 @@
 {
-    'name': 'Software License Location',
-    'version': "14.0.1.0.0",
-    'author': 'DEC, Yann Papouin',
-    'website': 'https://www.decgroupe.com',
-    'summary': "Add shipping partner location to licenses",
-    'depends': [
-        'base_location',
-        'software_license',
+    "name": "Software License Location",
+    "version": "14.0.1.0.0",
+    "author": "DEC",
+    "website": "https://www.decgroupe.com",
+    "depends": [
+        "base_location",
+        "software_license",
     ],
-    'data': ['views/software_license.xml', ],
-    'installable': True
+    "data": [
+        "views/software_license.xml",
+    ],
+    "installable": True,
 }
