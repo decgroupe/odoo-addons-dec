d29207ea00229ea7522496e8ba1bc096e0f6d6c4
Yann Papouin <ypa@decgroupe.com>
2023-07-21 13:44:34 +0200

    [IMP] software_license_pass_location: black, isort, prettier, readme, icon

--- software_license_pass_location/__manifest__.py
+++ software_license_pass_location/__manifest__.py
@@ -1,13 +1,14 @@
 {
-    'name': 'Software License (pass) Location',
-    'version': "14.0.1.0.0",
-    'author': 'DEC, Yann Papouin',
-    'website': 'https://www.decgroupe.com',
-    'summary': "Add shipping partner location to passes",
-    'depends': [
-        'base_location',
-        'software_license_pass',
+    "name": "Software License (pass) Location",
+    "version": "14.0.1.0.0",
+    "author": "DEC",
+    "website": "https://www.decgroupe.com",
+    "depends": [
+        "base_location",
+        "software_license_pass",
     ],
-    'data': ['views/software_license_pass.xml', ],
-    'installable': True
+    "data": [
+        "views/software_license_pass.xml",
+    ],
+    "installable": True,
 }
--- software_license_pass_location/models/software_license_pass.py
+++ software_license_pass_location/models/software_license_pass.py
@@ -5,17 +5,17 @@ from odoo import fields, models
 
 
 class SoftwareLicensePass(models.Model):
-    _inherit = 'software.license.pass'
+    _inherit = "software.license.pass"
 
     partner_zip_id = fields.Many2one(
-        'res.city.zip',
-        related='partner_id.zip_id',
+        comodel_name="res.city.zip",
+        related="partner_id.zip_id",
         string="Partner's ZIP",
         store=True,
     )
     partner_city_id = fields.Many2one(
-        'res.city',
-        related='partner_id.city_id',
+        comodel_name="res.city",
+        related="partner_id.city_id",
         string="Partner's City",
         store=True,
     )
