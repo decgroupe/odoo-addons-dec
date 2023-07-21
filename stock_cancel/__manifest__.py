fe766fe8b2669f80f453cdabc0580291dd5873cc
Yann Papouin <ypa@decgroupe.com>
2023-07-21 14:10:17 +0200

    [FIX] stock_cancel: Module is empty, should be removed

--- stock_cancel/__manifest__.py
+++ stock_cancel/__manifest__.py
@@ -1,11 +1,10 @@
 {
-    'name': 'Stock Cancel',
-    'version': "14.0.1.0.0",
-    'author': 'DEC, Yann Papouin',
-    'website': 'https://www.decgroupe.com',
-    'summary': '''Action to cancel stock moves recursively''',
-    'depends': ['stock'],
-    #'force_migration':'12.0.0.0.0',
-    'data': [],
-    'installable': True
+    "name": "Stock Cancel",
+    "version": "14.0.1.0.0",
+    "author": "DEC",
+    "website": "https://www.decgroupe.com",
+    "summary": """Action to cancel stock moves recursively""",
+    "depends": ["stock"],
+    "data": [],
+    "installable": True,
 }
