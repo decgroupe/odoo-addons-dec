c13878aaaab5ec06ecc61bb22f8f109a1c308c5d
Yann Papouin <ypa@decgroupe.com>
2023-07-26 14:12:08 +0200

    [IMP] mrp_note: Use builtin tracking

--- mrp_note/models/mrp_production.py
+++ mrp_note/models/mrp_production.py
@@ -7,12 +7,7 @@ from odoo import _, fields, models
 class MrpProduction(models.Model):
     _inherit = "mrp.production"
 
-    note = fields.Text("Internal Notes")
-
-    def write(self, vals):
-        if vals.get("note"):
-            self.message_post(
-                body=_("Internal notes changed to: {}").format(vals.get("note"))
-            )
-        res = super().write(vals)
-        return res
+    note = fields.Text(
+        string="Internal Notes",
+        tracking=True,
+    )
