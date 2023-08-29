13a32054326667efb17f9b387b82e34efcc1a07b
Yann Papouin <ypa@decgroupe.com>
2023-08-29 11:58:43 +0200

    [FIX] crm_lead_partner_location: Ensure variable is defined

--- crm_lead_partner_location/models/res_partner.py
+++ crm_lead_partner_location/models/res_partner.py
@@ -6,15 +6,16 @@ from odoo import api, models
 
 
 class ResPartner(models.Model):
-    _inherit = 'res.partner'
+    _inherit = "res.partner"
 
     @api.multi
     def address_get(self, adr_pref=None):
         res = super().address_get(adr_pref)
-        if "delivery" in adr_pref and "default_opportunity_id" in self.env.context:
-            opportunity_id = self.env["crm.lead"].browse(
-                self.env.context.get("default_opportunity_id", False)
-            )
-            if opportunity_id and opportunity_id.partner_shipping_id:
-                res['delivery'] = opportunity_id.partner_shipping_id.id
+        if adr_pref and "delivery" in adr_pref:
+            if "default_opportunity_id" in self.env.context:
+                opportunity_id = self.env["crm.lead"].browse(
+                    self.env.context.get("default_opportunity_id", False)
+                )
+                if opportunity_id and opportunity_id.partner_shipping_id:
+                    res["delivery"] = opportunity_id.partner_shipping_id.id
         return res
