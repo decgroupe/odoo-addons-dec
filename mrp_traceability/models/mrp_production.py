139ab4ebf69019e6407ac3ea99b85241ca0edd46
Yann Papouin <ypa@decgroupe.com>
2023-08-30 14:28:30 +0200

    [FIX] mrp_traceability: Link move destination to production moves to-do
    This hook is now processed in the create/write methods instead of `_get_move_raw_values` because this method is called too early (lot of NewId)

--- mrp_traceability/models/mrp_production.py
+++ mrp_traceability/models/mrp_production.py
@@ -1,17 +1,38 @@
 # Copyright (C) DEC SARL, Inc - All Rights Reserved.
 # Written by Yann Papouin <ypa at decgroupe.com>, Apr 2020
 
-from odoo import models
+from odoo import api, models
 
 
 class MrpProduction(models.Model):
     _inherit = "mrp.production"
 
-    def _get_raw_move_data(self, bom_line, line_data):
-        res = super(MrpProduction, self)._get_raw_move_data(bom_line, line_data)
-        if res is not None:
-            # Link move destination to production moves to-do, this hook is
-            # only done to keep REF Manager stock.move compatibility with
-            # legacy way (from OpenERP)
-            res["move_dest_ids"] = [(6, 0, self.move_finished_ids.ids)]
+    @api.model
+    def create(self, vals):
+        rec = super().create(vals)
+        # Update finished moves or they will be named « New »
+        if vals.get("move_finished_ids"):
+            rec.move_finished_ids.write({"name": rec.name})
+        # Update raw moves or they will be named « New »
+        if vals.get("move_raw_ids"):
+            rec.move_raw_ids.write({"name": rec.name})
+        rec._update_raw_move_dest_ids(vals)
+        return rec
+
+    def write(self, vals):
+        res = super().write(vals)
+        self._update_raw_move_dest_ids(vals)
         return res
+
+    def _update_raw_move_dest_ids(self, vals=None):
+        """Link move destination to production moves to-do, this hook is
+        only done to keep REF Manager stock.move compatibility with
+        legacy way (from OpenERP)
+        TODO: Use alternative fields (move_conv_dest_ids) to store this information
+        """
+        self.ensure_one()
+        if not vals or vals.get("move_finished_ids") or vals.get("move_raw_ids"):
+            if self.move_finished_ids.ids and self.move_raw_ids.ids:
+                self.move_raw_ids.write(
+                    {"move_dest_ids": [(6, 0, self.move_finished_ids.ids)]}
+                )
