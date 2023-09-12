bc0a89656dc89db42e0d34fd0a7ae1854288fd50
Yann Papouin <ypa@decgroupe.com>
2023-09-12 09:29:34 +0200

    [IMP] mail_activity_project: black, isort, prettier, readme, icon

--- mail_activity_project/__manifest__.py
+++ mail_activity_project/__manifest__.py
@@ -1,16 +1,17 @@
 {
-    'name': 'Mail Activity Project',
-    'summary': "Add Project to Activities",
-    'version': '12.0.1.0.0',
-    'author': 'DEC, Yann Papouin',
-    'category': 'Social Network',
-    'website': 'https://github.com/OCA/social',
-    'license': 'AGPL-3',
-    'depends': [
-        'project',
-        'mail_activity_board',
+    "name": "Mail Activity Project",
+    "version": "12.0.1.0.0",
+    "author": "DEC",
+    "category": "Social Network",
+    "website": "https://github.com/OCA/social",
+    "license": "AGPL-3",
+    "depends": [
+        "project",
+        "mail_activity_board",
     ],
-    "data": ['views/mail_activity.xml', ],
-    'qweb': [],
-    'installable': True
+    "data": [
+        "views/mail_activity.xml",
+    ],
+    "qweb": [],
+    "installable": True,
 }
--- mail_activity_project/models/mail_activity.py
+++ mail_activity_project/models/mail_activity.py
@@ -9,32 +9,33 @@ _logger = logging.getLogger(__name__)
 
 
 class MailActivity(models.Model):
-    _inherit = 'mail.activity'
+    _inherit = "mail.activity"
 
     project_id = fields.Many2one(
-        comodel_name='project.project',
+        comodel_name="project.project",
         index=True,
-        compute='_compute_project_id',
+        compute="_compute_project_id",
         store=True,
     )
 
-    @api.depends('res_model', 'res_id')
+    @api.depends("res_model", "res_id")
     def _compute_project_id(self):
         for obj in self:
             res_model = obj.res_model
             res_id = obj.res_id
             if not res_model or not res_id:
                 _logger.error(
-                    "Activity %d is missing a model/id "
-                    "(res_model=%s, res_id=%d)", obj.id, res_model, res_id
+                    "Activity %d is missing a model/id " "(res_model=%s, res_id=%d)",
+                    obj.id,
+                    res_model,
+                    res_id,
                 )
                 continue
-            if res_model == 'project.project':
+            if res_model == "project.project":
                 obj.project_id = res_id
             else:
-                res_model_id = obj.env[res_model].search([('id', '=', res_id)])
-                if 'project_id' in res_model_id._fields and \
-                        res_model_id.project_id:
+                res_model_id = obj.env[res_model].search([("id", "=", res_id)])
+                if "project_id" in res_model_id._fields and res_model_id.project_id:
                     obj.project_id = res_model_id.project_id
                 else:
                     obj.project_id = None
