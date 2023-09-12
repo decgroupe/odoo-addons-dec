428953337b40d0dadda0e2035bfc263bb3169ff3
Yann Papouin <ypa@decgroupe.com>
2023-09-12 11:01:44 +0200

    [MIG] mail_activity_schedule_dhxgantt: Migration to 14.0

--- mail_activity_schedule_dhxgantt/__manifest__.py
+++ mail_activity_schedule_dhxgantt/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Mail Activity Schedule (Gantt)",
-    "version": "12.0.1.0.0",
+    "version": "14.0.1.0.0",
     "author": "DEC",
     "category": "Social Network",
     "website": "https://github.com/OCA/social",
