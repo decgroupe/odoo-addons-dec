b1d199083e2f2c165a1850264b31f648a4184ab7
Yann Papouin <ypa@decgroupe.com>
2023-09-12 11:47:52 +0200

    [IMP] mrp_schedule: black, isort, prettier, readme, icon

--- mrp_schedule/__manifest__.py
+++ mrp_schedule/__manifest__.py
@@ -1,10 +1,13 @@
 {
-    'name': 'Manufacturing Schedule',
-    'version': '12.0.1.0.0',
-    'author': 'DEC, Yann Papouin',
-    'website': 'https://www.decgroupe.com',
-    'summary': "Schedule",
-    'depends': ['mail_activity_schedule', ],
-    'data': ['views/mrp_production.xml', ],
-    'installable': True
+    "name": "Manufacturing Schedule",
+    "version": "12.0.1.0.0",
+    "author": "DEC",
+    "website": "https://www.decgroupe.com",
+    "depends": [
+        "mail_activity_schedule",
+    ],
+    "data": [
+        "views/mrp_production.xml",
+    ],
+    "installable": True,
 }
--- mrp_schedule/models/mrp_production.py
+++ mrp_schedule/models/mrp_production.py
@@ -2,20 +2,20 @@
 # Copyright (C) DEC SARL, Inc - All Rights Reserved.
 # Written by Yann Papouin <ypa at decgroupe.com>, May 2022
 
-from odoo import api, fields, models
+from odoo import api, models
 
 
 class MrpProduction(models.Model):
-    _inherit = ['mrp.production', 'mail.activity.schedule.mixin']
-    _name = 'mrp.production'
+    _inherit = ["mrp.production", "mail.activity.schedule.mixin"]
+    _name = "mrp.production"
 
     def _get_schedule_date_fields(self):
         res = super()._get_schedule_date_fields()
         res.update(
             {
-                'start': 'date_planned_start',
-                'stop': 'date_planned_finished',
-                'deadline': 'date_planned_finished',
+                "start": "date_planned_start",
+                "stop": "date_planned_finished",
+                "deadline": "date_planned_finished",
             }
         )
         return res
@@ -28,6 +28,6 @@ class MrpProduction(models.Model):
     @api.multi
     def _is_schedulable(self):
         res = super()._is_schedulable()
-        if res and self.state in ['done', 'cancel']:
+        if res and self.state in ["done", "cancel"]:
             res = False
         return res
