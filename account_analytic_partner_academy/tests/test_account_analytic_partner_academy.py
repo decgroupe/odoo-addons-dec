a3ce6a295148871cf51112bcf08aae449288813b
Yann Papouin <ypa@decgroupe.com>
2023-09-27 11:45:40 +0200

    [IMP] account_analytic_partner_academy: Tests added

---
+++ account_analytic_partner_academy/tests/test_account_analytic_partner_academy.py
@@ -0,0 +1,10 @@
+# Copyright (C) DEC SARL, Inc - All Rights Reserved.
+# Written by Yann Papouin <ypa at decgroupe.com>, Sep 2023
+
+
+from odoo.tests import common
+
+
+class TestAccountAnalyticPartnerAcademy(common.TransactionCase):
+    def setUp(self):
+        super().setUp()
