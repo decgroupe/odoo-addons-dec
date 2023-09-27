1b74027571e589c64159b36ae4e6f32fd91380ce
Yann Papouin <ypa@decgroupe.com>
2023-09-27 16:03:23 +0200

    [IMP] product_reference_management: Tests added

---
+++ product_reference_management/tests/test_product_reference_management.py
@@ -0,0 +1,12 @@
+# Copyright (C) DEC SARL, Inc - All Rights Reserved.
+# Written by Yann Papouin <ypa at decgroupe.com>, Sep 2023
+
+from odoo.tests.common import TransactionCase
+
+
+class TestProductReferenceManagement(TransactionCase):
+    def setUp(self):
+        super().setUp()
+
+    # def test_01_(self):
+    #     pass
