e6a30c04be4df23d408af9ea0556272185f17f1a
Yann Papouin <ypa@decgroupe.com>
2023-09-27 11:19:29 +0200

    [IMP] partner_academy: Data added

--- partner_academy/__manifest__.py
+++ partner_academy/__manifest__.py
@@ -7,12 +7,16 @@
         "base",
         "contacts",
         "l10n_fr_department",
+        "l10n_fr_department_oversea",
+        "partner_fax",
     ],
     "data": [
         "security/model_security.xml",
         "security/ir.model.access.csv",
         "views/res_partner.xml",
         "views/res_partner_academy.xml",
+        "data/res_partner.xml",
+        "data/res_partner_academy.xml",
     ],
     "installable": True,
 }
