9dba02777f59b87cb9c357d866d9480e68feb7f5
Yann Papouin <ypa@decgroupe.com>
2023-11-07 14:24:55 +0100

    [FIX] helpdesk_merge: Allow base helpdesk user to merge tickets

--- helpdesk_merge/tests/test_helpdesk_merge.py
+++ helpdesk_merge/tests/test_helpdesk_merge.py
@@ -1,6 +1,8 @@
 # Copyright (C) DEC SARL, Inc - All Rights Reserved.
 # Written by Yann Papouin <ypa at decgroupe.com>, May 2023
 
+from odoo.exceptions import AccessDenied
+from odoo.tests import new_test_user
 from odoo.tests.common import TransactionCase
 
 
@@ -12,6 +14,24 @@ class TestHelpdeskMerge(TransactionCase):
         self.ticket_model = self.env["helpdesk.ticket"]
         self.merge_ticket_wizard_model = self.env["merge.helpdesk.ticket.wizard"]
         self.group_do_merge = self.env.ref("helpdesk_merge.res_group_do_merge")
+        ctx = {
+            "mail_create_nolog": True,
+            "mail_create_nosubscribe": True,
+            "mail_notrack": True,
+            "no_reset_password": True,
+        }
+        self.user = new_test_user(
+            self.env,
+            login="helpdesk_merge-user",
+            groups="base.group_user,helpdesk_mgmt.group_helpdesk_user",
+            context=ctx,
+        )
+        self.user = new_test_user(
+            self.env,
+            login="helpdesk_merge-user",
+            groups="base.group_user,helpdesk_mgmt.group_helpdesk_user",
+            context=ctx,
+        )
 
     def test_01_merge(self):
         # get a first ticket reference
@@ -54,3 +74,26 @@ class TestHelpdeskMerge(TransactionCase):
             set(ticket_ht4_new_data["message_ids"]),
             set(ticket_ht4_data["message_ids"] + ticket_ht7_data["message_ids"]),
         )
+
+    def test_02_merge_helpdesk_user(self):
+        # get a first ticket reference
+        ticket_ht4 = self.ticket_model.search([("number", "=", "HT00004")])
+        # get another ticket reference that will be merged in to the first one
+        ticket_ht7 = self.ticket_model.search([("number", "=", "HT00007")])
+        # run merge
+        wizard_id = self.merge_ticket_wizard_model.with_user(self.user).create(
+            {
+                "object_ids": (ticket_ht4 + ticket_ht7).ids,
+                "dst_object_id": ticket_ht4.id,
+            }
+        )
+        # first merge try
+        with self.assertRaisesRegex(
+            AccessDenied, "You don't have the right to merge tickets"
+        ):
+            wizard_id.action_merge()
+        # add needed group
+        self.user.groups_id += self.env.ref("helpdesk_merge.res_group_do_merge")
+        wizard_id.action_merge()
+        self.assertTrue(ticket_ht4.exists())
+        self.assertFalse(ticket_ht7.exists())
--- helpdesk_merge/wizard/merge_helpdesk_ticket.py
+++ helpdesk_merge/wizard/merge_helpdesk_ticket.py
@@ -1,7 +1,8 @@
 # Copyright (C) DEC SARL, Inc - All Rights Reserved.
 # Written by Yann Papouin <ypa at decgroupe.com>, Jun 2022
 
-from odoo import fields, models
+from odoo import _, fields, models
+from odoo.exceptions import AccessDenied
 
 
 class MergeHelpdeskTicket(models.TransientModel):
@@ -28,8 +29,18 @@ class MergeHelpdeskTicket(models.TransientModel):
     )
 
     def _merge(self, object_ids, dst_object=None, extra_checks=True):
+        if not self.env.user.has_group("helpdesk_merge.res_group_do_merge"):
+            raise AccessDenied(
+                _(
+                    "You don't have the right to merge tickets. "
+                    "Please contact an Administrator."
+                )
+            )
         return super()._merge(
             object_ids,
             dst_object.with_context(mail_auto_subscribe_no_notify=True),
             extra_checks,
         )
+
+    def _delete_source_objects(self, src_objects):
+        return super()._delete_source_objects(src_objects.sudo())
