8b597331d8b9e1e9a824fa312777fa409cd8ae1d
Yann Papouin <ypa@decgroupe.com>
2023-11-30 11:59:02 +0100

    [FIX] mrp_stock_cancel: Prepare module to be renamed

--- mrp_stock_cancel/__manifest__.py
+++ mrp_stock_cancel/__manifest__.py
@@ -1,5 +1,5 @@
 {
-    "name": "Manufacturing Stock Cancel",
+    "name": "Manufacturing Stock Actions",
     "version": "14.0.1.0.0",
     "author": "DEC",
     "website": "https://www.decgroupe.com",
@@ -12,4 +12,5 @@
         "views/mrp_production.xml",
     ],
     "installable": True,
+    "pre_init_hook": "rename_module",
 }
