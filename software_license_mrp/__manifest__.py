90c3969273525b12be5684c2460c99379d69c954
Yann Papouin <ypa@decgroupe.com>
2024-02-19 15:52:57 +0100

    [FIX] software_license_mrp: Missing author

--- software_license_mrp/__manifest__.py
+++ software_license_mrp/__manifest__.py
@@ -1,6 +1,7 @@
 {
     "name": "Software License (MRP)",
     "version": "14.0.1.0.0",
+    "author": "DEC",
     "website": "https://www.decgroupe.com",
     "depends": [
         "software_license",
