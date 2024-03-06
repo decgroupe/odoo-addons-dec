d6fe3da4724cbe04f97ed9f9c3f885c9ce779752
Yann Papouin <ypa@decgroupe.com>
2024-03-06 17:04:53 +0100

    [FIX] mail_composer_dummy: Better implementation with logging

--- mail_composer_dummy/wizard/mail_compose_message.py
+++ mail_composer_dummy/wizard/mail_compose_message.py
@@ -1,8 +1,12 @@
 # Copyright (C) DEC SARL, Inc - All Rights Reserved.
 # Written by Yann Papouin <ypa at decgroupe.com>, Aug 2023
 
+import logging
 
-from odoo import api, fields, models
+from odoo import fields, models
+
+
+_logger = logging.getLogger(__name__)
 
 
 class MailComposer(models.TransientModel):
@@ -19,12 +23,23 @@ class MailComposer(models.TransientModel):
 
     def get_mail_values(self, res_ids):
         results = super(MailComposer, self).get_mail_values(res_ids)
-        if self.email_to:
-            for res_id, mail_values in results.items():
-                if not mail_values.get("email_to"):
-                    mail_values["email_to"] = self.email_to
-                if not mail_values.get("email_cc"):
-                    mail_values["email_cc"] = self.email_to
+        for res_id, mail_values in results.items():
+            if self.email_to:
+                if "email_to" in mail_values:
+                    _logger.info(
+                        "Overriding 'email_to' from %s to %s",
+                        mail_values["email_to"],
+                        self.email_to,
+                    )
+                mail_values["email_to"] = self.email_to
+            if self.email_cc:
+                if "email_cc" in mail_values:
+                    _logger.info(
+                        "Overriding 'email_cc' from %s to %s",
+                        mail_values["email_cc"],
+                        self.email_cc,
+                    )
+                mail_values["email_cc"] = self.email_cc
         return results
 
     def send_mail(self, auto_commit=False):
