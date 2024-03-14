0cd7281d323244f796e65f5c3ba34330e29f293d
Yann Papouin <ypa@decgroupe.com>
2024-03-14 14:37:56 +0100

    [IMP] mail_delayed_deletion: Rename incoming e-mail identifier instead of deleting original message

--- mail_delayed_deletion/models/mail_thread.py
+++ mail_delayed_deletion/models/mail_thread.py
@@ -13,63 +13,52 @@ from odoo import models
 
 _logger = logging.getLogger(__name__)
 
+ODOO_MSGID = "-openerp"
+LOOP_MSGID = "-loop" + ODOO_MSGID
+
 
 class MailThread(models.AbstractModel):
     _inherit = "mail.thread"
 
-    def _message_process_delete_duplicated_messages(
-        self,
-        model,
-        message,
-        custom_values=None,
-        save_original=False,
-        thread_id=None,
-    ):
-        """Deleting mails, scheduled to be deleted, with duplicated
-        Message-Id before processing, otherwise they will be ignored.
+    def _message_process_duplicated_messages(self, message):
+        """Rename incoming e-mail sent by our system in order to process their route
+        correctly (duplicated Message-Id are ignored by Odoo while processing).
         It is necessary when a mail is sent to a channel from odoo.
+        A security is added to avoid processing a duplicate more than once.
         """
         # Use the same logic from odoo/addons/mail/models/mail_thread.py
         if isinstance(message, xmlrpclib.Binary):
             message = bytes(message.data)
         if isinstance(message, str):
             message = message.encode("utf-8")
-        msg_txt = email.message_from_bytes(message, policy=email.policy.SMTP)
-
-        msg = self.message_parse(msg_txt, save_original=save_original)
-        # Checks if Message-Id is already in the database but tagged to be
-        # deleted later (using `delayed_deletion`)
-        if msg.get("message_id"):
-            existing_mail_ids = self.env["mail.mail"].search(
-                [
-                    ("message_id", "=", msg.get("message_id")),
-                    ("delayed_deletion", "!=", False),
-                ]
+        email_message = email.message_from_bytes(message, policy=email.policy.SMTP)
+        message_id = email_message["message-id"]
+        if message_id and ODOO_MSGID in message_id:
+            # checks if Message-Id is already in the database
+            existing_message_ids = self.env["mail.message"].search(
+                [("message_id", "=", message_id)],
             )
-            if existing_mail_ids:
-                try:
-                    # Like the super method, find possible routes for the
-                    # message. If a ValueError exception is raised then no
-                    # route exists and the message will not be re-created, in
-                    # that case we don't delete it
-                    routes = self.message_route(
-                        msg_txt, msg, model, thread_id, custom_values
+            if existing_message_ids:
+                if LOOP_MSGID in message_id:
+                    _logger.info(
+                        "🧽 Duplicated incoming e-mail with Message-Id %s "
+                        "already processed.",
+                        message_id,
+                    )
+                else:
+                    del email_message["message-id"]
+                    email_message["message-id"] = message_id.replace(
+                        ODOO_MSGID, LOOP_MSGID
                     )
                     _logger.info(
-                        "Deleting mail, scheduled to be deleted, with "
-                        "duplicated Message-Id %s before processing.",
-                        msg.get("message_id"),
+                        "🧽 Duplicated incoming e-mail with Message-Id %s "
+                        "renamed to Message-Id %s",
+                        message_id,
+                        email_message["message-id"],
                     )
-                    # Instead of deleting mails, we delete messages (ignoring
-                    # notification value of mail object) that will also delete mails
-                    # via cascade
-                    existing_msg_ids = existing_mail_ids.mapped("mail_message_id")
-                    existing_msg_ids.unlink()
-                except ValueError:
-                    _logger.info(
-                        "No route found, keep existing mail with " "Message-Id %s",
-                        msg.get("message_id"),
-                    )
+                    # reconvert EmailMessage object to bytes (original format)
+                    message = bytes(email_message)
+        return message
 
     def message_process(
         self,
@@ -80,9 +69,7 @@ class MailThread(models.AbstractModel):
         strip_attachments=False,
         thread_id=None,
     ):
-        # self._message_process_delete_duplicated_messages(
-        #     model, message, custom_values, save_original, thread_id
-        # )
+        message = self._message_process_duplicated_messages(message)
         thread_id = super().message_process(
             model=model,
             message=message,
