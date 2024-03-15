77272f67f0b28a7ff4da3593a866acfe375533b8
Yann Papouin <ypa@decgroupe.com>
2024-03-15 10:37:12 +0100

    [FIX] mail_channel_prefix: Always add prefix

--- mail_channel_prefix/models/mail_channel.py
+++ mail_channel_prefix/models/mail_channel.py
@@ -12,9 +12,12 @@ class MailChannel(models.AbstractModel):
 
     @api.returns("mail.message", lambda value: value.id)
     def message_post(self, message_type="notification", **kwargs):
-        if self.subject_prefix and "subject" in kwargs:
-            kwargs["subject"] = "%s %s" % (
-                self.subject_prefix,
-                kwargs["subject"],
-            )
+        if self.subject_prefix:
+            if "subject" in kwargs:
+                kwargs["subject"] = "%s %s" % (
+                    self.subject_prefix,
+                    kwargs["subject"],
+                )
+            else:
+                kwargs["subject"] = "%s" % (self.subject_prefix)
         return super().message_post(message_type=message_type, **kwargs)
