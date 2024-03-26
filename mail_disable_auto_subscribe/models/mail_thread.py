ebbd3df9ac98399bc8b83a88274f023173af0bc7
Yann Papouin <ypa@decgroupe.com>
2024-03-26 09:58:42 +0100

    [IMP] mail_disable_auto_subscribe: Hook removed
    Hooking `_notify_classify_recipients` is no more necessary with Odoo 14.0

--- mail_disable_auto_subscribe/models/mail_thread.py
+++ mail_disable_auto_subscribe/models/mail_thread.py
@@ -56,24 +56,3 @@ class MailThread(models.AbstractModel):
                 self = self.with_context(mail_create_nosubscribe=True)
 
         return super(MailThread, self)._message_post_after_hook(message, msg_vals)
-
-    # TODO: [MIG] 14.0: Check if this hook is still necessary
-    def _notify_classify_recipients(self, recipient_data, model_name, msg_vals=None):
-        """This hook is made to convert internal user/partner recipient to
-        type `user` because when odoo convert channel to partners in
-        `_notify_recipients`, they are all set with default type
-        `customer`.
-        That way, the « See » Action will be available.
-
-        """
-        is_internal = lambda p: (
-            p.user_ids
-            and all(p.user_ids.mapped(lambda u: u.has_group("base.group_user")))
-        )
-        for recipient in recipient_data:
-            partner_id = self.env["res.partner"].browse(recipient["id"])
-            if is_internal(partner_id):
-                recipient["type"] = "user"
-        return super()._notify_classify_recipients(
-            recipient_data=recipient_data, model_name=model_name, msg_vals=msg_vals
-        )
