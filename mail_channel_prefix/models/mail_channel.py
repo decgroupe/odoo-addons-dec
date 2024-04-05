30d2d6aff22b7aaa13a2b9defc5877d8db1afb01
Yann Papouin <ypa@decgroupe.com>
2024-04-05 15:51:17 +0200

    [IMP] mail_channel_prefix: Be more consistent when setting/removing a subject prefix

--- mail_channel_prefix/models/mail_channel.py
+++ mail_channel_prefix/models/mail_channel.py
@@ -8,16 +8,27 @@ from odoo import api, fields, models
 class MailChannel(models.AbstractModel):
     _inherit = "mail.channel"
 
-    subject_prefix = fields.Char(string="Subject's Prefix")
+    subject_prefix = fields.Char(string="Subject's Prefix", help="【MAIL】")
+
+    def _remove_subject_client_prefix(self, subject):
+        for prefix in ["Re", "RE", "Fw", "FW", "Fwd", "FWD", ":"]:
+            if subject.startswith(prefix):
+                subject = subject.replace(prefix, "", 1).strip()
+        return subject
+
+    def _add_subject_channel_prefix(self, subject):
+        if self.subject_prefix:
+            if subject:
+                if not subject.startswith(self.subject_prefix):
+                    subject = "%s %s" % (self.subject_prefix, subject)
+            else:
+                subject = "%s" % (self.subject_prefix)
+        return subject
 
     @api.returns("mail.message", lambda value: value.id)
     def message_post(self, message_type="notification", **kwargs):
-        if self.subject_prefix:
-            if "subject" in kwargs:
-                kwargs["subject"] = "%s %s" % (
-                    self.subject_prefix,
-                    kwargs["subject"],
-                )
-            else:
-                kwargs["subject"] = "%s" % (self.subject_prefix)
+        subject = kwargs.get("subject")
+        if subject:
+            subject = self._remove_subject_client_prefix(subject)
+        kwargs["subject"] = self._add_subject_channel_prefix(subject)
         return super().message_post(message_type=message_type, **kwargs)
