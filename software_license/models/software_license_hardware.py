fb177e1e2cc002fd999c0b19c031e3094ae40c2b
Yann Papouin <ypa@decgroupe.com>
2024-04-10 15:04:19 +0200

    [IMP] software_license: Device name is now extracted from info data

--- software_license/models/software_license_hardware.py
+++ software_license/models/software_license_hardware.py
@@ -1,7 +1,9 @@
 # Copyright (C) DEC SARL, Inc - All Rights Reserved.
 # Written by Yann Papouin <ypa at decgroupe.com>, Mar 2021
 
-from odoo import fields, models
+import json
+
+from odoo import api, fields, models
 
 
 class SoftwareLicenseHardware(models.Model):
@@ -22,6 +24,22 @@ class SoftwareLicenseHardware(models.Model):
     info = fields.Text(
         string="Informations",
     )
+    device_name = fields.Char(
+        string="Device Name",
+        compute="_compute_device_info",
+        store=True,
+    )
+    device_domain = fields.Char(
+        string="Device Domain",
+        compute="_compute_device_info",
+        store=True,
+    )
+    device_fqdn = fields.Char(
+        string="Device FQDN",
+        help="Fully Qualified Domain Name",
+        compute="_compute_device_info",
+        store=True,
+    )
 
     def _prepare_export_vals(self, include_license_data=True):
         if include_license_data:
@@ -30,3 +48,26 @@ class SoftwareLicenseHardware(models.Model):
             res = {}
         res["hardware_identifier"] = self.name
         return res
+
+    @api.depends("info")
+    def _compute_device_info(self):
+        self.device_name = False
+        self.device_domain = False
+        self.device_fqdn = False
+        for rec in self.filtered("info"):
+            data = json.loads(rec.info)
+            # get telemetry node (fallback to params for older data)
+            telemetry = data.get("telemetry", data.get("params"))
+            if telemetry:
+                network_information = telemetry.get("NetworkInformation")
+                if network_information:
+                    rec.device_name = network_information.get("HostName")
+                    rec.device_domain = network_information.get("DomainName")
+                if not rec.device_name:
+                    system_info = telemetry.get("SystemInfo")
+                    if system_info:
+                        rec.device_name = system_info.get("deviceName")
+                if rec.device_domain:
+                    rec.device_fqdn = f"{rec.device_name}.{rec.device_domain}"
+                else:
+                    rec.device_fqdn = rec.device_name
