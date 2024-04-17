c89d84b60be541c7ab3b7f1acef83d536d9484a8
Yann Papouin <ypa@decgroupe.com>
2024-04-17 08:18:38 +0200

    [IMP] mail_qweb: Apply premailer specifically when rendering qweb body

--- mail_qweb/models/mail_template.py
+++ mail_qweb/models/mail_template.py
@@ -2,22 +2,36 @@
 # Written by Yann Papouin <ypa at decgroupe.com>, Mar 2024
 
 from odoo import models
+from odoo import tools
 
 
 class MailTemplate(models.Model):
     _inherit = "mail.template"
 
-    def generate_email(self, res_ids, fields):
-        # also apply premailer before Odoo opens its html editor
-        multi_mode = True
-        if isinstance(res_ids, int):
-            res_ids = [res_ids]
-            multi_mode = False
-        result = super(MailTemplate, self).generate_email(res_ids, fields=fields)
+    def _render_qweb_body_eval_context(self, record):
+        render_context = super()._render_qweb_body_eval_context(record)
+        render_context.update(
+            {
+                # we also need the mail subject in our custom templates
+                "subject": self._render_field("subject", record.ids)[record.id],
+                "is_html_empty": tools.is_html_empty,
+                "access_link": hasattr(record, "_notify_get_action_link")
+                and record._notify_get_action_link("view")
+                or False,
+            }
+        )
+        return render_context
 
-        for res_id in res_ids:
-            if self.body_type == "qweb" and (not fields or "body_html" in fields):
-                result[res_id]["body_html"] = self._premailer_apply_transform(
-                    result[res_id]["body_html"]
-                )
-        return result if multi_mode else result[res_ids[0]]
+    def _render_qweb_body(
+        self, res_ids, compute_lang=False, set_lang=False, post_process=False
+    ):
+        # will also apply premailer before Odoo opens its html editor
+        res = super()._render_qweb_body(
+            res_ids=res_ids,
+            compute_lang=compute_lang,
+            set_lang=set_lang,
+            post_process=post_process,
+        )
+        for res_id, rendered in res.items():
+            res[res_id] = self._premailer_apply_transform(tools.ustr(rendered))
+        return res
