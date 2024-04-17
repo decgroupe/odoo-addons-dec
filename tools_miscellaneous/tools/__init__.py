060c034a01615626c078b619bb2601eb57e21cc3
Yann Papouin <ypa@decgroupe.com>
2024-04-17 15:06:39 +0200

    [IMP] tools_miscellaneous: New tool to safely update translations from `post_init`

--- tools_miscellaneous/tools/__init__.py
+++ tools_miscellaneous/tools/__init__.py
@@ -2,3 +2,29 @@ from . import bench
 from . import html_helper
 from . import console_helper
 from . import context
+
+# import the real Gettext shortcut
+from odoo import _ as _t
+
+
+def _(value):
+    """Fake Gettext underscore shortcuts to force Odoo generating a translation entry"""
+    return value
+
+
+def update_translation(env, xmlid, vals):
+    # force translation update
+    rec = env.ref(xmlid)
+    for lang in env["res.lang"].get_installed():
+        # the Gettext alias `_` will use `inspect.currentframe` to retrieve references
+        # on `context` and `cr`, that's why we are faking them here
+        context = {"lang": lang[0]}
+        cr = env.cr
+        # ready to update translation
+        rec_with_lang = rec.with_context(**context)
+        trans_vals = {}
+        for field, value in vals.items():
+            # get the translated value
+            trans_vals[field] = _t(value)
+        # write translations
+        rec_with_lang.write(trans_vals)
