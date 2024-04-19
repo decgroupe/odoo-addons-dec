c82c8dae54f67db36dcf00bd3e2ff052da7cc326
Yann Papouin <ypa@decgroupe.com>
2024-04-19 16:11:59 +0200

    [IMP] mail_extra_notify_project: Implementation update

--- mail_extra_notify_project/models/project_task.py
+++ mail_extra_notify_project/models/project_task.py
@@ -7,23 +7,23 @@ from odoo import models
 class ProjectTask(models.Model):
     _inherit = "project.task"
 
-    def _get_user_assigned_extra_values(self):
+    def _get_assigned_extra_values(self, type):
         self.ensure_one()
         res = {}
 
         if self.sale_line_id:
-            key, value = self._get_user_assigned_extra_field_value(
+            key, value = self._get_assigned_extra_field_value(
                 self,
                 "sale_line_id",
             )
             res[key] = value
-            key, value = self._get_user_assigned_extra_field_value(
+            key, value = self._get_assigned_extra_field_value(
                 self.sale_line_id.order_id,
                 "partner_shipping_id",
             )
             res[key] = value
         # Zip
-        key, value = self._get_user_assigned_extra_field_value(
+        key, value = self._get_assigned_extra_field_value(
             self,
             "partner_shipping_zip_id",
         )
