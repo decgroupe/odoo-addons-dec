235abec01446632166a3f0bd808089564329e3ad
Yann Papouin <ypa@decgroupe.com>
2024-04-19 16:35:36 +0200

    [IMP] mail_extra_notify: Tests updated

--- mail_extra_notify/tests/test_mail_extra_notify.py
+++ mail_extra_notify/tests/test_mail_extra_notify.py
@@ -20,7 +20,7 @@ class TestMailExtraNotify(TestMailExtraNotifyCommon):
             existing_mail_ids = self.Mail.search([])
             # set salesperson
             self.partner_id.user_id = self.user1
-            # get lastest email
+            # get latest email
             mail_id = self.Mail.search([]) - existing_mail_ids
             self.assertEqual(len(mail_id), 1)
             self.assertEqual(
@@ -32,3 +32,28 @@ class TestMailExtraNotify(TestMailExtraNotifyCommon):
             self.assertIn("Created by", mail_id.body)
         finally:
             self.mail_unlink_enabled()
+
+    def test_02_assigned_activity_more_informations(self):
+        try:
+            self.mail_unlink_disabled()
+            # keep a trace of existing mail
+            existing_mail_ids = self.Mail.search([])
+            # assign an activity
+            activity_id = self.partner_id.activity_schedule(
+                act_type_xmlid="mail.mail_activity_data_todo",
+                note="Please check this",
+                user_id=self.user1.id,
+            )
+            # get latest email
+            mail_id = self.Mail.search([]) - existing_mail_ids
+            self.assertEqual(len(mail_id), 1)
+            self.assertEqual(
+                mail_id.subject,
+                "Azure Interior, Brandon Freeman: To Do assigned to you",
+            )
+            self.assertIn("Email", mail_id.body)
+            self.assertIn("Created on", mail_id.body)
+            self.assertIn("Created by", mail_id.body)
+        finally:
+            self.mail_unlink_enabled()
+
