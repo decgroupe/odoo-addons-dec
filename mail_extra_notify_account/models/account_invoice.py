d419a7cc836ccbad55f18e48c7066638a6e1a101
Yann Papouin <ypa@decgroupe.com>
2024-04-19 15:52:44 +0200

    [IMP] mail_extra_notify_account: Implementation update

--- mail_extra_notify_account/models/account_invoice.py
+++ mail_extra_notify_account/models/account_invoice.py
@@ -7,30 +7,30 @@ from odoo import models
 class AccountMove(models.Model):
     _inherit = "account.move"
 
-    def _get_user_assigned_extra_values(self):
+    def _get_assigned_extra_values(self, type):
         self.ensure_one()
         res = {}
 
         if self.invoice_origin:
-            key, value = self._get_user_assigned_extra_field_value(
+            key, value = self._get_assigned_extra_field_value(
                 self,
                 "invoice_origin",
             )
             res[key] = value
         if self.partner_shipping_id:
-            key, value = self._get_user_assigned_extra_field_value(
+            key, value = self._get_assigned_extra_field_value(
                 self,
                 "partner_shipping_id",
             )
             res[key] = value
         # State
-        key, value = self._get_user_assigned_extra_field_value(
+        key, value = self._get_assigned_extra_field_value(
             self,
             "state",
         )
         res[key] = value
         # Total
-        key, value = self._get_user_assigned_extra_field_value(
+        key, value = self._get_assigned_extra_field_value(
             self,
             "amount_total",
         )
