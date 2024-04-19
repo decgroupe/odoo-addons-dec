72d4cb9528dc8e2196071a952c12ccdbda37290a
Yann Papouin <ypa@decgroupe.com>
2024-04-19 16:11:41 +0200

    [IMP] mail_extra_notify_hr: Implementation update

--- mail_extra_notify_hr/models/hr_expense_sheet.py
+++ mail_extra_notify_hr/models/hr_expense_sheet.py
@@ -7,12 +7,12 @@ from odoo import models
 class HrExpenseSheet(models.Model):
     _inherit = "hr.expense.sheet"
 
-    def _get_user_assigned_extra_values(self):
+    def _get_assigned_extra_values(self, type):
         self.ensure_one()
         res = {}
 
-        if self.user_id:
-            key, value = self._get_user_assigned_extra_field_value(
+        if self.employee_id:
+            key, value = self._get_assigned_extra_field_value(
                 self,
                 "employee_id",
             )
