3e4e87a45c62c6dea19ae1d0ace7452449f93bc8
Yann Papouin <ypa@decgroupe.com>
2024-04-22 09:52:42 +0200

    [IMP] mail_extra_notify: Monetary field support added
    Also added technical field name as html attribute

--- mail_extra_notify/models/base.py
+++ mail_extra_notify/models/base.py
@@ -6,13 +6,24 @@ from datetime import datetime
 from odoo import models
 from odoo.tools import format_datetime
 
+
+def format_currency_amount(amount, currency_id):
+    pre = currency_id.position == "before"
+    symbol = "{symbol}".format(symbol=currency_id.symbol or "")
+    return "{pre}{0}{post}".format(
+        amount, pre=symbol if pre else "", post=symbol if not pre else ""
+    )
+
+
 class Base(models.AbstractModel):
     _inherit = "base"
 
     def _get_assigned_extra_field_value(self, model, field_name):
         IrTranslation = self.env["ir.translation"]
-        key = IrTranslation.get_field_string(model._name)[field_name]
+        field_name_translated = IrTranslation.get_field_string(model._name)[field_name]
+        key = (field_name, field_name_translated)
         value = model[field_name]
+        field = model._fields[field_name]
         if isinstance(value, models.Model):
             if value:
                 value = value.name_get()[0][1]
@@ -20,4 +31,8 @@ class Base(models.AbstractModel):
                 value = False
         elif isinstance(value, datetime):
             value = format_datetime(self.env, value, self.env.user.tz)
+        elif isinstance(value, float):
+            if field.type == "monetary":
+                currency_id = model[field.currency_field]
+                value = format_currency_amount(value, currency_id)
         return key, value
