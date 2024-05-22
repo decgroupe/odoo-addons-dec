4251e8aa08dd0707821f4d2424e50fdd0008bc34
Yann Papouin <ypa@decgroupe.com>
2024-05-22 14:33:42 +0200

    [IMP] account_merge: Remove dependency over `l10n_generic_coa`

--- account_merge/tests/test_account_merge_tax.py
+++ account_merge/tests/test_account_merge_tax.py
@@ -4,21 +4,49 @@
 from odoo.tests.common import TransactionCase, tagged
 
 
-@tagged('post_install', '-at_install')
+@tagged("post_install", "-at_install")
 class TestAccountMergeTax(TransactionCase):
+
+    def _create_tax(self, name, type_tax_use, amount):
+        return self.env["account.tax"].create(
+            {
+                "name": "Test Sale Tax",
+                "type_tax_use": "sale",
+                "amount": "20",
+            }
+        )
+
+    def __create_xmlid(self, res_model, res_id, name, module):
+        return self.env["ir.model.data"].create(
+            {
+                "module": module,
+                "name": name,
+                "model": res_model,
+                "res_id": res_id,
+            }
+        )
+
+    def _create_xmlid(self, record_id, name, module):
+        return self.__create_xmlid(record_id._name, record_id.id, name, module)
+
     def setUp(self):
         super().setUp()
         self.tax_model = self.env["account.tax"]
         self.merge_account_tax_wizard_model = self.env["merge.account.tax.wizard"]
         self.group_do_merge = self.env.ref("account_merge.res_group_do_merge")
+        # we cannot rely on `l10n_generic_coa`, so we create our own data
+        self.sale_tax = self._create_tax("Test Sale Tax", "sale", 20)
+        self._create_xmlid(self.sale_tax, "test_sale_tax", "account_merge")
+        self.purchase_tax = self._create_tax("Test Purchase Tax", "purchase", 20)
+        self._create_xmlid(self.purchase_tax, "test_purchase_tax", "account_merge")
 
     def test_01_merge(self):
         # get a first tax reference
-        t1 = self.env.ref("l10n_generic_coa.1_sale_tax_template")
+        t1 = self.sale_tax
         # keep current data for future comparison
         t1_data = t1.read()[0]
         # get another tax reference that will be merged in to the first one
-        t2 = self.env.ref("l10n_generic_coa.1_purchase_tax_template")
+        t2 = self.purchase_tax
         # edit some values
         t2.write({})
         # keep current data for future comparison
