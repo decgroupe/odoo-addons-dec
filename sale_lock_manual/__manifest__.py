8fdc06b4419dbb31b46d65133b0748613d20a30b
Yann Papouin <ypa@decgroupe.com>
2024-05-24 15:43:06 +0200

    [ADD] sale_lock_manual

---
+++ sale_lock_manual/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Sale Lock Manual",
+    "version": "14.0.1.0.0",
+    "author": "DEC",
+    "website": "https://www.decgroupe.com",
+    "category": "Sales",
+    "depends": [
+        "sale",
+    ],
+    "data": [
+        "security/model_security.xml",
+        "views/sale_order.xml",
+    ],
+    "installable": True,
+}
---
+++ sale_lock_manual/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+Allow manual sale order lock/unlock
