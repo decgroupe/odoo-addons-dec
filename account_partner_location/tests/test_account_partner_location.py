a42ff4b35018bc3e2f02c95dac24022389595544
Yann Papouin <ypa@decgroupe.com>
2024-10-04 15:17:07 +0200

    [IMP] account_partner_location: Tests added

--- account_partner_location/tests/test_account_partner_location.py
+++ account_partner_location/tests/test_account_partner_location.py
@@ -5,8 +5,14 @@ from odoo.tests.common import TransactionCase
 
 
 class TestAccountPartnerLocation(TransactionCase):
+
     def setUp(self):
         super().setUp()
+        self.account_move_model = self.env["account.move"]
+        self.account_move_line_model = self.env["account.move.line"]
 
-    # def test_01_(self):
-    #     pass
+    def test_01_field_names(self):
+        self.assertIn("partner_zip_id", self.account_move_model._fields)
+        self.assertIn("partner_city_id", self.account_move_model._fields)
+        self.assertIn("partner_zip_id", self.account_move_line_model._fields)
+        self.assertIn("partner_city_id", self.account_move_line_model._fields)
