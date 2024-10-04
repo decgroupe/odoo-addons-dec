d2149509146d4bc2c2d25d01f38ac79467de8e74
Yann Papouin <ypa@decgroupe.com>
2024-10-04 15:06:15 +0200

    [IMP] crm_lead_partner_location_department: Tests added

--- crm_lead_partner_location_department/tests/test_crm_lead_partner_location_department.py
+++ crm_lead_partner_location_department/tests/test_crm_lead_partner_location_department.py
@@ -5,8 +5,13 @@ from odoo.tests.common import TransactionCase
 
 
 class TestCrmLeadPartnerLocationDepartment(TransactionCase):
+
     def setUp(self):
         super().setUp()
+        self.lead_model = self.env["crm.lead"]
 
-    # def test_01_(self):
-    #     pass
+    def test_01_field_names(self):
+        self.assertIn("partner_department_id", self.lead_model._fields)
+        self.assertIn("partner_state_id", self.lead_model._fields)
+        self.assertIn("partner_shipping_department_id", self.lead_model._fields)
+        self.assertIn("partner_shipping_state_id", self.lead_model._fields)
