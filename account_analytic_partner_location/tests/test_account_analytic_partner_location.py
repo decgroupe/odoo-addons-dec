50c76496eb42b6551ab413f8d29d2f618407e657
Yann Papouin <ypa@decgroupe.com>
2024-10-04 15:10:41 +0200

    [IMP] account_analytic_partner_location: Tests added

--- account_analytic_partner_location/tests/test_account_analytic_partner_location.py
+++ account_analytic_partner_location/tests/test_account_analytic_partner_location.py
@@ -6,5 +6,11 @@ from odoo.tests import common
 
 
 class TestAccountAnalyticPartnerLocation(common.TransactionCase):
+
     def setUp(self):
         super().setUp()
+        self.analytic_line_model = self.env["account.analytic.line"]
+
+    def test_01_field_names(self):
+        self.assertIn("partner_zip_id", self.analytic_line_model._fields)
+        self.assertIn("partner_city_id", self.analytic_line_model._fields)
