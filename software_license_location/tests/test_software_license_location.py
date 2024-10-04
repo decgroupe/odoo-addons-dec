aa89f672dbf0212f5b93555c1ac9664b8ee094b8
Yann Papouin <ypa@decgroupe.com>
2024-10-04 15:29:23 +0200

    [IMP] software_license_location: Tests added

--- software_license_location/tests/test_software_license_location.py
+++ software_license_location/tests/test_software_license_location.py
@@ -5,8 +5,11 @@ from odoo.tests.common import TransactionCase
 
 
 class TestSoftwareLicenseLocation(TransactionCase):
+
     def setUp(self):
         super().setUp()
+        self.license_model = self.env["software.license"]
 
-    # def test_01_(self):
-    #     pass
+    def test_01_field_names(self):
+        self.assertIn("partner_zip_id", self.license_model._fields)
+        self.assertIn("partner_city_id", self.license_model._fields)
