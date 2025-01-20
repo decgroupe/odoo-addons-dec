665c08c4d8107c56de223134c8770d706e97be1b
Yann Papouin <ypa@decgroupe.com>
2025-01-20 16:03:57 +0100

    [FIX] company_report: Use main company for tests

--- company_report/tests/test_company_report.py
+++ company_report/tests/test_company_report.py
@@ -8,24 +8,23 @@ class TestCompanyReport(TransactionCase):
 
     def setUp(self):
         super().setUp()
-        # company model
-        self.cm = self.env["res.company"]
+        self.company_id = self.env.ref("base.main_company")
+        self.assertTrue(self.company_id.exists())
 
-    def test_01_(self):
-        company_id = self.cm.search([], limit=1)
-        self.assertTrue(company_id.exists())
+    def test_01_footer_format(self):
         # no footer until enabled
-        self.assertFalse(company_id.report_bank_footer)
+        self.assertFalse(self.company_id.report_bank_footer)
         # check one and only one bank
-        partner_bank_id = company_id.bank_ids
+        partner_bank_id = self.company_id.bank_ids
         self.assertEqual(len(partner_bank_id), 1)
         partner_bank_id.footer = True
         # ensure no bank record is linked to this partner bank instance
         self.assertFalse(partner_bank_id.bank_id)
-        self.assertEqual(company_id.report_bank_footer, "60-16-13 31926819")
+        self.assertEqual(self.company_id.report_bank_footer, "60-16-13 31926819")
         # manually assign an existing bank record
         partner_bank_id.bank_id = self.env.ref("base.bank_bnp")
         self.assertTrue(partner_bank_id.bank_id)
         self.assertEqual(
-            company_id.report_bank_footer, "BNP Paribas: 60-16-13 31926819 - GEBABEBB"
+            self.company_id.report_bank_footer,
+            "BNP Paribas: 60-16-13 31926819 - GEBABEBB",
         )
