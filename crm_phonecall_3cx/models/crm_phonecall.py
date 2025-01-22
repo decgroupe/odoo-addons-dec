b62da6149153d887caeee7882f4baab7e4cf4863
Yann Papouin <ypa@decgroupe.com>
2025-01-22 09:12:49 +0100

    [DEL] crm_phonecall_3cx: Call Design Workflow (CFD) is now disabled
    Since 3CX v.20 for 4.AS free licences

--- crm_phonecall_3cx/models/crm_phonecall.py
+++
@@ -1,8 +0,0 @@
-# Copyright (C) DEC SARL, Inc - All Rights Reserved.
-# Written by Yann Papouin <ypa at decgroupe.com>, Sep 2021
-
-from odoo import models
-
-
-class CrmPhonecall(models.Model):
-    _inherit = "crm.phonecall"
--- crm_phonecall_3cx/readme/DESCRIPTION.rst
+++
@@ -1,1 +0,0 @@
-CRM integration with 3CX using Call Design Workflow (CFD)
