d064e34687fd2a843fe05c40fe9aa218b4108b00
Yann Papouin <ypa@decgroupe.com>
2025-03-25 13:46:54 +0100

    [FIX] mrp_traceability: Invalid value set by default context
    The form context is set with a default value for state (default_state) but this key is propagated to `mail.tracking.email` that also contains a state field.
    
    This will raise an error like:
    
    2025-03-18 14:47:08,043 1706231 ERROR dec odoo.addons.mail.models.mail_mail: failed sending mail (id: 178939) due to Wrong value for mail.tracking.email.state: 'draft'
    Traceback (most recent call last):
      File "/opt/odoo/14.0/odoo/addons/mail/models/mail_mail.py", line 315, in _send
        values = mail._send_prepare_values(partner=partner)
      File "/opt/odoo/14.0/odoo-addons/oca/social/mail_tracking/models/mail_mail.py", line 36, in _send_prepare_values
        tracking_email = self.env["mail.tracking.email"].sudo().create(vals)
    [...]
      File "/opt/odoo/14.0/odoo/odoo/fields.py", line 2437, in convert_to_cache
        raise ValueError("Wrong value for %s: %r" % (self, value))
    ValueError: Wrong value for mail.tracking.email.state: 'draft'

---
+++ mrp_traceability/models/stock_move_line.py
@@ -0,0 +1,28 @@
+# Copyright (C) DEC SARL, Inc - All Rights Reserved.
+# Written by Yann Papouin <ypa at decgroupe.com>, Mar 2025
+
+import logging
+
+from odoo import api, fields, models
+from odoo.exceptions import ValidationError
+
+_logger = logging.getLogger(__name__)
+
+
+class StockMoveLine(models.Model):
+    _inherit = "stock.move.line"
+
+    def _log_message(self, record, move, template, vals):
+        # built-in log_message method will call `message_post_with_view` on record
+        # (not on self), so only its context is altered
+        if (
+            record.env.context.get("form_view_ref")
+            == "mrp_traceability.stock_move_details_form_view"
+        ):
+            context = dict(record.env.context)
+            # remove state from context to avoid unwanted side effects (since
+            # `mail.tracking.email` also have a `state` field)
+            context.pop("default_state", None)
+            record = record.with_context(context)
+        res = super()._log_message(record, move, template, vals)
+        return res
