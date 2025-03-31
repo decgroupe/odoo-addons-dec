bee87ba5b12df80efe80d8c6a58eaea9fa6039d4
Yann Papouin <ypa@decgroupe.com>
2025-03-31 11:18:35 +0200

    [IMP] hr_timesheet_autofill: Generic function to search for analytics

--- hr_timesheet_autofill/tests/common.py
+++ hr_timesheet_autofill/tests/common.py
@@ -25,6 +25,21 @@ class TestHrTimesheetAutofillCommon(TransactionCase):
         }
         return ctx
 
+    def _search_for(self, text, al_ids=False, exclude_ids=False):
+        res_ids = (
+            self.analytic_line_model.with_user(self.user_admin)
+            .with_context(**self._get_default_autofill_context())
+            .name_search(text)
+        )
+        # extract id from the name get tuple (id, display_name) and browse data
+        analytic_line_ids = self.analytic_line_model.browse([x[0] for x in res_ids])
+        for al_id in al_ids:
+            if exclude_ids and al_id in exclude_ids:
+                self.assertNotIn(al_id, analytic_line_ids)
+            else:
+                self.assertIn(al_id, analytic_line_ids)
+        return analytic_line_ids
+
     def setUp(self):
         super().setUp()
         self.analytic_line_model = self.env["account.analytic.line"]
