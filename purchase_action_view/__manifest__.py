de2526fb92f33873d571333b76859d10e7b71e8e
Yann Papouin <ypa@decgroupe.com>
2025-06-12 14:24:06 +0200

    [MIG] purchase_action_view: Migration to 18.0

--- purchase_action_view/__manifest__.py
+++ purchase_action_view/__manifest__.py
@@ -1,13 +1,14 @@
 {
     "name": "Purchase Action View",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
+    "license": "AGPL-3",
     "author": "DEC, Yann Papouin",
-    "website": "https://www.decgroupe.com",
+    "website": "https://decgroupe.com",
     "depends": [
         "purchase",
     ],
     "data": [
         "views/purchase_order.xml",
     ],
-    "installable": False,
+    "installable": True,
 }
