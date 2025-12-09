17d1dae0b4d1aa943b20d953bcade01686a42751
Yann Papouin <ypa@decgroupe.com>
2025-12-09 14:29:12 +0100

    [MIG] base_module_path: Migration to 18.0

--- base_module_path/__manifest__.py
+++ base_module_path/__manifest__.py
@@ -1,6 +1,7 @@
 {
     "name": "Module Path",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
+    "license": "AGPL-3",
     "author": "DEC",
     "website": "https://decgroupe.com",
     "depends": [
@@ -8,8 +9,12 @@
         "web",
     ],
     "data": [
-        "views/assets.xml",
         "views/ir_module.xml",
     ],
-    "installable": False,
+    "assets": {
+        "web.assets_backend": [
+            "base_module_path/static/src/css/style.scss",
+        ]
+    },
+    "installable": True,
 }
