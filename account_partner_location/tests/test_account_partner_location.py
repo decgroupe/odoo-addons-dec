f29b10cd0ca714eb5bbf10b962744110a643e568
Yann Papouin <ypa@decgroupe.com>
2025-12-10 13:44:50 +0100

    [MIG] account_partner_location: Migration to 18.0

--- account_partner_location/tests/test_account_partner_location.py
+++ account_partner_location/tests/test_account_partner_location.py
@@ -5,7 +5,6 @@ from odoo.tests.common import TransactionCase
 
 
 class TestAccountPartnerLocation(TransactionCase):
-
     def setUp(self):
         super().setUp()
         self.account_move_model = self.env["account.move"]
