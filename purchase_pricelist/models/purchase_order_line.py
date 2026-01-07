028e1aa45749f0d909e070c6fac0900dfb3ab210
Yann Papouin <ypa@decgroupe.com>
2026-01-07 13:57:42 +0100

    [FIX] purchase_pricelist: Method to update procurement moved to `puchase_stock_pricelist`

--- purchase_pricelist/models/purchase_order_line.py
+++ purchase_pricelist/models/purchase_order_line.py
@@ -79,22 +79,9 @@ class PurchaseOrderLine(models.Model):
     def _prepare_purchase_order_line(
         self, product_id, product_qty, product_uom, company_id, supplier, po
     ):
-        # Note that this method is called only when creating a PO from procurement
+        # note that this method is called only when creating a PO from procurement.
+        # this case is managed and tested by "purchase_stock_pricelist" module
         res = super()._prepare_purchase_order_line(
             product_id, product_qty, product_uom, company_id, supplier, po
         )
-        if po and po.pricelist_id:
-            taxes_id = self.env["account.tax"]
-            if res.get("taxes_id") and len(res["taxes_id"][0]) == 3:
-                taxes_id = taxes_id.browse(res["taxes_id"][0][2])
-            price_unit = self._get_price_unit(
-                supplier.partner_id,
-                po.pricelist_id,
-                product_id,
-                product_qty,
-                product_uom,
-                taxes_id,
-                company_id=po.company_id,
-            )
-            res["price_unit"] = price_unit
         return res
