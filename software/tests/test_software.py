1434393574d67766f38bf75e45977ac5b54ffeec
Yann Papouin <ypa@decgroupe.com>
2026-01-15 14:42:07 +0100

    [MIG] software: Migration to 18.0

--- software/tests/test_software.py
+++ software/tests/test_software.py
@@ -8,5 +8,10 @@ class TestSoftware(TransactionCase):
     def setUp(self):
         super().setUp()
 
-    # def test_01_(self):
-    #     pass
+    def test_01_security_group_exists(self):
+        group_id = self.env.ref("software.group_software_user")
+        self.assertTrue(group_id.exists())
+        group_id = self.env.ref("software.group_software_manager")
+        self.assertTrue(group_id.exists())
+        group_id = self.env.ref("software.group_software_supermanager")
+        self.assertTrue(group_id.exists())
