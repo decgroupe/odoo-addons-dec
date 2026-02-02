c245ed455c9eb373c7845b665a49fe3176a1330b
Yann Papouin <ypa@decgroupe.com>
2026-02-02 10:29:13 +0100

    [DEL] stock_picking_line_auto_fill: Not needed anymore
    Like `stock_move_line_auto_fill`, this functionality is now native

--- stock_picking_line_auto_fill/models/stock_picking.py
+++
@@ -1,9 +0,0 @@
-# Copyright (C) DEC SARL, Inc - All Rights Reserved.
-# Written by Yann Papouin <ypa at decgroupe.com>, Nov 2020
-
-from odoo import models
-
-
-class StockPicking(models.Model):
-    _inherit = "stock.picking"
-    _order = "id desc"
