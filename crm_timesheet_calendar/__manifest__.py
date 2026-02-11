d8829f94979bad409249916725065ab6d012ecfd
Yann Papouin <ypa@decgroupe.com>
2026-02-11 16:41:00 +0100

    [MIG] crm_timesheet_calendar: Migration to 18.0

--- crm_timesheet_calendar/__manifest__.py
+++ crm_timesheet_calendar/__manifest__.py
@@ -1,14 +1,15 @@
 {
     "name": "CRM Timesheet Calendar",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
+    "license": "AGPL-3",
     "author": "DEC",
     "website": "https://decgroupe.com",
     "depends": [
         "crm_timesheet",
-        "hr_timesheet_calendar",
+        "hr_timesheet_calendar_dec",
     ],
     "data": [
         "views/account_analytic_line.xml",
     ],
-    "installable": False,
+    "installable": True,
 }
---
+++ crm_timesheet_calendar/tests/test_crm_timesheet_calendar.py
@@ -0,0 +1,13 @@
+# Copyright (C) DEC SARL, Inc - All Rights Reserved.
+# Written by Yann Papouin <ypa at decgroupe.com>, Sep 2023
+
+from odoo.tests.common import TransactionCase
+
+
+class TestCrmTimesheetCalendar(TransactionCase):
+    """Tests for crm_timesheet_calendar module."""
+
+    @classmethod
+    def setUpClass(cls):
+        """Set up shared test data."""
+        super().setUpClass()
