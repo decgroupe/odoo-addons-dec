a5124dd223fe023b8fa2ac8c4e4468f50398edfa
Yann Papouin <ypa@decgroupe.com>
2026-02-12 16:39:20 +0100

    [MIG] product_pack_line: Migration to 18.0

--- product_pack_line/tests/test_product_pack_line.py
+++ product_pack_line/tests/test_product_pack_line.py
@@ -7,6 +7,10 @@ from odoo.tests.common import TransactionCase
 class TestProductPackLine(TransactionCase):
     def setUp(self):
         super().setUp()
+        self.ProductPackLine = self.env["product.pack.line"]
 
-    # def test_01_(self):
-    #     pass
+    def test_01_field_names(self):
+        self.assertIn("product_name", self.ProductPackLine._fields)
+        self.assertIn("product_code", self.ProductPackLine._fields)
+        self.assertIn("product_uom_id", self.ProductPackLine._fields)
+        self.assertIn("product_categ_id", self.ProductPackLine._fields)
