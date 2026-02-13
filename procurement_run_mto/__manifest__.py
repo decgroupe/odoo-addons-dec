971de18d9799a2f5f9ddee9bf9258e77b7f870eb
Yann Papouin <ypa@decgroupe.com>
2026-02-13 11:04:05 +0100

    [MIG] procurement_run_mto: Migration to 18.0

--- procurement_run_mto/__manifest__.py
+++ procurement_run_mto/__manifest__.py
@@ -1,6 +1,7 @@
 {
     "name": "Procurement Run MTO",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
+    "license": "AGPL-3",
     "author": "DEC",
     "website": "https://decgroupe.com",
     "depends": [
@@ -12,5 +13,5 @@
         "procurement_log",
     ],
     "data": [],
-    "installable": False,
+    "installable": True,
 }
--- procurement_run_mto/models/procurement_group.py
+++ procurement_run_mto/models/procurement_group.py
@@ -15,15 +15,32 @@ class ProcurementGroup(models.Model):
     @api.model
     def _run_scheduler_tasks(self, use_new_cursor=False, company_id=False):
         # Retry moves where PO/MO were canceled or deleted
-        self.sudo()._retry_orphan_mto_moves(
+        self.sudo()._retry_orphan_mto_moves(company_id=company_id)
+        result = super()._run_scheduler_tasks(
             use_new_cursor=use_new_cursor, company_id=company_id
         )
-        super(ProcurementGroup, self)._run_scheduler_tasks(
-            use_new_cursor=use_new_cursor, company_id=company_id
-        )
+        # basic copy/paste of odoo's original code
+        if "scheduler_task_done" in self._context:
+            task_done = (
+                self._context.get("scheduler_task_done", {"task_done": 0})["task_done"]
+                + 1
+            )
+            self._context["scheduler_task_done"]["task_done"] = task_done
+        else:
+            task_done = self._get_scheduler_tasks_to_do()
+        if use_new_cursor:
+            self.env["ir.cron"]._notify_progress(
+                done=task_done, remaining=self._get_scheduler_tasks_to_do() - task_done
+            )
+            self.env.cr.commit()  # pylint: disable=E8102
+        return result
 
     @api.model
-    def _retry_orphan_mto_moves(self, use_new_cursor=False, company_id=False):
+    def _get_scheduler_tasks_to_do(self):
+        return super()._get_scheduler_tasks_to_do() + 1
+
+    @api.model
+    def _retry_orphan_mto_moves(self, company_id=False):
         """Re-create procurements for 'make_to_order' moves if necessary.
         :param bool use_new_cursor: if set, use a dedicated cursor
         """
@@ -49,8 +66,6 @@ class ProcurementGroup(models.Model):
                     self._action_confirm_one_move(move)
             except UserError as error:
                 self._log_exception(product_id, error.name, self.env.user)
-        if use_new_cursor:
-            self._cr.commit()
 
     @api.model
     def _get_mto_moves_to_confirm(self):
@@ -71,15 +86,15 @@ class ProcurementGroup(models.Model):
 
     @api.model
     def _filter_mto_picking_moves_to_confirm(self, picking_ids):
-        return picking_ids.mapped("move_lines").filtered(
+        return picking_ids.mapped("move_ids").filtered(
             lambda x: x.state not in ("done", "cancel")
             and x.procure_method == "make_to_order"
             and x.location_id == self.env.ref("stock.stock_location_stock")
-            and x.created_purchase_line_id.id == False
-            and x.move_orig_ids.purchase_line_id.id == False
-            and x.created_production_id.id == False
-            and x.created_mrp_production_request_id.id == False
-            and x.move_orig_ids.ids == []
+            and not x.created_purchase_line_ids
+            and not x.move_orig_ids.purchase_line_id
+            and not x.created_production_id
+            and not x.created_mrp_production_request_id
+            and not x.move_orig_ids
         )
 
     @api.model
@@ -88,23 +103,21 @@ class ProcurementGroup(models.Model):
             lambda x: x.state not in ("done", "cancel")
             and x.procure_method == "make_to_order"
             and x.location_id == self.env.ref("stock.stock_location_stock")
-            and x.created_purchase_line_id.id == False
-            and x.move_orig_ids.purchase_line_id.id == False
-            and x.created_production_id.id == False
-            and x.created_mrp_production_request_id.id == False
-            and x.orderpoint_created_production_ids.ids == []
-            and x.orderpoint_created_purchase_line_ids.ids == []
-            and x.move_orig_ids.ids == []
+            and not x.created_purchase_line_ids
+            and not x.move_orig_ids.purchase_line_id
+            and not x.created_production_id
+            and not x.created_mrp_production_request_id
+            and not x.orderpoint_created_production_ids
+            and not x.orderpoint_created_purchase_line_ids
+            and not x.move_orig_ids
         )
 
     @api.model
     def _action_confirm_one_move(self, move):
         move.ensure_one()
         _logger.info(
-            "Execute method _action_confirm on {} ({})".format(
-                move.product_id.display_name,
-                move.picking_id.display_name,
-            )
+            "Execute method _action_confirm on "
+            f"{move.product_id.display_name} ({move.picking_id.display_name})"
         )
         move._do_unreserve()
         if move.move_line_ids:
