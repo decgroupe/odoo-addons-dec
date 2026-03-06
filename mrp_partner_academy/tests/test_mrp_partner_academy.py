716c0b658a7ffa62c03b4af54e23da285002a3e7
Yann Papouin <ypa@decgroupe.com>
2026-03-06 16:22:33 +0100

    [MIG] mrp_partner_academy: Migration to 18.0

--- mrp_partner_academy/tests/test_mrp_partner_academy.py
+++ mrp_partner_academy/tests/test_mrp_partner_academy.py
@@ -5,8 +5,11 @@ from odoo.tests.common import TransactionCase
 
 
 class TestMrpPartnerAcademy(TransactionCase):
+    """Test the mrp_partner_academy module."""
+
     def setUp(self):
         super().setUp()
+        self.production_model = self.env["mrp.production"]
 
-    # def test_01_(self):
-    #     pass
+    def test_01_field_names(self):
+        self.assertIn("partner_academy_id", self.production_model._fields)
