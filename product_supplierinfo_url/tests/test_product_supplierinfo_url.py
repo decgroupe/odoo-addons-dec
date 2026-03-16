e93d3680e31188e1f351f70c5be1938940e0c096
Yann Papouin <ypa@decgroupe.com>
2026-03-16 16:08:26 +0100

    [MIG] product_supplierinfo_url: Migration to 18.0

--- product_supplierinfo_url/tests/test_product_supplierinfo_url.py
+++ product_supplierinfo_url/tests/test_product_supplierinfo_url.py
@@ -5,8 +5,11 @@ from odoo.tests.common import TransactionCase
 
 
 class TestProductSupplierinfoUrl(TransactionCase):
+    """Test product_supplierinfo_url module."""
+
     def setUp(self):
         super().setUp()
+        self.supplierinfo_model = self.env["product.supplierinfo"]
 
-    # def test_01_(self):
-    #     pass
+    def test_01_field_names(self):
+        self.assertIn("url", self.supplierinfo_model._fields)
