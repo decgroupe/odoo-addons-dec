09d18e8fbbbb42a0bf4bd668c485594199807d6c
Yann Papouin <ypa@decgroupe.com>
2026-03-24 15:25:24 +0100

    [MIG] project_timesheet_naming: Migration to 18.0

--- project_timesheet_naming/__manifest__.py
+++ project_timesheet_naming/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Project Timesheet Naming",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
     "author": "DEC",
     "website": "https://decgroupe.com",
     "depends": [
@@ -8,5 +8,5 @@
         "project",
     ],
     "data": [],
-    "installable": False,
+    "installable": True,
 }
