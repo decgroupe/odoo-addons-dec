537a3f48d26c1d6a128e38e6eec5aee6bf9d1bae
Yann Papouin <ypa@decgroupe.com>
2026-03-27 15:25:20 +0100

    [MIG] partner_search_city: Migration to 18.0

--- partner_search_city/__manifest__.py
+++ partner_search_city/__manifest__.py
@@ -1,6 +1,7 @@
 {
     "name": "Partner Search City",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
+    "license": "AGPL-3",
     "author": "DEC",
     "website": "https://decgroupe.com",
     "depends": [
@@ -10,5 +11,5 @@
     "data": [
         "views/partner_view.xml",
     ],
-    "installable": False,
+    "installable": True,
 }
--- partner_search_city/tests/test_partner_search_city.py
+++ partner_search_city/tests/test_partner_search_city.py
@@ -1,12 +1,23 @@
 # Copyright (C) DEC SARL, Inc - All Rights Reserved.
-# Written by Yann Papouin <ypa at decgroupe.com>, Sep 2023
+# Written by Yann Papouin <ypa at decgroupe.com>, Mar 2026
 
 from odoo.tests.common import TransactionCase
 
 
 class TestPartnerSearchCity(TransactionCase):
-    def setUp(self):
-        super().setUp()
+    """Tests for partner_search_city module."""
 
-    # def test_01_(self):
-    #     pass
+    @classmethod
+    def setUpClass(cls):
+        """Set up shared test data."""
+        super().setUpClass()
+
+    def test_01_search_view_fields(self):
+        """Verify that the inherited search view contains the expected fields."""
+        view = self.env.ref("partner_search_city.view_res_partner_search_city")
+        self.assertTrue(view)
+        arch = view.get_combined_arch()
+        self.assertIn("zip_id", arch)
+        self.assertIn("city", arch)
+        self.assertIn("zip", arch)
+        self.assertIn("street", arch)
