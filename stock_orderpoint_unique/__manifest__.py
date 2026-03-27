a0dd35feecedc95070646d983fb717470700c65a
Yann Papouin <ypa@decgroupe.com>
2026-03-27 09:07:56 +0100

    [MIG] stock_orderpoint_unique: Migration to 18.0

--- stock_orderpoint_unique/__manifest__.py
+++ stock_orderpoint_unique/__manifest__.py
@@ -1,11 +1,11 @@
 {
     "name": "Unique Orderpoint",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
     "author": "DEC",
     "website": "https://decgroupe.com",
     "depends": [
         "stock",
     ],
     "data": [],
-    "installable": False,
+    "installable": True,
 }
