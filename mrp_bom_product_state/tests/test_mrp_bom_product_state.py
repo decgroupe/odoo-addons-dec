bdcf78ed759ce131796485be0a11daf83a81b0e4
Yann Papouin <ypa@decgroupe.com>
2026-03-28 16:31:02 +0100

    [MIG] mrp_bom_product_state: Migration to 18.0

--- mrp_bom_product_state/tests/test_mrp_bom_product_state.py
+++ mrp_bom_product_state/tests/test_mrp_bom_product_state.py
@@ -5,8 +5,26 @@ from odoo.tests.common import TransactionCase
 
 
 class TestMrpBomProductState(TransactionCase):
-    def setUp(self):
-        super().setUp()
+    """Tests for mrp_bom_product_state module."""
 
-    # def test_01_(self):
-    #     pass
+    @classmethod
+    def setUpClass(cls):
+        """Set up shared test data."""
+        super().setUpClass()
+        cls.state = cls.env["product.state"].create(
+            {"name": "Test State", "code": "test"}
+        )
+        cls.product_tmpl = cls.env["product.template"].create(
+            {"name": "Test Product", "type": "consu"}
+        )
+        cls.bom = cls.env["mrp.bom"].create({"product_tmpl_id": cls.product_tmpl.id})
+
+    def test_01_state_from_template_visible_on_bom(self):
+        """Test that the product state set on the template is visible on the bom."""
+        self.product_tmpl.product_state_id = self.state
+        self.assertEqual(self.bom.product_state_id, self.state)
+
+    def test_02_state_writable_via_bom(self):
+        """Test that writing product_state_id on the bom propagates to the template."""
+        self.bom.product_state_id = self.state
+        self.assertEqual(self.product_tmpl.product_state_id, self.state)
