5b1b30c81631148fe754f203bba5e6723da39ac4
Yann Papouin <ypa@decgroupe.com>
2026-03-30 18:04:29 +0200

    [MIG] crm_timesheet_time_control: Migration to 18.0

--- crm_timesheet_time_control/__manifest__.py
+++ crm_timesheet_time_control/__manifest__.py
@@ -1,6 +1,7 @@
 {
     "name": "CRM Timesheet Time Control",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
+    "license": "AGPL-3",
     "author": "DEC",
     "website": "https://decgroupe.com",
     "depends": [
@@ -10,5 +11,5 @@
     "data": [
         "views/crm_lead.xml",
     ],
-    "installable": False,
+    "installable": True,
 }
