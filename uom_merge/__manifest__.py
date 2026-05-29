2c30ff9947d739a50ac9ca5a04df0b137673a1e2
Yann Papouin <ypa@decgroupe.com>
2026-05-29 13:46:30 +0200

    [ADD] uom_merge

---
+++ uom_merge/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Merge Units of Measure",
+    "version": "14.0.1.0.0",
+    "author": "DEC",
+    "website": "https://www.decgroupe.com",
+    "depends": [
+        "uom",
+        "deltatech_merge",
+    ],
+    "data": [
+        "security/res_groups.xml",
+        "security/ir.model.access.csv",
+        "wizard/merge_uom_uom.xml",
+    ],
+    "installable": True,
+}
