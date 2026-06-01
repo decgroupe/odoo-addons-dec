f63d732ff0b461b4332cb5fa8a5f733e3904e910
Yann Papouin <ypa@decgroupe.com>
2026-06-01 10:54:21 +0200

    [FIX] mrp_stage: Also set stage in "done" state

--- mrp_stage/models/mrp_production.py
+++ mrp_stage/models/mrp_production.py
@@ -85,12 +85,16 @@ class MrpProduction(models.Model):
             if activity_stage_id:
                 stage_id = activity_stage_id
         elif self.state in ("done"):
-            move_finished_ids = self.move_finished_ids.filtered(
-                lambda x: x.state in ("done", "cancel")
-            )
-            picking_move_ids = move_finished_ids.mapped("move_dest_ids")
-            if not all(m.state in ("done", "cancel") for m in picking_move_ids):
-                stage_id = stages["dispatch_ready"]
+            activity_stage_id = self._get_stage_from_activity()
+            if activity_stage_id:
+                stage_id = activity_stage_id
+            else:
+                move_finished_ids = self.move_finished_ids.filtered(
+                    lambda x: x.state in ("done", "cancel")
+                )
+                picking_move_ids = move_finished_ids.mapped("move_dest_ids")
+                if not all(m.state in ("done", "cancel") for m in picking_move_ids):
+                    stage_id = stages["dispatch_ready"]
         return stage_id
 
     @api.depends(
@@ -141,6 +145,7 @@ class MrpProduction(models.Model):
         self.write(
             {
                 "state": "confirmed",
+                "date_start": False,
             }
         )
         # OCA module needed: web_ir_actions_act_view_reload
